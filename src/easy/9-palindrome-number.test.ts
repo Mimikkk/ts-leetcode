b4616816bbd36aed1463de712e6d8df969f6fe9b
Hououin Kyouma <inwazzin@gmail.com>
2022-04-23 22:13:54 +0200

    9 - palindrome number

---
+++ src/easy/9-palindrome-number.test.ts
@@ -0,0 +1,19 @@
+export {};
+
+const isPalindrome = (n: number): boolean =>
+  [...`${n}`].join('') == [...`${n}`].reverse().join('');
+
+
+describe("3 - palindrome number", () => {
+  it("1 - 121", () => {
+    expect(isPalindrome(121)).toEqual(true);
+  });
+
+  it("2 - 13", () => {
+    expect(isPalindrome(13)).toEqual(false);
+  });
+
+  it("3 - 1", () => {
+    expect(isPalindrome(1)).toEqual(true);
+  });
+});
