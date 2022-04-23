901563a92dc5193b6498a4c8d6d0885a778d8e70
Hououin Kyouma <inwazzin@gmail.com>
2022-04-23 23:06:08 +0200

    26 - remove duplicates from sorted array

---
+++ src/easy/26-remove-duplicates-from-sorted-array.test.ts
@@ -0,0 +1,24 @@
+export {};
+
+const removeDuplicates = (nums: number[]): number => {
+  for (let i = nums.length - 1; i > 0; i--) {
+    if (nums[i] === nums[i - 1]) {
+      nums.splice(i, 1);
+    }
+  }
+
+  return nums.length;
+};
+
+describe("26 - remove duplicates from sorted array", () => {
+  test("case 1", () => {
+    expect(removeDuplicates([1, 1, 2])).toEqual(2);
+  });
+  test("case 2", () => {
+    expect(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])).toEqual(5);
+  });
+
+  test("case 3", () => {
+    expect(removeDuplicates([1, 1, 1, 2, 2, 3])).toEqual(3);
+  });
+});
