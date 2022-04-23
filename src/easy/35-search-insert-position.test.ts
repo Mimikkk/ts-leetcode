bfdc73bcbde4c53f98ee2f00588b5e7560ab5c98
Hououin Kyouma <inwazzin@gmail.com>
2022-04-23 23:41:53 +0200

    35 - search insert position

---
+++ src/easy/35-search-insert-position.test.ts
@@ -0,0 +1,33 @@
+export {};
+
+const searchInsert = (nums: number[], target: number): number => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) return mid;
+    if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+};
+
+describe("35 - search insert position", () => {
+  it("should pass the first case", () => {
+    expect(searchInsert([1, 3, 5, 6], 5)).toBe(2);
+  });
+
+  it("should pass the second case", () => {
+    expect(searchInsert([1, 3, 5, 6], 2)).toBe(1);
+  });
+
+  it("should pass the third case", () => {
+    expect(searchInsert([1, 3, 5, 6], 7)).toBe(4);
+  });
+});
