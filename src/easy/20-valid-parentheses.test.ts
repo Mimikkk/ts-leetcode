9f4bba6585c48df82a12c6d64f19f40c5fc68c08
Hououin Kyouma <inwazzin@gmail.com>
2022-04-23 22:33:50 +0200

    20 - valid parentheses

---
+++ src/easy/20-valid-parentheses.test.ts
@@ -0,0 +1,50 @@
+export {};
+
+const complement: Record<string, string> = {
+  "(":")",
+  "{":"}",
+  "[":"]",
+};
+
+const isValid = (s: string): boolean => {
+  const stack: string[] = [];
+
+  return [...s].every(c => {
+    switch (c) {
+      case "(":
+      case "{":
+      case "[":
+        return stack.push(c) > 0;
+      case ")":
+      case "}":
+      case "]":
+        return complement[stack.pop() ?? ""] === c;
+    }
+  }) && stack.length === 0;
+};
+
+describe("20 - valid parentheses", () => {
+  it("test case 1", () => {
+    expect(isValid("()")).toEqual(true);
+  });
+
+  it("test case 2", () => {
+    expect(isValid("()[]{}")).toEqual(true);
+  });
+
+  it("test case 3", () => {
+    expect(isValid("(]")).toEqual(false);
+  });
+
+  it("test case 4", () => {
+    expect(isValid("([)]")).toEqual(false);
+  });
+
+  it("test case 5", () => {
+    expect(isValid("{[]}")).toEqual(true);
+  });
+
+  it("test case 6", () => {
+    expect(isValid("]")).toEqual(false);
+  });
+});
