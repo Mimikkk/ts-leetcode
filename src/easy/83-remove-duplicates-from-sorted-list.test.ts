78dbff3ee5c6c857c96e313d3469e7791bcdb8a2
Hououin Kyouma <inwazzin@gmail.com>
2022-04-24 01:20:48 +0200

    83 remove duplicates from sorted list

--- src/easy/83-remove-duplicates-from-sorted-list.test.ts
+++ src/easy/83-remove-duplicates-from-sorted-list.test.ts
@@ -1,10 +1,42 @@
 import { ListNode } from "../structures";
-export {};
+const { node, array } = ListNode;
 
-const deleteDuplicates = (head: ListNode | null): ListNode | null => {
-  return null;
+const tester = (numbers: number[]): number[] =>
+  array(deleteDuplicates(node(numbers)));
+
+const deleteDuplicates = (list: ListNode | null): ListNode | null => {
+  let current = list;
+
+  while (current && current.next) {
+    if (current.val == current.next.val) current.next = current.next.next;
+    else current = current.next;
+  }
+
+  return list;
 };
 
 describe("83 - remove duplicates from sorted list", () => {
-  it.todo("aa");
+  it("should return null for an empty list", () => {
+    expect(tester([])).toEqual([]);
+  });
+
+  it("should return the list with no duplicates", () => {
+    expect(tester([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it("should return the list with no duplicates", () => {
+    expect(tester([1, 1, 2, 2, 3, 3, 4, 4, 5, 5])).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it("should return the list with no duplicates", () => {
+    expect(tester([1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9])).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+
+  it("should return the list with no duplicates", () => {
+    expect(tester([1, 1])).toEqual([1]);
+  });
+
+  it("should return the list with no duplicates", () => {
+    expect(tester([0])).toEqual([0]);
+  });
 });
