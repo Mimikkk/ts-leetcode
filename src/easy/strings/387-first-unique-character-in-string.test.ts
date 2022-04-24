7ca927540608347e58b2b1a27f4c471c393a432a
Hououin Kyouma <inwazzin@gmail.com>
2022-04-25 01:44:47 +0200

    387 - first unique character in string

---
+++ src/easy/strings/387-first-unique-character-in-string.test.ts
@@ -0,0 +1,25 @@
+export {};
+
+const firstUniqChar = (s: string): number => {
+  const characters: Record<string, number> = {};
+
+  for (const char of s) characters[char] = (characters[char] || 0) + 1;
+
+  for (let i = 0; i < s.length; i++) if (characters[s[i]] === 1) return i;
+
+  return -1;
+};
+
+describe("firstUniqChar", () => {
+  it("it returns the index of the first unique character", () => {
+    expect(firstUniqChar("leetcode")).toEqual(0);
+  });
+
+  it("it returns -1 if there is no unique character", () => {
+    expect(firstUniqChar("loveleetcode")).toEqual(2);
+  });
+
+  it("it returns -1 if the string is empty", () => {
+    expect(firstUniqChar("")).toEqual(-1);
+  });
+});
