3d4225f4b04e8f72ada3dbf02fb941271c75fc4e
Hououin Kyouma <inwazzin@gmail.com>
2022-04-24 02:43:56 +0200

    108 - convert sorted array to binary search tree

---
+++ src/easy/108-convert-sorted-array-to-binary-search-tree.test.ts
@@ -0,0 +1,23 @@
+import { TreeNode } from "../structures";
+
+const sortedArrayToBST = (nums: number[]): TreeNode | null => {
+  if (!nums.length) return null;
+
+  const mid = Math.floor(nums.length / 2);
+
+  const root = new TreeNode(nums[mid]);
+  root.left = sortedArrayToBST(nums.slice(0, mid));
+  root.right = sortedArrayToBST(nums.slice(mid + 1));
+
+  return root;
+};
+
+describe("108 - convert sorted array to binary search tree", () => {
+  it("case 2 empty", () => {
+    expect(sortedArrayToBST([])).toEqual(null);
+  });
+
+  it("case 4 one element", () => {
+    expect(sortedArrayToBST([1])).toEqual(new TreeNode(1, null, null));
+  });
+});
