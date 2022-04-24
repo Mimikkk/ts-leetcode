7890b17c20b023f9fcaf4dc943821dbd3d4b21c6
Hououin Kyouma <inwazzin@gmail.com>
2022-04-25 01:24:26 +0200

    349 - intersections of two arrays

---
+++ src/easy/arrays/349-intersections-of-two-arrays.test.ts
@@ -0,0 +1,17 @@
+export {};
+
+const intersection = (nums1: number[], nums2: number[]): number[] => {
+  const set = new Set(nums1);
+
+  return [...new Set(nums2.filter(num => set.has(num)))];
+};
+
+describe("Intersection of Two Arrays", () => {
+  it("case 1", () => {
+    expect(intersection([1, 2, 2, 1], [2, 2])).toEqual([2]);
+  });
+
+  it("case 2", () => {
+    expect(intersection([4, 9, 5], [9, 4, 9, 8, 4])).toEqual([9, 4]);
+  });
+});
