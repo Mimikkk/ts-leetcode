47f62761dbf37e5d342432504e01a98998d96f01
Hououin Kyouma <inwazzin@gmail.com>
2022-04-24 16:00:20 +0200

    242 - valid anagram

---
+++ src/easy/strings/242-valid-anagram.test.ts
@@ -0,0 +1,24 @@
+export {};
+
+const isAnagram = (s: string, t: string): boolean => {
+  const map: Record<string, number> = {};
+
+  for (const char of s) map[char] = (map[char] || 0) + 1;
+
+  for (const char of t) {
+    if (!map[char]) return false;
+    map[char] -= 1;
+  }
+
+  return Object.values(map).every(val => val === 0);
+};
+
+describe("242-valid-anagram", () => {
+  it("anagram is valid", () => {
+    expect(isAnagram("anagram", "nagaram")).toEqual(true);
+  });
+
+  it("anagram is not valid", () => {
+    expect(isAnagram("rat", "car")).toEqual(false);
+  });
+});
