ec57af7709bd89967c819a25f67e2f1c5f8c3ee6
Hououin Kyouma <inwazzin@gmail.com>
2022-04-25 01:44:57 +0200

    389 - find the difference

---
+++ src/easy/strings/389-find-the-difference.test.ts
@@ -0,0 +1,17 @@
+export {};
+
+const findTheDifference = (s: string, t: string): string => {
+  const ss = [...s].sort();
+  const ts = [...t].sort();
+
+  const len = Math.max(ss.length, ts.length);
+  for (let i = 0; i < len; i++) if (ss[i] !== ts[i]) return ts[i];
+
+  return "";
+};
+
+describe('findTheDifference', () => {
+  it('#1', () => {
+    expect(findTheDifference('abcd', 'abcde')).toEqual('e');
+  });
+});
