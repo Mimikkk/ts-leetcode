e3e7fc7976b83727c2c01ecb1ea65d3f55665c11
Hououin Kyouma <inwazzin@gmail.com>
2022-04-24 14:45:28 +0200

    205 - isomorphic strings

---
+++ src/easy/strings/205-isomorphic-strings.test.ts
@@ -0,0 +1,35 @@
+export {};
+
+const isIsomorphic = (s: string, t: string): boolean => {
+  if (s.length !== t.length) return false;
+
+  let map = new Map();
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      if (map.get(s[i]) !== t[i]) return false;
+    } else {
+      if ([...map.values()].some(x => x === t[i])) return false;
+      map.set(s[i], t[i]);
+    }
+  }
+
+  return true;
+};
+
+describe("205 - isomorphic strings", () => {
+  it("should return true", () => {
+    expect(isIsomorphic("egg", "add")).toBe(true);
+  });
+
+  it("should return false", () => {
+    expect(isIsomorphic("foo", "bar")).toBe(false);
+  });
+
+  it("should return true", () => {
+    expect(isIsomorphic("paper", "title")).toBe(true);
+  });
+
+  it("should return false", () => {
+    expect(isIsomorphic("ab", "aa")).toBe(false);
+  });
+});
