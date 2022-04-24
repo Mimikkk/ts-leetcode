286c28bfe4531bc036198b040409d8a6be644ae3
Hououin Kyouma <inwazzin@gmail.com>
2022-04-25 01:37:33 +0200

    374 - guess number higher or lower

---
+++ src/easy/numbers/374-guess-number-higher-or-lower.test.ts
@@ -0,0 +1,29 @@
+export {};
+
+const guess = (n: number) => 0;
+
+const guessNumber = (n: number): number => {
+  let low = 1;
+  let high = n;
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const guessed = guess(mid);
+    if (guessed === 0) {
+      return mid;
+    } else if (guessed === -1) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return -1;
+};
+
+
+describe("guessNumber", () => {
+  it("should return the number if it is correct", () => {
+    expect(guessNumber(5)).toEqual(3);
+  });
+});
