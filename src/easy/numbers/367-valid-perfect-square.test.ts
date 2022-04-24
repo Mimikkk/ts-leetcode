c860aff8c7e937db386fb4963e6b53e65ead8a3a
Hououin Kyouma <inwazzin@gmail.com>
2022-04-25 01:34:30 +0200

    367 - valid perfect square

---
+++ src/easy/numbers/367-valid-perfect-square.test.ts
@@ -0,0 +1,28 @@
+export {};
+
+const isPerfectSquare = (num: number): boolean => {
+  if (num === 1) return true;
+
+  let left = 1;
+  let right = num;
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    const square = mid * mid;
+    if (square === num) return true;
+
+    if (square > num) right = mid - 1;
+    else left = mid + 1;
+  }
+
+  return false;
+};
+
+describe("Valid Perfect Square", () => {
+  it("it returns true for perfect squares", () => {
+    expect(isPerfectSquare(16)).toBe(true);
+  });
+
+  it("it returns false for non-perfect squares", () => {
+    expect(isPerfectSquare(14)).toBe(false);
+  });
+});
