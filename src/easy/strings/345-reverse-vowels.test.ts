920c31e74a93581b31a6eed1eb44f8868d515099
Hououin Kyouma <inwazzin@gmail.com>
2022-04-25 01:17:29 +0200

    345 - reverse vowels

---
+++ src/easy/strings/345-reverse-vowels.test.ts
@@ -0,0 +1,31 @@
+export {};
+
+const reverseVowels = (s: string): string => {
+  const vowels = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]);
+  const result = [...s];
+
+  let left = 0;
+  let right = result.length - 1;
+
+  while (left < right) {
+    if (!vowels.has(result[left])) ++left;
+    else if (!vowels.has(result[right])) --right;
+    else {
+      [result[left], result[right]] = [result[right], result[left]];
+      ++left;
+      --right;
+    }
+  }
+
+  return result.join("");
+};
+
+describe("Reverse vowels", () => {
+  it("should revert vowels", () => {
+    expect(reverseVowels("hello")).toEqual("holle");
+  });
+
+  it("should revert vowels", () => {
+    expect(reverseVowels("leetcode")).toEqual("leotcede");
+  });
+});
