f2e99c83122a9b7f457f48693083991b1ccde38b
Hououin Kyouma <inwazzin@gmail.com>
2022-04-24 03:54:01 +0200

    136 - single number

---
+++ src/easy/arrays/136-single-number.test.ts
@@ -0,0 +1,17 @@
+export {};
+
+const singleNumber = (nums: number[]): number => nums.reduce((acc, curr) => acc ^ curr, 0);
+
+describe("136 - single number", () => {
+  it("#1", () => {
+    expect(singleNumber([2, 2, 1])).toEqual(1);
+  });
+
+  it("#2", () => {
+    expect(singleNumber([4, 1, 2, 1, 2])).toEqual(4);
+  });
+
+  it("#3", () => {
+    expect(singleNumber([1, 2, 1, 3, 3])).toEqual(2);
+  });
+});
