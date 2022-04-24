5b36a5aa04414bc72f7743eab80fdded1da6e7bd
Hououin Kyouma <inwazzin@gmail.com>
2022-04-25 01:05:50 +0200

    326 power of three

---
+++ src/easy/numbers/326-power-of-three.test.ts
@@ -0,0 +1,25 @@
+export {};
+
+const isPowerOfThree = (n: number): boolean => {
+  if (n < 1) return false;
+  while (!(n % 3)) n /= 3;
+  return n === 1;
+};
+
+describe("326 - power of three", () => {
+  it("#1", () => {
+    expect(isPowerOfThree(27)).toBe(true);
+  });
+
+  it("#2", () => {
+    expect(isPowerOfThree(0)).toBe(false);
+  });
+
+  it("#3", () => {
+    expect(isPowerOfThree(9)).toBe(true);
+  });
+
+  it("#4", () => {
+    expect(isPowerOfThree(45)).toBe(false);
+  });
+});
