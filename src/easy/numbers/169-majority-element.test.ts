5c518d45db5220fa2d556fbf0fabe9744160e1b2
Hououin Kyouma <inwazzin@gmail.com>
2022-04-24 04:25:47 +0200

    169 - majority element

---
+++ src/easy/numbers/169-majority-element.test.ts
@@ -0,0 +1,30 @@
+export {};
+
+const majorityElement = (nums: number[]): number => {
+  const elements: Record<number, number> = {};
+
+  for (const num of nums) elements[num] = (elements[num] || 0) + 1;
+
+  let [max, index] = [0, 0];
+  for (const [key, value] of Object.entries(elements)) {
+    if (value > max) {
+      max = value;
+      index = Number(key);
+    }
+  }
+  return index;
+};
+
+describe("169 - majority element", () => {
+  it("three", () => {
+    expect(majorityElement([3, 2, 3])).toEqual(3);
+  });
+
+  it("four", () => {
+    expect(majorityElement([4, 4, 4, 4, 5, 5, 5])).toEqual(4);
+  });
+
+  it("two", () => {
+    expect(majorityElement([2, 2, 1, 1, 1, 2, 2])).toEqual(2);
+  });
+});
