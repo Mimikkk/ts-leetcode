738015419e21aec28dd48cbf638819979895a667
Hououin Kyouma <inwazzin@gmail.com>
2022-04-25 01:48:00 +0200

    392 - is subsequence

---
+++ src/easy/strings/392-is-subsequence.test.ts
@@ -0,0 +1,34 @@
+export {};
+
+const isSubsequence = (s: string, t: string): boolean => {
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) ++i;
+    ++j;
+  }
+
+  return i === s.length;
+};
+
+describe("isSubsequence", () => {
+  it("#1", () => {
+    expect(isSubsequence("abc", "ahbgdc")).toEqual(true);
+  });
+
+  it("#2", () => {
+    expect(isSubsequence("axc", "ahbgdc")).toEqual(false);
+  });
+
+  it("#3", () => {
+    expect(isSubsequence("", "ahbgdc")).toEqual(true);
+  });
+
+  it("#4", () => {
+    expect(isSubsequence("abc", "")).toEqual(false);
+  });
+
+  it("#5", () => {
+    expect(isSubsequence("", "")).toEqual(true);
+  });
+});
