ed9ba663b7ab89cc7b3e687f7e481c56d430163b
Hououin Kyouma <inwazzin@gmail.com>
2022-04-27 01:39:25 +0200

    492 - construct the rectangle

---
+++ src/easy/numbers/492-construct-the-rectangle.test.ts
@@ -0,0 +1,17 @@
+export {};
+
+const constructRectangle = (area: number): number[] => {
+  let mid = Math.floor(Math.sqrt(area));
+  while (area % mid) --mid;
+  return [Number(area / mid), mid];
+};
+
+describe("constructRectangle", () => {
+  it("area 37", () => {
+    expect(constructRectangle(37)).toEqual([37, 1]);
+  });
+
+  it("area 122122", () => {
+    expect(constructRectangle(122122)).toEqual([427, 286]);
+  });
+});
