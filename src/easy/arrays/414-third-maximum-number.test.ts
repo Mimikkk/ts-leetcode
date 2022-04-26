f31770ad3a304865628e1c3fd8604f22254be2a1
Hououin Kyouma <inwazzin@gmail.com>
2022-04-26 12:56:32 +0200

    414 - third maximum number

---
+++ src/easy/arrays/414-third-maximum-number.test.ts
@@ -0,0 +1,24 @@
+export {};
+
+const thirdMax = (nums: number[]): number => {
+  nums = [...new Set(nums)];
+  nums.sort((a, b) => b - a);
+
+  if (nums.length > 2) nums.length = 3;
+  else return Math.max(...nums);
+  return Math.min(...nums);
+};
+
+describe("Third max", () => {
+  it("should return third maximum number from array", () => {
+    expect(thirdMax([3, 2, 1])).toEqual(1);
+  });
+
+  it("should return third maximum number from array", () => {
+    expect(thirdMax([1, 2])).toEqual(2);
+  });
+
+  it("should return third maximum number from array", () => {
+    expect(thirdMax([2, 2, 3, 1])).toEqual(1);
+  });
+});
