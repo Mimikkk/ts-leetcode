811716b8b9005da03f44912c787991266864d375
Hououin Kyouma <inwazzin@gmail.com>
2022-04-26 13:24:37 +0200

    455 - assign cookies

---
+++ src/easy/arrays/455-assign-cookies.test.ts
@@ -0,0 +1,39 @@
+export {};
+
+const findContentChildren = (greediness: number[], sizes: number[]): number => {
+  greediness.sort((a, b) => a - b);
+  sizes.sort((a, b) => a - b);
+
+  let greedIndex = 0;
+  let sizeIndex = 0;
+
+  let contentCount = 0;
+
+  while (greedIndex < greediness.length && sizeIndex < sizes.length) {
+    if (greediness[greedIndex] <= sizes[sizeIndex]) {
+      ++contentCount;
+      ++greedIndex;
+    }
+    ++sizeIndex;
+  }
+
+  return contentCount;
+};
+
+describe("find content children", () => {
+  it("correct children", () => {
+    expect(findContentChildren([1, 2, 3], [1, 1])).toEqual(1);
+  });
+
+  it("no children", () => {
+    expect(findContentChildren([1, 2], [3, 3, 3])).toEqual(2);
+  });
+
+  it("no g", () => {
+    expect(findContentChildren([], [1, 2, 3])).toEqual(0);
+  });
+
+  it("no g", () => {
+    expect(findContentChildren([1, 2], [1, 2, 3])).toEqual(2);
+  });
+});
