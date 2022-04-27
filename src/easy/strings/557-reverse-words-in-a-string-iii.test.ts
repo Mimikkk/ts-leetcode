8ebaabb87652c5611dee484486ad8485bc03bf8c
Hououin Kyouma <inwazzin@gmail.com>
2022-04-28 01:43:43 +0200

    557 - reverse words in a string iii

---
+++ src/easy/strings/557-reverse-words-in-a-string-iii.test.ts
@@ -0,0 +1,13 @@
+export {};
+
+const reverseString = (s: string) => s.split("").reverse().join("");
+
+const reverseWords = (s: string) => s.split(" ").map(reverseString).join(" ");
+
+describe("reverse words in a string iii", () => {
+  test("case 1", () => {
+    expect(reverseWords("Let's take LeetCode contest")).toEqual(
+      "s'teL ekat edoCteeL tsetnoc",
+    );
+  });
+});
