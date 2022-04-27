5c4c149647d818193aa357ddfbbcbc3807b6dd72
Hououin Kyouma <inwazzin@gmail.com>
2022-04-28 00:30:30 +0200

    512 - longest uncommon sequence

---
+++ src/easy/strings/521-longest-uncommon-sequence.test.ts
@@ -0,0 +1,26 @@
+export {};
+
+const findLUSlength = (a: string, b: string): number =>
+  a === b ?-1:Math.max(a.length, b.length);
+
+describe("fin lus", () => {
+  it("case 1", () => {
+    expect(findLUSlength("aba", "cdc")).toEqual(3);
+  });
+
+  it("case 2", () => {
+    expect(findLUSlength("aaa", "bbb")).toEqual(3);
+  });
+
+  it("case 2", () => {
+    expect(findLUSlength("aaa", "abb")).toEqual(3);
+  });
+
+  it("case 3", () => {
+    expect(findLUSlength("aaa", "aaa")).toEqual(-1);
+  });
+
+  it("case 4", () => {
+    expect(findLUSlength("aefawfawfawfaw","aefawfeawfwafwaef")).toEqual(17);
+  });
+});
