404b0e9f33bd74d5a3d25563acf448bfdba54613
Hououin Kyouma <inwazzin@gmail.com>
2022-04-27 23:57:36 +0200

    520 - detect capital

---
+++ src/easy/strings/520-detect-capital.test.ts
@@ -0,0 +1,28 @@
+export {};
+
+const detectCapitalUse = (word: string) => {
+  const allCapital = word.toUpperCase() === word;
+  const allLower = word.toLowerCase() === word;
+  const firstCapitalOnly = word[0].toUpperCase() === word[0]
+    && word.slice(1).toLowerCase() === word.slice(1);
+
+  return allCapital || firstCapitalOnly || allLower;
+};
+
+describe("detect capitalization", () => {
+  it("usa", () => {
+    expect(detectCapitalUse("USA")).toBe(true);
+  });
+
+  it("Usa", () => {
+    expect(detectCapitalUse("Usa")).toBe(true);
+  });
+
+  it("usa", () => {
+    expect(detectCapitalUse("usa")).toBe(true);
+  });
+
+  it("Usa", () => {
+    expect(detectCapitalUse("UsA")).toBe(false);
+  });
+});
