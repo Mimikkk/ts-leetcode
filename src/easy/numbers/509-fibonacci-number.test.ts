1b08abda82e5ec3563cbc30ba857ca71e9ecd5d3
Hououin Kyouma <inwazzin@gmail.com>
2022-04-27 03:06:04 +0200

    509 - fibonacci number

---
+++ src/easy/numbers/509-fibonacci-number.test.ts
@@ -0,0 +1,21 @@
+export {};
+
+const fib = (n: number): number => {
+  let [a, b] = [0, 1];
+  for (let i = 0; i < n; i++) [a, b] = [b, a + b];
+  return a;
+};
+
+describe("fib", () => {
+  it("4", () => {
+    expect(fib(4)).toEqual(3);
+  });
+
+  it("10", () => {
+    expect(fib(10)).toEqual(55);
+  });
+
+  it("28", () => {
+    expect(fib(28)).toEqual(317811);
+  });
+});
