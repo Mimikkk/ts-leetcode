ab64bdbc2ba2f9be46682e0a7d646290b17852ac
Hououin Kyouma <inwazzin@gmail.com>
2022-04-27 03:05:54 +0200

    507 - perfect number

---
+++ src/easy/numbers/507-perfect-number.test.ts
@@ -0,0 +1,27 @@
+export {};
+
+const checkPerfectNumber = (num: number): boolean => {
+  if (num <= 5) return false;
+
+  const sqrt = Math.sqrt(num);
+
+  let sum = 1;
+  for (let i = 2; i <= sqrt; i++) {
+    if (num % i === 0) sum += (i === sqrt) ?i:i + num / i;
+  }
+  return (sum === num);
+};
+
+describe("check perfect number", () => {
+  it("should return true", () => {
+    expect(checkPerfectNumber(28)).toBe(true);
+  });
+
+  it("should return false", () => {
+    expect(checkPerfectNumber(1)).toBe(false);
+  });
+
+  it("should return false", () => {
+    expect(checkPerfectNumber(2)).toBe(false);
+  });
+});
