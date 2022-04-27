376c44fe5ca9036ee7456becfdefeab15cf74f85
Hououin Kyouma <inwazzin@gmail.com>
2022-04-27 23:49:45 +0200

    598 - range addition ii

---
+++ src/easy/numbers/598-range-addition-ii.test.ts
@@ -0,0 +1,20 @@
+export {};
+
+const maxCount = (m: number, n: number, ops: [number, number][]): number => {
+  for (const [x, y] of ops) {
+    m = Math.min(m, x);
+    n = Math.min(n, y);
+  }
+
+  return m * n;
+};
+
+describe("range addition ii", () => {
+  it("case 1", () => {
+    expect(maxCount(3, 3, [[1, 2], [2, 3], [3, 4]])).toBe(2);
+  });
+
+  it("case 2", () => {
+    expect(maxCount(3, 3, [[1, 2], [2, 3]])).toBe(2);
+  });
+})
