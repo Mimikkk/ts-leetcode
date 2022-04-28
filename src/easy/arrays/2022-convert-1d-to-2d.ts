6a8b24b84bded4560df0fd4a44c351b3cdf55b6b
Hououin Kyouma <inwazzin@gmail.com>
2022-04-28 02:29:21 +0200

    2022 - convert 1d to 2d

---
+++ src/easy/arrays/2022-convert-1d-to-2d.ts
@@ -0,0 +1,14 @@
+export {};
+
+const construct2DArray = (original: number[], m: number, n: number): number[][] => {
+  if (original.length !== m * n) return [];
+
+  const res: number[][] = Array(m).fill(0).map(() => Array(n).fill(0));
+  for (let i = 0; i < m; i++) {
+    res[i] = [];
+    for (let j = 0; j < n; j++) {
+      res[i][j] = original[i * n + j];
+    }
+  }
+  return res;
+};
