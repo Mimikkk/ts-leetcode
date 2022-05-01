afbe8c600769ed0d510dae7546b51f91cb5959bf
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 21:17:41 +0200

    696 - simplification

--- src/easy/binary/696-count-binary-substring.test.ts
+++ src/easy/binary/696-count-binary-substring.test.ts
@@ -4,12 +4,8 @@ const countBinarySubstrings = (s: string): number => {
   let [count, previous, current] = [0, 0, 1];
 
   for (let i = 1; i < s.length; i++) {
-    if (s[i] === s[i - 1]) {
-      ++current;
-    } else {
-      previous = current;
-      current = 1;
-    }
+    if (s[i] === s[i - 1]) ++current;
+    else [previous, current] = [current, 1];
 
     if (previous >= current) ++count;
   }
