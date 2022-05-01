c3834c97befa53f96fdcfed475425cfd7ea3f2ee
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 21:14:00 +0200

    680 - valid palindrome ii

---
+++ src/easy/strings/680-valid-palindrome-ii.test.ts
@@ -0,0 +1,38 @@
+export {};
+
+const isPalindrome = (s: string, l: number, r: number) => {
+  while (l < r) {
+    if (s[l] !== s[r]) return false;
+    ++l;
+    --r;
+  }
+  return true;
+};
+
+const validPalindrome = (s: string) => {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right])
+      return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+    ++left;
+    --right;
+  }
+
+  return true;
+};
+
+describe("valid palindrome ii", () => {
+  it("case 1 - with one remove", () => {
+    expect(validPalindrome("aba")).toBe(true);
+  });
+
+  it("case 2 - with one remove", () => {
+    expect(validPalindrome("abca")).toBe(true);
+  });
+
+  it("case 3 - with one remove", () => {
+    expect(validPalindrome("abc")).toBe(false);
+  });
+});
