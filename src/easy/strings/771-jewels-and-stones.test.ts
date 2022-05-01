4a3dd9629f9de28ecf2cd0d8ad30ec9f8103a196
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 23:20:40 +0200

    771 - jewels and stones

---
+++ src/easy/strings/771-jewels-and-stones.test.ts
@@ -0,0 +1,22 @@
+export {};
+
+const createCounter = (s: string) => {
+  const counter: Record<string, number> = {};
+  for (const char of s) counter[char] = (counter[char] || 0) + 1;
+  return counter;
+};
+
+const numJewelsInStones = (jewels: string, stones: string): number => {
+  const stoneCounter = createCounter(stones);
+  return [...jewels].reduce((acc, char) => acc + (stoneCounter[char] || 0), 0);
+};
+
+describe("jewels and stones", () => {
+  it("should pass", () => {
+    expect(numJewelsInStones("aA", "aAAbbbb")).toEqual(3);
+  });
+
+  it("should pass", () => {
+    expect(numJewelsInStones("z", "ZZ")).toEqual(0);
+  });
+});
