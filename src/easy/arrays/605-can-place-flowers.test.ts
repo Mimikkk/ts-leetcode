42cfdae611f840e4efb5399a1c972ed91f754f18
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 02:57:28 +0200

    605 - can place flowers

---
+++ src/easy/arrays/605-can-place-flowers.test.ts
@@ -0,0 +1,20 @@
+export {};
+
+const canPlaceFlowers = (flowerbed: number[], n: number): boolean =>
+  flowerbed.reduce((acc, _, i) => {
+    if (!flowerbed[i - 1] && !flowerbed[i] && !flowerbed[i + 1]) {
+      flowerbed[i] = 1;
+      return acc + 1;
+    }
+    return acc;
+  }, 0) >= n;
+
+describe("canPlaceFlowers", () => {
+  it("should return true", () => {
+    expect(canPlaceFlowers([1, 0, 0, 0, 1], 1)).toEqual(true);
+  });
+
+  it("should return false", () => {
+    expect(canPlaceFlowers([1, 0, 0, 0, 1], 2)).toEqual(false);
+  });
+});
