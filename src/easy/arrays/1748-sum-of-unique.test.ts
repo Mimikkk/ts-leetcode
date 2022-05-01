53ab5165a89beebdc786646d4bd66cf92b054c8d
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 21:15:21 +0200

    1748 - sum of unique

---
+++ src/easy/arrays/1748-sum-of-unique.test.ts
@@ -0,0 +1,28 @@
+export {};
+
+const sumOfUnique = (nums: number[]): number => {
+  const unique = new Set<number>(nums);
+  const visited = new Set<number>();
+
+  for (const num of nums) {
+    if (visited.has(num)) {
+      unique.delete(num);
+    } else visited.add(num);
+  }
+
+  return [...unique].reduce((acc, curr) => acc + curr, 0);
+};
+
+describe("sum of unique", () => {
+  it("case 1", () => {
+    expect(sumOfUnique([1, 2, 3, 1])).toEqual(5);
+  });
+
+  it("case 2", () => {
+    expect(sumOfUnique([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toEqual(55);
+  });
+
+  it("case 3", () => {
+    expect(sumOfUnique([2, 2, 2, 2])).toEqual(0);
+  });
+});
