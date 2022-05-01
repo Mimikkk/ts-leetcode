f1fdfb7c7b681612a63e5230e9ebd32730083594
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 21:14:57 +0200

    844 - backspace string compare

---
+++ src/easy/strings/844-backspace-string-compare.test.ts
@@ -0,0 +1,35 @@
+export {};
+
+const backspaceCompare = (s: string, t: string) => {
+  const applyBackspaces = (s: string) => {
+    let stack = [];
+
+    for (const char of s) {
+      switch (char) {
+        case "#":
+          stack.pop();
+          break;
+        default:
+          stack.push(char);
+      }
+    }
+
+    return stack.join("");
+  };
+
+  return applyBackspaces(s) === applyBackspaces(t);
+};
+
+describe("backspaceCompare", () => {
+  it("case 1", () => {
+    expect(backspaceCompare("ab#c", "ad#c")).toEqual(true);
+  });
+
+  it("case 2", () => {
+    expect(backspaceCompare("ab##", "c#d#")).toEqual(true);
+  });
+
+  it("case 3", () => {
+    expect(backspaceCompare("a#c", "b")).toEqual(false);
+  });
+});
