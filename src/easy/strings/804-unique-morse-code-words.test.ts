f6f43c5f891df108b040c3a5915bf8ef63ea32dc
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 22:58:42 +0200

    804 - unique morse code words

---
+++ src/easy/strings/804-unique-morse-code-words.test.ts
@@ -0,0 +1,37 @@
+export {};
+
+const MorseCode = [
+  ".-",
+  "-...",
+  "-.-.",
+  "-..",
+  ".",
+  "..-.",
+  "--.",
+  "....",
+  "..",
+  ".---",
+  "-.-",
+  ".-..",
+  "--",
+  "-.",
+  "---",
+  ".--.",
+  "--.-",
+  ".-.",
+  "...",
+  "-",
+  "..-",
+  "...-",
+  ".--",
+  "-..-",
+  "-.--",
+  "--.."];
+const morse = (s: string) => [...s].map(c => MorseCode[c.charCodeAt(0) - 97]).join("");
+const uniqueMorseRepresentations = (words: string[]): number => [...new Set(words.map(morse))].length;
+
+describe("unique morse code words", () => {
+  it("case 1", () => {
+    expect(uniqueMorseRepresentations(["gin", "zen", "gig", "msg"])).toEqual(2);
+  });
+});
