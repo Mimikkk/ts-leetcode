db5edc40c2e64d0e2d08380f535bdf2c75b637c7
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 19:16:18 +0200

    653 - two sum iv input is a bst

---
+++ src/easy/trees/binary-trees/653-two-sum-iv-input-is-a-bst.test.ts
@@ -0,0 +1,34 @@
+import { TreeNode } from "shared/structures";
+import node = TreeNode.node;
+
+const findTarget = (root: TreeNode | null, k: number): boolean => {
+  const complement = new Set();
+
+  const stack = [root];
+
+  while (stack.length) {
+    const node = stack.pop();
+
+    if (node) {
+      if (complement.has(node.val)) return true;
+      complement.add(k - node.val);
+      stack.push(node.left, node.right);
+    }
+  }
+
+  return false;
+};
+
+describe("Find target", () => {
+  it("can find target", () => {
+    expect(findTarget(node([5, 3, 6, 2, 4, null, 7]), 9)).toEqual(true);
+  });
+
+  it("can find target", () => {
+    expect(findTarget(node([5, 3, 6, 2, 4, null, 7]), 28)).toEqual(false);
+  });
+
+  it("can find target", () => {
+    expect(findTarget(node([0, -2, 3, null, -1, null, 4]), -2)).toEqual(true);
+  });
+});
