730ad17f5964620c99899dc695b6f3b5e050a070
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 21:14:27 +0200

    696 - count binary substrings

---
+++ src/easy/binary/696-count-binary-substring.test.ts
@@ -0,0 +1,28 @@
+export {};
+
+const countBinarySubstrings = (s: string): number => {
+  let [count, previous, current] = [0, 0, 1];
+
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] === s[i - 1]) {
+      ++current;
+    } else {
+      previous = current;
+      current = 1;
+    }
+
+    if (previous >= current) ++count;
+  }
+
+  return count;
+};
+
+describe("countBinarySubstrings", () => {
+  it("case 1", () => {
+    expect(countBinarySubstrings("00110011")).toEqual(6);
+  });
+
+  it("case 2", () => {
+    expect(countBinarySubstrings("10101")).toEqual(4);
+  });
+});
