c4ca12ebf010fc0a1e0e2a70de80d6a61f04c37a
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 03:58:07 +0200

    637 - average of levels in binary tree

---
+++ src/easy/trees/binary-trees/637-average-of-levels-in-binary-tree.test.ts
@@ -0,0 +1,43 @@
+import { TreeNode } from "shared/structures";
+import node = TreeNode.node;
+
+const averageOfLevels = (root: TreeNode | null): number[] => {
+  if (!root) return [];
+
+  const averages = [];
+
+  let nextLevel = [root];
+  while (nextLevel.length) {
+    const currentLevel = nextLevel;
+    nextLevel = [];
+
+    let sum = 0;
+    let count = 0;
+    while (currentLevel.length) {
+      const node = currentLevel.pop()!;
+
+      sum += node.val;
+      ++count;
+
+      if (node.left) nextLevel.push(node.left);
+      if (node.right) nextLevel.push(node.right);
+    }
+    averages.push(sum / count);
+  }
+
+  return averages;
+};
+
+describe(" averageOfLevels", () => {
+  describe("case 1", () => {
+    it("should return []", () => {
+      expect(averageOfLevels(node([1]))).toEqual([1]);
+    });
+  });
+
+  describe("case 1", () => {
+    it("should return []", () => {
+      expect(averageOfLevels(node([1, 2, 6]))).toEqual([1, 4]);
+    });
+  });
+});
