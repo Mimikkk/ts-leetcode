8e7fc911eebfec6bb5e920f17f08948d4e891325
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 21:15:06 +0200

    1408 - string matching in an array

---
+++ src/easy/strings/1408-string-matching-in-an-array.test.ts
@@ -0,0 +1,31 @@
+export {};
+
+const stringMatching = (words: string[]): string[] => {
+  const res = new Set<string>();
+
+  for (let i = 0; i < words.length; i++) {
+    for (let j = i + 1; j < words.length; j++) {
+      if (words[i].includes(words[j])) res.add(words[j]);
+      if (words[j].includes(words[i])) res.add(words[i]);
+    }
+  }
+  return [...res];
+};
+
+describe("string matching in array", () => {
+  it("case 1", () => {
+    expect(stringMatching(["leetcode", "et", "code"])).toEqual(["et", "code"]);
+  });
+
+  it("case 2", () => {
+    expect(stringMatching(["blue", "green", "bu"])).toEqual([]);
+  });
+
+  it("case 3", () => {
+    expect(stringMatching(["mass", "as", "hero", "superhero"])).toEqual(["as", "hero"]);
+  });
+
+  it("case 4", () => {
+    expect(stringMatching(["leetcoder", "leetcode", "od", "hamlet", "am"])).toEqual(["leetcode", "od", "am"]);
+  });
+});
