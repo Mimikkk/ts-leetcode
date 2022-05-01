33ed3f150b83788f5199e1d377e75f017590fc36
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 22:58:34 +0200

    925 - long pressed name

---
+++ src/easy/strings/925-long-pressed-name.test.ts
@@ -0,0 +1,34 @@
+export {};
+
+const isLongPressedName = (name: string, typed: string): boolean => {
+  let [i, j] = [0, 0];
+  while (j < typed.length) {
+    if (i < name.length && name[i] === typed[j]) ++i;
+    else if (typed[j] !== typed[j - 1]) return false;
+    ++j;
+  }
+
+  return i === name.length;
+};
+
+describe("is long pressed name", () => {
+  it("should pass", () => {
+    expect(isLongPressedName("alex", "aaleex")).toEqual(true);
+  });
+
+  it("should pass", () => {
+    expect(isLongPressedName("leelee", "lleeelee")).toEqual(true);
+  });
+
+  it("should pass", () => {
+    expect(isLongPressedName("saeed", "ssaaedd")).toEqual(false);
+  });
+
+  it("should pass", () => {
+    expect(isLongPressedName("laiden", "laiden")).toEqual(true);
+  });
+
+  it("should pass", () => {
+    expect(isLongPressedName("alex", "aaleexa")).toEqual(false);
+  });
+});
