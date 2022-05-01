b91a931a2f54ce7a2e48d069850ff5a3729da643
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 03:03:33 +0200

    606 - construct string from binary tree

---
+++ src/easy/trees/binary-trees/606-construct-string-from-binary-tree.test.ts
@@ -0,0 +1,37 @@
+import { TreeNode } from "shared/structures";
+import node = TreeNode.node;
+
+const tree2str = (root: TreeNode | null): string => {
+  if (root == null) return "";
+  const stack = [root];
+  const visited = new Set();
+
+  let result = "";
+  while (stack.length != 0) {
+    const current = stack[stack.length - 1];
+
+    if (visited.has(current)) {
+      stack.pop();
+      result = `${result})`;
+    } else {
+      visited.add(current);
+      result = `${result}(${current.val}`;
+      if (current.left === null && current.right) result = `${result}()`;
+      if (current.right) stack.push(current.right);
+      if (current.left) stack.push(current.left);
+    }
+  }
+
+  return result.slice(1, -1);
+};
+
+
+describe("tree2str", () => {
+  it("returns the string representation of binary tree", () => {
+    expect(tree2str(node([1, 2, 3, 4]))).toBe("1(2(4))(3)");
+  });
+
+  it("returns the string representation of binary tree", () => {
+    expect(tree2str(node([1, 2, 3, null, 4]))).toBe("1(2()(4))(3)");
+  });
+});
