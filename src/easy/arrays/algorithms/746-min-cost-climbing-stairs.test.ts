ab71b7b655a21099349a58815207961a3c58d3ec
Hououin Kyouma <inwazzin@gmail.com>
2022-05-02 01:31:58 +0200

    746 - min cost climbing stairs

---
+++ src/easy/arrays/algorithms/746-min-cost-climbing-stairs.test.ts
@@ -0,0 +1,19 @@
+export {};
+
+const minCostClimbingStairs = (costs: number[]): number => {
+  let [f1, f2] = [0, 0];
+
+  for (const cost of costs.reverse()) [f2, f1] = [f1, Math.min(f1, f2) + cost];
+
+  return Math.min(f1, f2);
+};
+
+describe("minCostClimbingStairs", () => {
+  it("case 1", () => {
+    expect(minCostClimbingStairs([10, 15, 20])).toEqual(15);
+  });
+
+  it("case 2", () => {
+    expect(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])).toEqual(6);
+  });
+});
