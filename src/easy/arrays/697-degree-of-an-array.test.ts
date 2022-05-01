f472c8d3b940604ea4132330a611a804b89c347b
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 21:52:13 +0200

    697 - degree of an array

---
+++ src/easy/arrays/697-degree-of-an-array.test.ts
@@ -0,0 +1,27 @@
+export {};
+
+const createCounter = (nums: number[]) => {
+  const counter: Record<number,number> = {};
+  for (const num of nums) counter[num] = (counter[num] || 0) + 1;
+  return counter;
+};
+
+const findShortestSubArray = (nums: number[]): number => {
+  let counter = createCounter(nums);
+  let max = Math.max(...Object.values(counter));
+
+  const hasSameDegree = ([, degree]: [string, number]) => degree === max;
+  const indices = ([num]: [string, number]) => nums.lastIndexOf(+num) - nums.indexOf(+num) + 1;
+
+  return Math.min(...Object.entries(counter).filter(hasSameDegree).map(indices));
+};
+
+describe("find shortest subarray", () => {
+  it("case 1", () => {
+    expect(findShortestSubArray([1, 2, 2, 3, 1])).toEqual(2);
+  });
+
+  it("case 2", () => {
+    expect(findShortestSubArray([1, 2, 2, 3, 1, 4, 2])).toEqual(6);
+  });
+});
