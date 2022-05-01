1b6b04e5e0cf8ef6546328e2bd7bef5d0a8a932e
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 21:13:15 +0200

    671 - second minimum node in a binary tree

---
+++ src/easy/trees/binary-trees/671-second-minimum-node-in-a-binary-tree.test.ts
@@ -0,0 +1,28 @@
+import { TreeNode } from "shared/structures";
+import node = TreeNode.node;
+
+const findSecondMinimumValue = (root: TreeNode | null): number => {
+  if (!root) return -1;
+
+  const result = [];
+  const stack = [root];
+  while (stack.length) {
+    const node = stack.pop()!;
+    result.push(node.val);
+    if (node.left) stack.push(node.left);
+    if (node.right) stack.push(node.right);
+  }
+  result.sort((a, b) => a - b);
+  const secondMin = [...new Set(result)][1];
+  return secondMin === undefined ? -1 : secondMin;
+};
+
+describe("find second minimum value", () => {
+  it("case 1", () => {
+    expect(findSecondMinimumValue(node([2, 2, 5, null, null, 5, 7]))).toEqual(5);
+  });
+
+  it("case 2", () => {
+    expect(findSecondMinimumValue(node([2, 2, 2]))).toEqual(-1);
+  });
+});
