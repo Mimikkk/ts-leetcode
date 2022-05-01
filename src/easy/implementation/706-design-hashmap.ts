63050ee886f081ee536e0803c853109006712ec6
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 22:12:55 +0200

    706 - design hashmap

---
+++ src/easy/implementation/706-design-hashmap.ts
@@ -0,0 +1,9 @@
+export {};
+
+class MyHashMap {
+  private readonly hash: number[] = new Array(1e6 + 1).fill(-1);
+
+  put = (key: number, value: number) => this.hash[key] = value;
+  get = (key: number) => this.hash[key];
+  remove = (key: number) => this.hash[key] = -1;
+}
