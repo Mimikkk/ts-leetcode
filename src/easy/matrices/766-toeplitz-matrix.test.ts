8eb107758c55153c09e1795a6eca38e8e6d175b9
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 23:29:48 +0200

    766 - toeplitz matrix

---
+++ src/easy/matrices/766-toeplitz-matrix.test.ts
@@ -0,0 +1,35 @@
+export {};
+
+const isToeplitzMatrix = (matrix: number[][]): boolean => {
+  const isToeplitzDiagonal = (i: number, j: number) => {
+    const diagonal = matrix[i][j];
+    while (matrix[++i]?.[++j] === diagonal) {}
+    return matrix[i]?.[j] === undefined;
+  };
+
+  const [m, n] = [matrix.length, matrix[0].length];
+  for (let x = 0; x < m; x++) {
+    for (let y = 0; y < n; y++) {
+      if (matrix[x - 1]?.[y - 1] === undefined && !isToeplitzDiagonal(x, y)) return false;
+    }
+  }
+
+  return true;
+};
+
+describe("is toeplitz matrix", () => {
+  it("case 1", () => {
+    expect(isToeplitzMatrix([
+      [1, 2, 3, 4],
+      [5, 1, 2, 3],
+      [9, 5, 1, 2],
+    ])).toEqual(true);
+  });
+
+  it("case 2", () => {
+    expect(isToeplitzMatrix([
+      [1, 2],
+      [2, 2],
+    ])).toEqual(false);
+  });
+});
