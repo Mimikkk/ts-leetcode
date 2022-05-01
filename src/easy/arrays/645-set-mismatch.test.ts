a50a7a4c9a1fe87b3fac488f9ec8fff04b8ce259
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 13:12:10 +0200

    645 - set mismatch

---
+++ src/easy/arrays/645-set-mismatch.test.ts
@@ -0,0 +1,30 @@
+export {};
+
+const findErrorNums = (nums: number[]): number[] => {
+  const visited: Record<number, boolean> = {};
+  const expected = (nums.length * (nums.length + 1)) / 2;
+
+  let repeat = 0;
+  let actual = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] in visited) repeat = nums[i];
+
+    else {
+      visited[nums[i]] = true;
+      actual += nums[i];
+    }
+  }
+
+  return [repeat, expected - actual];
+};
+
+describe("find error numbers", () => {
+  it("should pass the first test", () => {
+    expect(findErrorNums([1, 2, 2, 4])).toEqual([2, 3]);
+  });
+
+  it("should pass the second test", () => {
+    expect(findErrorNums([1, 1])).toEqual([1, 2]);
+  });
+});
