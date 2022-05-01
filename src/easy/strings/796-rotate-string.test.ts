fe651d59d00298918bd7a8153186e641ae9fa8fe
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 22:58:50 +0200

    796 - rotate string

---
+++ src/easy/strings/796-rotate-string.test.ts
@@ -0,0 +1,28 @@
+export {};
+
+const shift = (s: string) => s.slice(1) + s[0];
+const rotateString = (s: string, goal: string): boolean => {
+  if (s.length !== goal.length) return false;
+
+  let shiftsLeft = s.length;
+  while (shiftsLeft--) {
+    if (s === goal) return true;
+    s = shift(s);
+  }
+
+  return false;
+};
+
+describe("rotate string", () => {
+  it("case 1", () => {
+    expect(rotateString("abcde", "cdeab")).toBeTruthy();
+  });
+
+  it("case 2", () => {
+    expect(rotateString("abcde", "abced")).toBeFalsy();
+  });
+
+  it("case 3", () => {
+    expect(rotateString("abcde", "abcde")).toBeTruthy();
+  });
+});
