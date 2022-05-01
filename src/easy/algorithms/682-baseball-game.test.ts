38017cdb200c851dddd26d6f345f3864a24954bc
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 21:14:07 +0200

    682 - baseball game

---
+++ src/easy/algorithms/682-baseball-game.test.ts
@@ -0,0 +1,37 @@
+export {};
+
+const calPoints = (ops: string[]): number => {
+  const stack: number[] = [];
+
+  for (const op of ops) {
+    switch (op) {
+      case "+":
+        stack.push(stack[stack.length - 1] + stack[stack.length - 2]);
+        break;
+      case "D":
+        stack.push(stack[stack.length - 1] << 1);
+        break;
+      case "C":
+        stack.pop();
+        break;
+      default:
+        stack.push(+op);
+    }
+  }
+
+  return stack.reduce((a, b) => a + b, 0);
+};
+
+describe("baseball game", () => {
+  it("case 1", () => {
+    expect(calPoints(["5", "2", "C", "D", "+"])).toEqual(30);
+  });
+
+  it("case 2", () => {
+    expect(calPoints(["5", "-2", "4", "C", "D", "9", "+", "+"])).toEqual(27);
+  });
+
+  it("case 3", () => {
+    expect(calPoints(["1", "C"])).toEqual(0);
+  });
+});
