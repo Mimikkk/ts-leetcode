235bb64b30ad20a7fee451b68263348d6163744e
Hououin Kyouma <inwazzin@gmail.com>
2022-05-02 01:32:15 +0200

    748 - shorest completing word

---
+++ src/easy/strings/words/748-shorest-completing-word.test.ts
@@ -0,0 +1,24 @@
+export {};
+
+const shortestCompletingWord = (licensePlate: string, words: string[]): string => {
+  licensePlate = licensePlate.toLowerCase().replace(/[ 0-9]/g, "");
+  words.sort((a, b) => a.length - b.length);
+
+  for (let word of words) {
+    let plate = licensePlate;
+    for (let char of word) plate = plate.replace(char, "");
+    if (!plate) return word;
+  }
+
+  return "";
+};
+
+describe("shortest completing word", () => {
+  it("case 1", () => {
+    expect(shortestCompletingWord("1s3 PSt", ["step", "steps", "stripe", "stepple"])).toBe("steps");
+  });
+
+  it("case 2", () => {
+    expect(shortestCompletingWord("1s3 456", ["looks", "pest", "stew", "show"])).toBe("pest");
+  });
+});
