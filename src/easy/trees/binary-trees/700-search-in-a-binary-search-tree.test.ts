6f73a7154f8ff1c3e42fe8b1edce2abc4c583d51
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 21:52:22 +0200

    700 - search in a binary search tree

---
+++ src/easy/trees/binary-trees/700-search-in-a-binary-search-tree.test.ts
@@ -0,0 +1,29 @@
+import { TreeNode } from "shared/structures";
+import node = TreeNode.node;
+
+const searchBST = (root: TreeNode | null, target: number): TreeNode | null => {
+  const stack = [root];
+
+  while (stack.length) {
+    const node = stack.pop()!;
+
+    if (node) {
+      if (node.val === target) return node;
+
+      if (node.val > target) stack.push(node.left);
+      else stack.push(node.right);
+    }
+  }
+
+  return null;
+};
+
+describe("search bst", () => {
+  it("case 1", () => {
+    expect(searchBST(node([4, 2, 7, 1, 3]), 2)).toEqual(node([2, 1, 3]));
+  });
+
+  it("case 2", () => {
+    expect(searchBST(node([4, 2, 1, 3, 7]), 5)).toEqual(null);
+  });
+});
