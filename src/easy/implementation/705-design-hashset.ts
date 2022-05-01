2a312125f2bd1657204f8117dc7bed6636578941
Hououin Kyouma <inwazzin@gmail.com>
2022-05-01 22:12:46 +0200

    705 - design hashset

---
+++ src/easy/implementation/705-design-hashset.ts
@@ -0,0 +1,9 @@
+export {};
+
+class MyHashSet {
+  private readonly hash: boolean[] = new Array(1e6 + 1).fill(false);
+
+  add = (key: number) => this.hash[key] = true;
+  remove = (key: number) => this.hash[key] = false;
+  contains = (key: number) => this.hash[key];
+}
