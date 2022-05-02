f3960f21887fe8d39c34ee2e2ec71965f20d1f0b
Hououin Kyouma <inwazzin@gmail.com>
2022-05-02 02:56:02 +0200

    717 - 1-bit and 2-bit characters

---
+++ src/easy/binary/717-1-bit-and-2-bit-characters.test.ts
@@ -0,0 +1,20 @@
+export {};
+
+const isOneBitCharacter = (bits: number[]): boolean => {
+  for (let i = 0; i < bits.length; i++) {
+    if (i == bits.length - 1) return true;
+
+    if (bits[i] == 1) ++i;
+  }
+  return false;
+};
+
+describe("is one bit character", () => {
+  it("case 1", () => {
+    expect(isOneBitCharacter([1, 0, 0])).toBe(true);
+  });
+
+  it("case 2", () => {
+    expect(isOneBitCharacter([1, 1, 1, 0])).toBe(false);
+  });
+});
