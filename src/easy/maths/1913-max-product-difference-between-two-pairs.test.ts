05ae091b24469ca87aace2bf6a1577631b8cdb01
Hououin Kyouma <inwazzin@gmail.com>
2022-05-03 01:23:24 +0200

    1913 - max product difference between two pairs

---
+++ src/easy/maths/1913-max-product-difference-between-two-pairs.test.ts
@@ -0,0 +1,16 @@
+export {};
+
+const maxProductDifference = (nums: number[]): number => {
+  nums.sort((a, b) => a - b);
+  return nums[nums.length - 1] * nums[nums.length - 2] - nums[0] * nums[1];
+};
+
+describe("max product difference", () => {
+  it("should pass", () => {
+    expect(maxProductDifference([5, 6, 2, 7, 4])).toEqual(34);
+  });
+
+  it("should pass", () => {
+    expect(maxProductDifference([4, 2, 5, 9, 7, 4, 8])).toEqual(64);
+  });
+});
