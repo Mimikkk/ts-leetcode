e872359fc62dcba3ffdddd0ccb3977a389062212
Hououin Kyouma <inwazzin@gmail.com>
2022-05-02 02:56:09 +0200

    724 - find pivot index

---
+++ src/easy/numbers/maths/724-find-pivot-index.test.ts
@@ -0,0 +1,26 @@
+export {};
+
+const sum = (...nums: number[]) => nums.reduce((a, b) => a + b, 0);
+const pivotIndex = (nums: number[]): number => {
+  let [left, right] = [0, sum(...nums)];
+
+  return nums.findIndex((num) => {
+    if (right - left === num) return true;
+    left += num;
+    right -= num;
+  });
+};
+
+describe("pivot index", () => {
+  it("case 1", () => {
+    expect(pivotIndex([1, 7, 3, 6, 5, 6])).toEqual(3);
+  });
+
+  it("case 2", () => {
+    expect(pivotIndex([1, 2, 3])).toEqual(-1);
+  });
+
+  it("case 3", () => {
+    expect(pivotIndex([2, 1, -1])).toEqual(0);
+  });
+});
