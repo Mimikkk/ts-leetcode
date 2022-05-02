cecef66865dfa6df6aa12a3a502b6548ae5cc282
Hououin Kyouma <inwazzin@gmail.com>
2022-05-02 02:56:30 +0200

    1991 - find the middle index in array

---
+++ src/easy/numbers/maths/1991-find-the-middle-index-in-array.test.ts
@@ -0,0 +1,26 @@
+export {};
+
+const zip = (a: number[], b: number[]): [number, number][] => a.map((x, i) => [x, b[i]]);
+const prefixsum = (nums: number[]) => {
+  let sum = 0;
+  return nums.map(n => sum += n);
+};
+const equal = ([a, b]: [number, number]) => a === b;
+
+const findMiddleIndex = (nums: number[]): number =>
+  zip(prefixsum(nums), prefixsum(nums.reverse()).reverse())
+    .findIndex(equal);
+
+describe("find middle index", () => {
+  it("case 1", () => {
+    expect(findMiddleIndex([2, 3, -1, 8, 4])).toEqual(3);
+  });
+
+  it("case 2", () => {
+    expect(findMiddleIndex([1, -1, 4])).toEqual(2);
+  });
+
+  it("case 3", () => {
+    expect(findMiddleIndex([2, 5])).toEqual(-1);
+  });
+});
