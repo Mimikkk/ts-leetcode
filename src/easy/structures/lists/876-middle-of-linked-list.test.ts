f89a562f9d7ab4665dc4ac3f992eba326c028414
Hououin Kyouma <inwazzin@gmail.com>
2022-05-05 02:37:13 +0200

    876 - middle of linked list

---
+++ src/easy/structures/lists/876-middle-of-linked-list.test.ts
@@ -0,0 +1,25 @@
+import { ListNode } from "shared/structures";
+import node = ListNode.node;
+
+const middleNode = (head: ListNode | null): ListNode | null => {
+  if (!head) return null;
+  let slow = head;
+  let fast = head;
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next!;
+    fast = fast.next.next;
+  }
+
+  return fast.next ?slow.next:slow;
+};
+
+describe("middle node", () => {
+  it("case 1", () => {
+    expect(middleNode(node([1, 2, 3, 4, 5]))).toEqual(node([3, 4, 5]));
+  });
+
+  it("case 2", () => {
+    expect(middleNode(node([1, 2, 3, 4, 5, 6]))).toEqual(node([4, 5, 6]));
+  });
+});
