0b964bb2568c85dd481de80a7f6a27d4b75a8743
Hououin Kyouma <inwazzin@gmail.com>
2022-05-06 00:38:07 +0200

    914 - x of a kind in a deck of cards

---
+++ src/easy/algorithms/914-x-of-a-kind-in-a-deck-of-cards.test.ts
@@ -0,0 +1,23 @@
+export {};
+
+const createCounter = (nums: number[]) => {
+  const counter: Record<number,number> = {};
+  nums.forEach(n => counter[n] = (counter[n] || 0) + 1);
+  return counter;
+};
+
+const gcd = (divisor: number, dividend: number): number =>
+  divisor ?gcd(dividend % divisor, divisor):dividend;
+
+const hasGroupsSizeX = (deck: number[]) =>
+  Object.values(createCounter(deck)).reduce(gcd) !== 1;
+
+describe("has groups size", () => {
+  it("case 1", () => {
+    expect(hasGroupsSizeX([1, 2, 3, 4, 4, 3, 2, 1])).toEqual(true);
+  });
+
+  it("case 2", () => {
+    expect(hasGroupsSizeX([1, 1, 1, 2, 2, 2, 3, 3])).toEqual(false);
+  });
+});
