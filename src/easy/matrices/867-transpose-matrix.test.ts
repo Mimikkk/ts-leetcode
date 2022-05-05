01294439cdd7c19a32e97cdb1aabe4d7ea110947
Hououin Kyouma <inwazzin@gmail.com>
2022-05-05 02:36:49 +0200

    867 - transpose matrix

---
+++ src/easy/matrices/867-transpose-matrix.test.ts
@@ -0,0 +1,25 @@
+export {};
+
+const mat = <T>(m: number, n: number, fill: T): T[][] => Array.from(Array(m), () => Array(n).fill(fill));
+
+const transpose = (matrix: number[][]): number[][] => {
+  const [m, n] = [matrix.length, matrix[0].length];
+
+  const result = mat(m, n, 0);
+  for (let x = 0; x < m; ++x) for (let y = 0; y < n; ++y) result[y][x] = matrix[x][y];
+  return result;
+};
+
+describe("transpose", () => {
+  it("case 1", () => {
+    expect(transpose([
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ])).toEqual([
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+    ]);
+  });
+});
