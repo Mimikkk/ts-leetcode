3264eca1047ff1c25d73c665c3315d3d7072b4d1
Hououin Kyouma <inwazzin@gmail.com>
2022-05-05 02:36:42 +0200

    860 - lemonade change

---
+++ src/easy/numbers/algorithms/860-lemonade-change.test.ts
@@ -0,0 +1,35 @@
+export {};
+
+const lemonadeChange = (bills: number[]): boolean => {
+  const change = { 5:0, 10:0 };
+
+  return !bills.some((bill) => {
+    switch (bill) {
+      case 5:
+        ++change[5];
+        break;
+      case 10:
+        if (!change[5]) return true;
+        --change[5];
+        ++change[10];
+        break;
+      case 20:
+        if (change[10] > 0 && change[5] > 0) {
+          --change[10];
+          --change[5];
+        } else if (change[5] >= 3) change[5] -= 3;
+        else return true;
+        break;
+    }
+  });
+};
+
+describe("lemonade change", () => {
+  it("case 1", () => {
+    expect(lemonadeChange([5, 5, 5, 10, 20])).toBe(true);
+  });
+
+  it("case 2", () => {
+    expect(lemonadeChange([5, 5, 10, 10, 20])).toBe(false);
+  });
+});
