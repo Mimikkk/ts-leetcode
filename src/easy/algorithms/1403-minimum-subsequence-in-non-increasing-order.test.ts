7b4519211c23ebd7fedc7d2ec70f3bb37189ca24
Hououin Kyouma <inwazzin@gmail.com>
2022-05-05 03:54:48 +0200

    1403 - minimum subsequence in non increasing order

---
+++ src/easy/algorithms/1403-minimum-subsequence-in-non-increasing-order.test.ts
@@ -0,0 +1,23 @@
+export {};
+
+const add = (arr: number[]) => arr.reduce((a, b) => a + b) ?? NaN;
+const desc = (a: number, b: number) => b - a;
+
+const minSubsequence = (nums: number[]): number[] => {
+  const sum = add(nums);
+
+  let bSum = 0;
+  nums.sort(desc);
+  const i = nums.findIndex((n) => (bSum += n) > sum / 2);
+  return nums.slice(0, i + 1);
+};
+
+describe("min seq", () => {
+  it("case 1", () => {
+    expect(minSubsequence([4, 3, 10, 9, 8])).toEqual([10, 9]);
+  });
+
+  it("case 2", () => {
+    expect(minSubsequence([4, 4, 7, 6, 7])).toEqual([7, 7, 6]);
+  });
+});
