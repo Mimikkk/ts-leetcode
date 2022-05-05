19a08e58f617f905bb9fd818acd618d0f7b9ed62
Hououin Kyouma <inwazzin@gmail.com>
2022-05-05 02:37:49 +0200

    1013 - partition array into three equal sum parts

---
+++ src/easy/numbers/algorithms/1013-partition-array-into-three-equal-sum-parts.test.ts
@@ -0,0 +1,33 @@
+export {};
+
+const canThreePartsEqualSum = (nums: number[]): boolean => {
+  const sum = nums.reduce((a, b) => a + b);
+  if (sum % 3) return false;
+
+  let count = 0;
+  let partialSum = 0;
+  for (let num of nums) {
+    partialSum += num;
+
+    if (sum / 3 == partialSum) {
+      ++count;
+      partialSum = 0;
+    }
+  }
+
+  return count >= 3;
+};
+
+describe("can three parts equal sum", () => {
+  it("case 1", () => {
+    expect(canThreePartsEqualSum([0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1])).toEqual(true);
+  });
+
+  it("case 2", () => {
+    expect(canThreePartsEqualSum([0, 2, 1, -6, 6, 7, 9, -1, 2, 0, 1])).toEqual(false);
+  });
+
+  it("case 3", () => {
+    expect(canThreePartsEqualSum([3, 3, 6, 5, -2, 2, 5, 1, -9, 4])).toEqual(true);
+  });
+});
