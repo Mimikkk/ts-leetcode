42e7c46449bea6e417546a34cf6d5aa0fd2488a8
Hououin Kyouma <inwazzin@gmail.com>
2022-05-05 02:36:16 +0200

    852 - peak index in a mountain array

---
+++ src/easy/maths/852-peak-index-in-a-mountian-array.test.ts
@@ -0,0 +1,16 @@
+export {};
+
+const isPeak = (num: number, i: number, nums: number[]) => num > nums[i + 1] && num > nums[i - 1];
+
+const peakIndexInMountainArray = (nums: number[]): number =>
+  nums.findIndex(isPeak);
+
+describe("peak index in mountain array", () => {
+  it("case 1", () => {
+    expect(peakIndexInMountainArray([0, 1, 0])).toEqual(1);
+  });
+
+  it("case 2", () => {
+    expect(peakIndexInMountainArray([0, 2, 1, 0])).toEqual(1);
+  });
+});
