ba5137073f5643d3c9504bd70a43cd4c0afda611
Hououin Kyouma <inwazzin@gmail.com>
2022-05-05 03:54:09 +0200

    888 - fair candy swap

---
+++ src/easy/algorithms/888-fair-candy-swap.test.ts
@@ -0,0 +1,29 @@
+export {};
+
+const sum = (arr: number[]) => arr.reduce((a, b) => a + b) ?? NaN;
+const fairCandySwap = (aliceSizes: number[], bobSizes: number[]): number[] => {
+  const aliceSum = sum(aliceSizes);
+  const bobSum = sum(bobSizes);
+
+  const diff = (aliceSum - bobSum) / 2;
+  for (let a of aliceSizes) {
+    for (let b of bobSizes) {
+      if (a !== b && a - b === diff) return [a, b];
+    }
+  }
+  return [];
+};
+
+describe("fair candy swap", () => {
+  it("case 1", () => {
+    expect(fairCandySwap([1, 1], [2, 2])).toEqual([1, 2]);
+  });
+
+  it("case 2", () => {
+    expect(fairCandySwap([1, 2], [2, 3])).toEqual([1, 2]);
+  });
+
+  it("case 3", () => {
+    expect(fairCandySwap([2], [1, 3])).toEqual([2, 3]);
+  });
+});
