9486b15a597aeac6a55e2161300ae38f5bd106c9
Hououin Kyouma <inwazzin@gmail.com>
2022-05-05 02:36:57 +0200

    868 - binary gap

---
+++ src/easy/binary/868-binary-gap.test.ts
@@ -0,0 +1,27 @@
+export {};
+
+const binaryGap = (n: number) => {
+  let [last, result] = [-1, 0];
+
+  let i = 0;
+  while (n > 0) {
+      if (n & 1) {
+        if (last >= 0) result = Math.max(result, i - last);
+        last = i;
+      }
+    n >>= 1;
+    ++i;
+  }
+
+  return result;
+};
+
+describe("binary gap", () => {
+  it("case 1", () => {
+    expect(binaryGap(8)).toEqual(0);
+  });
+
+  it("case 2", () => {
+    expect(binaryGap(5)).toEqual(2);
+  });
+});
