4fd70454dfb3f9ee8b1ad4c6bc617d93c554a9eb
Hououin Kyouma <inwazzin@gmail.com>
2022-05-05 02:39:24 +0200

    2085 - count common words with one occurence

---
+++ src/easy/strings/words/2085-count-common-words-with-one-occurence.test.ts
@@ -0,0 +1,32 @@
+export {};
+
+const createCounter = (words: string[]) => {
+  const counter: Record<string, number> = {};
+  for (const word of words) counter[word] = (counter[word] || 0) + 1;
+  return counter;
+};
+
+const countWords = (w1: string[], w2: string[]): number => {
+  const [c1, c2] = [w1, w2].map(createCounter);
+
+  const once = (w: string) => c1[w] === 1 && c2[w] === 1;
+  return Object.keys(c1).filter(once).length;
+};
+
+describe("count words", () => {
+  it("case 1", () => {
+    expect(countWords(["a", "b", "c", "d", "a"], ["a", "b", "c"])).toEqual(2);
+  });
+
+  it("case 2", () => {
+    expect(countWords(["b", "bb", "bbb"], ["a", "aa", "aaa"])).toEqual(0);
+  });
+
+  it("case 3", () => {
+    expect(countWords(["a", "ab"], ["a", "a", "a", "ab"])).toEqual(1);
+  });
+
+  it("case 4", () => {
+    expect(countWords(["leetcode", "is", "amazing", "as", "is"], ["amazing", "leetcode", "is"])).toEqual(2);
+  });
+});
