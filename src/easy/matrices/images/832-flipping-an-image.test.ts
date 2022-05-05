b562173b17d7961d5775a49518d2567f6b822475
Hououin Kyouma <inwazzin@gmail.com>
2022-05-05 02:36:33 +0200

    832 - flipping an image

---
+++ src/easy/matrices/images/832-flipping-an-image.test.ts
@@ -0,0 +1,34 @@
+export {};
+
+const reverse = (arr: number[]) => arr.reverse();
+const invert = (arr: number[]) => arr.map(x => x ^ 1);
+const flipAndInvertImage = (image: number[][]): number[][] =>
+  image.map(reverse).map(invert);
+
+describe("flip and invert image", () => {
+  it("case 1", () => {
+    expect(flipAndInvertImage([
+      [1, 1, 0],
+      [1, 0, 1],
+      [0, 0, 0],
+    ])).toEqual([
+      [1, 0, 0],
+      [0, 1, 0],
+      [1, 1, 1],
+    ]);
+  });
+
+  it("case 2", () => {
+    expect(flipAndInvertImage([
+      [1, 1, 0, 0],
+      [1, 0, 0, 1],
+      [0, 1, 1, 1],
+      [1, 0, 1, 0],
+    ])).toEqual([
+      [1, 1, 0, 0],
+      [0, 1, 1, 0],
+      [0, 0, 0, 1],
+      [1, 0, 1, 0],
+    ]);
+  });
+});
