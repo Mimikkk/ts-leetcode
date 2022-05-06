900a23ddeb4665e91d7f6b720c7bd1a984ff2d69
Hououin Kyouma <inwazzin@gmail.com>
2022-05-06 02:02:56 +0200

    2932 - two out of three

---
+++ src/easy/arrays/algorithms/2932-two-out-of-three.test.ts
@@ -0,0 +1,13 @@
+export {};
+
+const common = (...arr: number[][]) => [...new Set(arr.flat())];
+
+const twoOutOfThree = (n1: number[], n2: number[], n3: number[]) =>
+  common(n1, n2, n3)
+    .filter(x => +n1.includes(x) + +n2.includes(x) + +n3.includes(x) > 1);
+
+describe("two out of three", () => {
+  it("case 1", () => {
+    expect(twoOutOfThree([1, 2, 3], [2, 3, 4], [3, 4, 5])).toEqual([2, 3, 4]);
+  });
+});
