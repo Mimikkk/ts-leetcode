6dc3ff6c3abb6f4182089405e6ef6349ec650469
Hououin Kyouma <inwazzin@gmail.com>
2022-05-06 17:17:46 +0200

    989 - add to array from of integer

---
+++ src/easy/arrays/algorithms/989-add-to-array-from-of-integer.test.ts
@@ -0,0 +1,32 @@
+export {};
+
+const divmod = (n: number, mod: number) => [~~(n / mod), n % mod];
+
+const addToArrayForm = (num: number[], k: number) => {
+  let mod;
+  const result = [];
+  while (k > 0 || num.length) {
+    [k, mod] = divmod((num.pop() || 0) + k, 10);
+    result.push(mod);
+  }
+
+  return result.reverse();
+};
+
+describe("add to array form", () => {
+  it("case 1", () => {
+    expect(addToArrayForm([1, 2, 0, 0], 34)).toEqual([1, 2, 3, 4]);
+  });
+
+  it("case 2", () => {
+    expect(addToArrayForm([2, 7, 4], 181)).toEqual([4, 5, 5]);
+  });
+
+  it("case 3", () => {
+    expect(addToArrayForm([0], 181)).toEqual([1, 8, 1]);
+  });
+
+  it("case 4", () => {
+    expect(addToArrayForm([9, 9, 9, 9, 9, 9, 9, 9, 9, 9], 1)).toEqual([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  });
+});
