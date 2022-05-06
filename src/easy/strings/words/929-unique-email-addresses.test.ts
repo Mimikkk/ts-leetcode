3b30bd39cfbccf41377f9b0a9c23c3c6c5372e8b
Hououin Kyouma <inwazzin@gmail.com>
2022-05-06 02:01:50 +0200

    929 - unique email addresses

---
+++ src/easy/strings/words/929-unique-email-addresses.test.ts
@@ -0,0 +1,33 @@
+export {};
+
+const unique = (items: string[]) => [...new Set(items)];
+const removeDots = (s: string) => s.replace(/\./g, "");
+
+const makeCommon = (email: string) => {
+  let [local, domain] = email.split("@");
+  if (local.includes("+")) local = local.substring(0, local.indexOf("+"));
+
+  return `${removeDots(local)}@${domain}`;
+};
+
+
+const numUniqueEmails = (emails: string[]) =>
+  unique(emails.map(makeCommon)).length;
+
+describe("num unique emails", () => {
+  it("case 1", () => {
+    expect(numUniqueEmails([
+      "test.email+alex@leetcode.com",
+      "test.e.mail+bob.cathy@leetcode.com",
+      "testemail+david@lee.tcode.com",
+    ])).toEqual(2);
+  });
+
+  it("case 2", () => {
+    expect(numUniqueEmails([
+      "a@leetcode.com",
+      "b@leetcode.com",
+      "c@leetcode.com",
+    ])).toEqual(3);
+  });
+});
