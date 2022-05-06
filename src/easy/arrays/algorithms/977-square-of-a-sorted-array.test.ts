4d37d701dfa44c81f71735893a412bbf7bbb8373
Hououin Kyouma <inwazzin@gmail.com>
2022-05-06 17:17:36 +0200

    977 - square of a sorted array

---
+++ src/easy/arrays/algorithms/977-square-of-a-sorted-array.test.ts
@@ -0,0 +1,16 @@
+export {};
+
+const asc = (a: number, b: number) => a - b;
+const square = (a: number) => a * a;
+const sortedSquares = (nums: number[]) => nums.map(square).sort(asc);
+
+
+describe("sorted squares", () => {
+  it("case 1", () => {
+    expect(sortedSquares([-4, -1, 0, 3, 10])).toEqual([0, 1, 9, 16, 100]);
+  });
+
+  it("case 2", () => {
+    expect(sortedSquares([-7, -3, 2, 3, 11])).toEqual([4, 9, 9, 49, 121]);
+  });
+});
