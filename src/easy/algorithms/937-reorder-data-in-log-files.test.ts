0a2dc016561631d4c9274c5bfc89889baf07095e
Hououin Kyouma <inwazzin@gmail.com>
2022-05-06 02:02:11 +0200

    937 - reorder data in log files

---
+++ src/easy/algorithms/937-reorder-data-in-log-files.test.ts
@@ -0,0 +1,36 @@
+export {};
+
+const splitLog = (log: string) => {
+  const firstSpaceIdx = log.indexOf(" ");
+  const identifier = log.slice(0, firstSpaceIdx);
+  const content = log.slice(firstSpaceIdx + 1);
+
+  return [identifier, content, content[0]];
+};
+
+const comparator = (a: string, b: string) => {
+  const [identifierA, contentA, firstcharA] = splitLog(a);
+  const [identifierB, contentB, firstcharB] = splitLog(b);
+
+  if (firstcharA <= "9")
+    if (firstcharB <= "9") return 0;
+  else return 1;
+  if (firstcharB <= "9") return -1;
+
+  const comparison = contentA.localeCompare(contentB);
+  return comparison === 0 ?identifierA.localeCompare(identifierB):comparison;
+};
+
+const reorderLogFiles = (logs: string[]) => logs.sort(comparator);
+
+describe("reorder log files", () => {
+  it("case 1", () => {
+    expect(reorderLogFiles(["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]))
+      .toEqual(["let1 art can", "let3 art zero", "let2 own kit dig", "dig1 8 1 5 1", "dig2 3 6"]);
+  });
+
+  it("case 2", () => {
+    expect(reorderLogFiles(["a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"]))
+      .toEqual(["g1 act car", "a8 act zoo", "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7"]);
+  });
+});
