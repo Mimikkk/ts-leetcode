d0b2342d6ea11d0f04e2d70690943e843fd20361
Hououin Kyouma <inwazzin@gmail.com>
2022-05-06 02:02:35 +0200

    942 - di string match

---
+++ src/easy/strings/single/942-di-string-match.test.ts
@@ -0,0 +1,28 @@
+export {};
+
+const diStringMatch = (s: string): number[] => {
+  let [first, last] = [0, s.length];
+
+  let result = [];
+  for (let char of s) {
+    if (char === "I") result.push(first++);
+    else if (char === "D") result.push(last--);
+  }
+  result.push(first);
+
+  return result;
+};
+
+describe("di string match", () => {
+  it("case 1", () => {
+    expect(diStringMatch("IDID")).toEqual([0, 4, 1, 3, 2]);
+  });
+
+  it("case 2", () => {
+    expect(diStringMatch("III")).toEqual([0, 1, 2, 3]);
+  });
+
+  it("case 3", () => {
+    expect(diStringMatch("DDI")).toEqual([3, 2, 0, 1]);
+  });
+});
