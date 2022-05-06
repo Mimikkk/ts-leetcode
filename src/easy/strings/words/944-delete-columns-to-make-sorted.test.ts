9f1e797aa4616b8f51c44a7a026633bf0b4319eb
Hououin Kyouma <inwazzin@gmail.com>
2022-05-06 17:16:53 +0200

    944 - delete columns to make sorted

---
+++ src/easy/strings/words/944-delete-columns-to-make-sorted.test.ts
@@ -0,0 +1,31 @@
+export {};
+
+const minDeletionSize = (strs: string[]) => {
+  let counter = 0;
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] < strs[j - 1][i]) {
+        counter++;
+        break;
+      }
+    }
+  }
+  return counter;
+};
+
+describe("delete columns to make sorted", () => {
+  it("case 1", () => {
+    expect(minDeletionSize(["cba", "daf", "ghi"])).toEqual(1);
+  });
+  it("case 2", () => {
+    expect(minDeletionSize(["cb", "da", "gh"])).toEqual(1);
+  });
+
+  it("case 3", () => {
+    expect(minDeletionSize(["rrjk", "furt", "guzm"])).toEqual(2);
+  });
+
+  it("case 4", () => {
+    expect(minDeletionSize(["a", "b"])).toEqual(0);
+  });
+});
