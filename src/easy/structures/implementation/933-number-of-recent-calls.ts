b94ac8586ab409b34c6e456bb1c9004dfeaba58e
Hououin Kyouma <inwazzin@gmail.com>
2022-05-06 02:02:02 +0200

    933 - number of recent calls

---
+++ src/easy/structures/implementation/933-number-of-recent-calls.ts
@@ -0,0 +1,16 @@
+export {};
+
+const MaxLatency = 3000;
+class RecentCounter {
+  queue: number[] = [];
+
+  constructor() {
+    this.queue = [];
+  }
+
+  ping(timestamp: number) {
+    this.queue.push(timestamp);
+    while(this.queue[0] < timestamp - MaxLatency) this.queue.shift();
+    return this.queue.length;
+  }
+}
