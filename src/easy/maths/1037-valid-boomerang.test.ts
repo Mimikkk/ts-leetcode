5334ea71afb384280ccbc8a74503280f3ffbb250
Hououin Kyouma <inwazzin@gmail.com>
2022-05-07 16:11:12 +0200

    1037 - valid boomerang

---
+++ src/easy/maths/1037-valid-boomerang.test.ts
@@ -0,0 +1,18 @@
+export {};
+
+type Position = [number, number];
+const cross = ([x1, y1]: Position, [x2, y2]: Position) => x1 * y2 - x2 * y1;
+const collinear = ([x1, y1]: Position, [x2, y2]: Position, [x3, y3]: Position) =>
+  cross([x2 - x1, y2 - y1], [x3 - x1, y3 - y1]) === 0;
+
+const isBoomerang = (points: [Position, Position, Position]) => !collinear(...points);
+
+describe("valid boomerang", () => {
+  it("case 1", () => {
+    expect(isBoomerang([[1, 1], [2, 3], [3, 2]])).toBe(true);
+  });
+
+  it("case 2", () => {
+    expect(isBoomerang([[1, 1], [2, 2], [3, 3]])).toBe(false);
+  });
+});
