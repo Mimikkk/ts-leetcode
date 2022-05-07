de3ac5fa3dc3b69068b542e97a4fb9f3eb115459
Hououin Kyouma <inwazzin@gmail.com>
2022-05-07 16:11:27 +0200

    1290 - convert binary number in a linked list to integer

---
+++ src/easy/structures/lists/1290-convert-binary-number-in-a-linked-list-to-integer.test.ts
@@ -0,0 +1,41 @@
+import { ListNode } from "shared/structures";
+const { node } = ListNode;
+
+const getDecimalValue = (node: ListNode | null): number => {
+  let sum = 0;
+  while (node) {
+    sum = (sum << 1) + node.val;
+    node = node.next;
+  }
+  return sum;
+};
+
+describe("234 - palindrome linked list", () => {
+  it("case 1", () => {
+    expect(getDecimalValue(node([1, 0, 1]))).toEqual(5);
+  });
+
+  it("case 2", () => {
+    expect(getDecimalValue(node([1]))).toEqual(1);
+  });
+
+  it("case 3", () => {
+    expect(getDecimalValue(node([1, 0]))).toEqual(2);
+  });
+
+  it("case 4", () => {
+    expect(getDecimalValue(node([0]))).toEqual(0);
+  });
+
+  it("case 5", () => {
+    expect(getDecimalValue(node([0, 0, 0]))).toEqual(0);
+  });
+
+  it("case 6", () => {
+    expect(getDecimalValue(node([0, 0, 1]))).toEqual(1);
+  });
+
+  it("case 7", () => {
+    expect(getDecimalValue(node([1, 0, 0]))).toEqual(4);
+  });
+});
