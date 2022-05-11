0371299456b9f883b39b4b21df74f89727b4b403
Hououin Kyouma <inwazzin@gmail.com>
2022-05-11 03:07:46 +0200

    1071 - greatest common divisor of strings

---
+++ src/easy/strings/algorithms/1071-greatest-common-divisor-of-strings.test.ts
@@ -0,0 +1,30 @@
+export {};
+
+const gcd = (x: number, y: number): number => !(y % x) ?x:gcd(y % x, x);
+
+const gcdOfStrings = (str1: string, str2: string): string =>
+  str1 + str2 === str2 + str1
+    ?str1.slice(0, gcd(str1.length, str2.length))
+    :"";
+
+describe("gcd of strings", () => {
+  it("case 1", () => {
+    expect(gcdOfStrings("ABABAB", "ABABAB")).toBe("ABABAB");
+  });
+
+  it("case 2", () => {
+    expect(gcdOfStrings("ABABAB", "ABAB")).toBe("AB");
+  });
+
+  it("case 3", () => {
+    expect(gcdOfStrings("", "")).toBe("");
+  });
+
+  it("case 4", () => {
+    expect(gcdOfStrings("", "A")).toBe("");
+  });
+
+  it("case 5", () => {
+    expect(gcdOfStrings("A", "A")).toBe("A");
+  });
+});
