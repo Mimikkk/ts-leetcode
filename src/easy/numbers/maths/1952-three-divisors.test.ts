39ce6ef6cdb7840ba43e3a33e049a2887f7cf3a9
Hououin Kyouma <inwazzin@gmail.com>
2022-05-11 03:08:05 +0200

    1952 - three divisors

---
+++ src/easy/numbers/maths/1952-three-divisors.test.ts
@@ -0,0 +1,21 @@
+export {};
+
+const isThree = (n: number): boolean => {
+  let c = 0;
+  for (let i = 1; i <= n; ++i) if (!(n % i)) ++c;
+  return c === 3;
+};
+
+describe("isThree", () => {
+  it("case 1", () => {
+    expect(isThree(3)).toBe(false);
+  });
+
+  it("case 2", () => {
+    expect(isThree(4)).toBe(true);
+  });
+
+  it("case 3", () => {
+    expect(isThree(7)).toBe(true);
+  });
+});
