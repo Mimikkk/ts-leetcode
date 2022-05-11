2e1f961e654b237037d85bccf0762d35c57da233
Hououin Kyouma <inwazzin@gmail.com>
2022-05-11 03:07:57 +0200

    1979 - greatest common divisor of array

---
+++ src/easy/arrays/algorithms/1979-find-greatest-common-divisor-of-array.test.ts
@@ -0,0 +1,18 @@
+export {};
+
+const gcd = (x: number, y: number): number => !x ?y:gcd(y % x, x);
+const findGCD = (nums: number[]): number => gcd(Math.min(...nums), Math.max(...nums));
+
+describe("gcd of array", () => {
+  it("case 1", () => {
+    expect(findGCD([2, 4, 6, 8])).toBe(2);
+  });
+
+  it("case 2", () => {
+    expect(findGCD([2, 5, 6, 9, 10])).toBe(2);
+  });
+
+  it("case 3", () => {
+    expect(findGCD([3, 9, 27, 81])).toBe(3);
+  });
+});
