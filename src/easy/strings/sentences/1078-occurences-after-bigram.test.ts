1bdb085ef8158b0eaf03c1d1dfa45229a1b29a25
Hououin Kyouma <inwazzin@gmail.com>
2022-05-13 00:57:45 +0200

    1078 - occurences after bigram

---
+++ src/easy/strings/sentences/1078-occurences-after-bigram.test.ts
@@ -0,0 +1,18 @@
+export {};
+
+const range = (start: number, end: number) =>
+  Array.from({ length:end - start }, (_, i) => i + start);
+
+const findOcurrences = (text: string, first: string, second: string): string[] => {
+  const words = text.split(" ");
+
+  return range(0, words.length - 2)
+    .map(i => words.slice(i, i + 3))
+    .filter(([a, b]) => a === first && b === second).map(([, , c]) => c);
+};
+
+describe("find occurences", () => {
+  it("case 1", () => {
+    expect(findOcurrences("alice is a good girl she is a good student", "a", "good")).toEqual(["girl", "student"]);
+  });
+});
