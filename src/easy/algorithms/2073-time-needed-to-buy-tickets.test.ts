fe7d71a55fef7d8448149f610ad29904f3ace8aa
Hououin Kyouma <inwazzin@gmail.com>
2022-05-16 00:48:13 +0200

    2073 - time needed to buy tickets

---
+++ src/easy/algorithms/2073-time-needed-to-buy-tickets.test.ts
@@ -0,0 +1,27 @@
+export {};
+
+const timeRequiredToBuy = (tickets: number[], k: number) => {
+
+  let time = 0;
+  for (let i = 0; i < tickets.length; i++) {
+    if (i === k || tickets[i] < tickets[k]) {
+      time += tickets[i];
+    } else if (i > k) {
+      time += tickets[k] - 1;
+    } else {
+      time += tickets[k];
+    }
+  }
+
+  return time;
+};
+
+describe("time required to buy", () => {
+  it("case 1", () => {
+    expect(timeRequiredToBuy([2, 3, 2], 2)).toEqual(6);
+  });
+
+  it("case 2", () => {
+    expect(timeRequiredToBuy([5, 1, 1, 1], 0)).toEqual(8);
+  });
+});
