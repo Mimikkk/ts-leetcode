96e87913619d24702576743a17f12097e1de4e92
Hououin Kyouma <inwazzin@gmail.com>
2022-05-16 00:18:48 +0200

    1700 - number of students unable to eat

---
+++ src/easy/algorithms/1700-number-of-students-unable-to-eat.test.ts
@@ -0,0 +1,30 @@
+export {};
+
+const countStudents = (students: number[], sandwiches: number[]): number => {
+
+  let count = 0;
+  while (students.length > count) {
+    const student = students.shift()!;
+    const sandwich = sandwiches.shift()!;
+
+    if (student == sandwich) {
+      count = 0;
+    } else {
+      ++count;
+      students.push(student);
+      sandwiches.unshift(sandwich);
+    }
+  }
+
+  return students.length;
+};
+
+describe("count students unable to eat", () => {
+  it("case 1", () => {
+    expect(countStudents([1, 1, 0, 0], [0, 1, 0, 1])).toBe(0);
+  });
+
+  it("case 2", () => {
+    expect(countStudents([1, 1, 1, 0, 0, 1], [1, 0, 0, 0, 1, 1])).toBe(3);
+  });
+});
