4231f382a0079e9395971809776f24151df1edd4
Hououin Kyouma <inwazzin@gmail.com>
2022-05-16 01:56:09 +0200

    1566 - detect pattern of length m repeated k or more times

---
+++ src/easy/numbers/algorithms/1566-detect-pattern-of-length-m-repeated-k-or-more-times.test.ts
@@ -0,0 +1,23 @@
+export {};
+
+const containsPattern = (arr: number[], m: number, k: number): boolean => {
+  for (let i = m, count = 0; i < arr.length; ++i) {
+    if (arr[i] != arr[i - m]) count = 0;
+    else if (++count == m * (k - 1)) return true;
+  }
+  return false;
+};
+
+describe("contains pattern", () => {
+  it("case 1", () => {
+    expect(containsPattern([1, 2, 4, 4, 4, 4], 1, 3)).toBe(true);
+  });
+
+  it("case 2", () => {
+    expect(containsPattern([1, 2, 1, 2, 1, 1, 1, 3], 2, 2)).toBe(true);
+  });
+
+  it("case 3", () => {
+    expect(containsPattern([1, 2, 1, 2, 1, 3], 2, 3)).toBe(false);
+  });
+});
