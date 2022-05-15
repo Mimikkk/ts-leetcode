37938c833cd19de327d2456cd97e8fa9436c7b43
Hououin Kyouma <inwazzin@gmail.com>
2022-05-16 00:18:30 +0200

    1103 - distribute cnadies to people

---
+++ src/easy/algorithms/1103-distribute-candies-to-people.test.ts
@@ -0,0 +1,27 @@
+export {};
+
+const array = (n: number, fill: number = 0) => new Array(n).fill(fill);
+const distributeCandies = (candies: number, num_people: number): number[] => {
+  const dp = array(num_people);
+
+  let i = 0;
+  let count = 1;
+  while (candies > 0) {
+    dp[i] += count;
+    candies -= count;
+    count = count + 1 < candies ?count + 1:candies;
+    i = (i + 1) % num_people;
+  }
+
+  return dp;
+};
+
+describe("distribute", () => {
+  it("case 1", () => {
+    expect(distributeCandies(7, 4)).toEqual([1, 2, 3, 1]);
+  });
+
+  it("case 2", () => {
+    expect(distributeCandies(10, 3)).toEqual([5, 2, 3]);
+  });
+});
