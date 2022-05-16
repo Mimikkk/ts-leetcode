fdaf1846b2f741c3895223b07b02bcf51e6319be
Hououin Kyouma <inwazzin@gmail.com>
2022-05-17 01:33:52 +0200

    1221 - split string in a balanced strings

---
+++ src/easy/strings/single/1221-split-string-in-a-balanced-strings.test.ts
@@ -0,0 +1,32 @@
+export {};
+
+const balancedStringSplit = (s: string): number => {
+  let count = 0;
+  let left = 0;
+  let right = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "L") left++;
+    else right++;
+
+    if (left === right) {
+      left = 0;
+      right = 0;
+      ++count;
+    }
+  }
+  return count;
+};
+
+describe("balanced string split", () => {
+  it("case 1", () => {
+    expect(balancedStringSplit("RLRRLLRLRL")).toEqual(4);
+  });
+
+  it("case 2", () => {
+    expect(balancedStringSplit("RLLLLRRRLR")).toEqual(3);
+  });
+
+  it("case 3", () => {
+    expect(balancedStringSplit("LLLLRRRR")).toEqual(1);
+  });
+});
