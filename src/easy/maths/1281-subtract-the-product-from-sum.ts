1c0b2bf9c2dde93eef2c497f8f7c6a0adbe9d193
Hououin Kyouma <inwazzin@gmail.com>
2022-05-21 16:27:52 +0200

    1281 - subtract sum from product

---
+++ src/easy/maths/1281-subtract-the-product-from-sum.ts
@@ -0,0 +1,10 @@
+export {};
+
+const add = (a: number, b: number) => a + b;
+const sum = (args: number[]) => args.reduce(add, 0);
+const multiply = (a: number, b: number) => a * b;
+const product = (args: number[]) => args.reduce(multiply, 1);
+
+const digits = (n: number) => n.toString().split("").map(Number);
+
+const subtractProductAndSum = (n: number) => product(digits(n)) - sum(digits(n));
