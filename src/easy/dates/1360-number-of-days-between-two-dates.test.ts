854208c55fee030b1369191431e0e34084a2ab8c
Hououin Kyouma <inwazzin@gmail.com>
2022-05-21 22:19:14 +0200

    1360 - number of days between two dates

---
+++ src/easy/dates/1360-number-of-days-between-two-dates.test.ts
@@ -0,0 +1,18 @@
+export {};
+
+const daysBetweenDates = (date1: string, date2: string): number => {
+  const d1 = new Date(date1);
+  const d2 = new Date(date2);
+
+  return Math.abs(d1.getTime() - d2.getTime()) / (1000 * 60 * 60 * 24);
+};
+
+describe("number of days between dates", () => {
+  it("case 1", () => {
+    expect(daysBetweenDates("2019-06-29", "2019-06-30")).toBe(1);
+  });
+
+  it("case 2", () => {
+    expect(daysBetweenDates("2020-01-15", "2019-12-31")).toBe(15);
+  });
+});
