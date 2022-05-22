f5054895d789615f77031e4a2110f190a7f2145c
Hououin Kyouma <inwazzin@gmail.com>
2022-05-22 03:24:29 +0200

    1827 - minimum operations to make the array increasing

---
+++ src/easy/numbers/algorithms/1827-minimum-operations-to-make-the-array-increasing.test.ts
@@ -0,0 +1,29 @@
+export {};
+
+const minOperations = (nums: number[]): number => {
+  let count = 0;
+
+  for (let i = 1; i < nums.length; ++i) {
+    if (nums[i] <= nums[i - 1]) {
+      const s = nums[i - 1] - nums[i] + 1;
+      nums[i] += s;
+      count += s;
+    }
+  }
+
+  return count;
+};
+
+describe("min operations", () => {
+  it("case 1", () => {
+    expect(minOperations([1, 1, 1])).toBe(3);
+  });
+
+  it("case 2", () => {
+    expect(minOperations([1, 5, 2, 4, 1])).toBe(14);
+  });
+
+  it("case 3", () => {
+    expect(minOperations([8])).toBe(0);
+  });
+});
