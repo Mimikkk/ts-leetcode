a5ce20ee7e76cf7e5699ced91ebefd928c9b6ead
Hououin Kyouma <inwazzin@gmail.com>
2022-05-22 03:15:31 +0200

    1323 - maximum 69 number

---
+++ src/easy/numbers/algorithms/1323-maximum-69-number.test.ts
@@ -0,0 +1,22 @@
+export {};
+
+const maximum69Number = (num: number): number =>
+  `${num}`.indexOf("6") === -1 && num || +`${num}`.replace("6", "9");
+
+describe("maximum 69 number", () => {
+  it("case 1", () => {
+    expect(maximum69Number(9669)).toBe(9969);
+  });
+
+  it("case 2", () => {
+    expect(maximum69Number(9996)).toBe(9999);
+  });
+
+  it("case 3", () => {
+    expect(maximum69Number(9999)).toBe(9999);
+  });
+
+  it("case 4", () => {
+    expect(maximum69Number(696)).toBe(996);
+  });
+});
