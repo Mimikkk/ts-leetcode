c83ba57067ecee74cb605859e1f1a5cfe3923803
Hououin Kyouma <inwazzin@gmail.com>
2022-05-24 02:44:12 +0200

    1351 - count negative numbers in a sorted array

---
+++ src/easy/arrays/algorithms/1351-count-negative-numbers-in-a-sorted-array.test.ts
@@ -0,0 +1,10 @@
+export {};
+
+const countNegatives = (grid: number[][]) =>
+  grid.flat().filter(x => x < 0).length;
+
+describe("count negatives", () => {
+  it("case 1", () => {
+    expect(countNegatives([[4, 3, 2, -1], [3, 2, 1, -1], [1, 1, -1, -2], [-1, -1, -2, -3]])).toBe(8);
+  });
+});
