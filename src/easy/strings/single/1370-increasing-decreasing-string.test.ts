9bf8ea0a0798307203692fbb1dc44a50a41e83f8
Hououin Kyouma <inwazzin@gmail.com>
2022-05-24 02:44:36 +0200

    1370 - increasing decreasing string

---
+++ src/easy/strings/single/1370-increasing-decreasing-string.test.ts
@@ -0,0 +1,24 @@
+export {};
+
+const sortString = (s: string) => {
+  s = s.split("").sort().join("");
+
+  let result = "";
+  while (s.length) {
+    result += s.match(/([a-z])(?!\1)/gi)!.join("");
+    s = s.replace(/([a-z])(?!\1)/gi, "");
+    result += (s.match(/([a-z])(?!\1)/gi) || []).reverse().join("");
+    s = s.replace(/([a-z])(?!\1)/gi, "");
+  }
+  return result;
+};
+
+describe("sort string", () => {
+  it("case 1", () => {
+    expect(sortString("aaaabbbbcccc")).toBe("abccbaabccba");
+  });
+
+  it("case 2", () => {
+    expect(sortString("rat")).toBe("art");
+  });
+});
