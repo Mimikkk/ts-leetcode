946af3e5669627ce58dfef90657c6eb56ec5def9
Hououin Kyouma <inwazzin@gmail.com>
2022-07-25 20:01:36 +0200

    11 container with most water

---
+++ src/medium/algorithm/11-container-with-most-water.test.ts
@@ -0,0 +1,19 @@
+const maxArea = (height: number[]): number => {
+  let [max, left, right] = [0, 0, height.length - 1];
+
+  while (left < right) {
+    const area = Math.min(height[left], height[right]) * (right - left);
+    max = Math.max(max, area);
+
+    if (height[left] < height[right]) ++left;
+    else --right;
+  }
+
+  return max;
+};
+
+describe("max area", () => {
+  it("case 1", () => {
+    expect(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])).toBe(49);
+  });
+});
