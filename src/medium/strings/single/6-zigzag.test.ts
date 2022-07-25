2bab0e4d2f241d7be6eb06ad475b211fb049330e
Hououin Kyouma <inwazzin@gmail.com>
2022-07-25 20:01:24 +0200

    6 zigzag

---
+++ src/medium/strings/single/6-zigzag.test.ts
@@ -0,0 +1,25 @@
+export {};
+const convert = (s: string, numRows: number): string => {
+  if (numRows === 1) return s;
+  const rows = Array(numRows).fill('');
+
+  let row = 0;
+  let isGoingDown = false;
+  for (let i = 0; i < s.length; i++) {
+    rows[row] += s[i];
+    if (row === 0 || row === numRows - 1) isGoingDown = !isGoingDown;
+    row += isGoingDown ? 1 : -1;
+  }
+
+  return rows.join('');
+};
+
+describe("zigzag", () => {
+  it("case 1", () => {
+    expect(convert("PAYPALISHIRING", 3)).toBe("PAHNAPLSIIGYIR");
+  });
+
+  it("case 2", () => {
+    expect(convert("PAYPALISHIRING", 4)).toBe("PINALSIGYAHRPI");
+  });
+});
