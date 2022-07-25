ae3f93d3198bce00163f447bc602555d5673a35a
Hououin Kyouma <inwazzin@gmail.com>
2022-07-25 20:01:46 +0200

    15 three sum

---
+++ src/medium/maths/15-three-sum.ts
@@ -0,0 +1,18 @@
+type Triplet = [number, number, number];
+
+const threeSum = (nums: number[]): Triplet[] => {
+  for (let i = 0; i < nums.length; ++i) {
+    const [a, b, c] = [nums[i], nums[i + 1], nums[i + 2]];
+    if (a + b + c === 0) return [[a, b, c]];
+  }
+
+};
+
+describe("threeSum", () => {
+  it("case 1", () => {
+    expect(threeSum([-1, 0, 1, 2, -1, -4])).toEqual([
+      [-1, 0, 1],
+      [-1, -1, 2],
+    ]);
+  });
+});
