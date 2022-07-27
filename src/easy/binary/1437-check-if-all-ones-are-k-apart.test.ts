836127f4925bbbe9638dba7887e111d0317de31f
Hououin Kyouma <inwazzin@gmail.com>
2022-07-27 23:21:47 +0200

    1437 check if all ones are k apart

---
+++ src/easy/binary/1437-check-if-all-ones-are-k-apart.test.ts
@@ -0,0 +1,23 @@
+export {};
+
+const kLengthApart = (nums: number[], k: number): boolean => {
+  let prev = -k - 1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      if (i - prev <= k) return false;
+      prev = i;
+    }
+  }
+
+  return true;
+};
+
+describe("are k aparrt", () => {
+  it("case 1", () => {
+    expect(kLengthApart([1, 0, 0, 1, 0, 1], 1)).toBeTruthy();
+  });
+
+  it("case 2", () => {
+    expect(kLengthApart([1, 0, 0, 1, 0, 1], 2)).toBeFalsy();
+  });
+});
