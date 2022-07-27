683cf4a3b83153edad971627a7f0427d4a123011
Hououin Kyouma <inwazzin@gmail.com>
2022-07-27 23:22:15 +0200

    1450 number of students doing homework at given time

---
+++ src/easy/algorithms/1450-number-of-students-doing-homework-at-given-time.test.ts
@@ -0,0 +1,23 @@
+export {};
+
+const busyStudent = (startTime: number[], endTime: number[], queryTime: number): number => {
+  let count = 0;
+
+  for (let i = 0; i < startTime.length; ++i) {
+    if (startTime[i] <= queryTime && queryTime <= endTime[i]) {
+      ++count;
+    }
+  }
+
+  return count;
+};
+
+describe("busy students", () => {
+  it("case 1", () => {
+    expect(busyStudent([1, 2, 3], [3, 2, 7], 4)).toBe(1);
+  });
+
+  it("case 2", () => {
+    expect(busyStudent([4], [4], 4)).toBe(1);
+  });
+});
