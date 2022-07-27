be5742329075a0f7f6476976279d0684fca45908
Hououin Kyouma <inwazzin@gmail.com>
2022-07-28 00:19:34 +0200

    1523 count odd in interval

---
+++ src/easy/maths/1523-count-odd-in-interval.test.ts
@@ -0,0 +1,17 @@
+export {};
+
+const countOdds = (low: number, high: number) => {
+  const c = Math.round((high - low) / 2);
+
+  return low % 2 === 1 && high % 2 === 1 ? c + 1 : c;
+};
+
+describe("count odd in interval", () => {
+  it("case 1", () => {
+    expect(countOdds(1, 7)).toBe(4);
+  });
+
+  it("case 2", () => {
+    expect(countOdds(8, 10)).toBe(1);
+  });
+});
