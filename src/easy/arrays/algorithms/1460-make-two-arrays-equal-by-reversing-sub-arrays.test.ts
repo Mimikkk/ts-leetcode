19fc27d404ea37c6a85fbbe4ae680ab8e32df9fd
Hououin Kyouma <inwazzin@gmail.com>
2022-07-27 23:22:41 +0200

    1460 make two arrays equal by reversing sub arrays

---
+++ src/easy/arrays/algorithms/1460-make-two-arrays-equal-by-reversing-sub-arrays.test.ts
@@ -0,0 +1,48 @@
+export {};
+
+interface Counter extends Record<number, number> {}
+
+module Counter {
+  export const create = (items: number[]) => {
+    const counter: Record<number, number> = {};
+    for (let item of items) counter[item] = (counter[item] || 0) + 1;
+    return counter;
+  };
+
+  export const isEqual = (left: Counter, right: Counter) => {
+    for (const key in left) {
+      if (left[key] !== right[key]) return false;
+    }
+    for (const key in right) {
+      if (left[key] !== right[key]) return false;
+    }
+
+    return true;
+  };
+}
+
+const { isEqual, create } = Counter;
+const canBeEqual = (left: number[], right: number[]) =>
+  isEqual(create(left), create(right));
+
+describe("can be equal by reversing", () => {
+  it("case 1", () => {
+    expect(canBeEqual([1, 2, 3, 4], [4, 3, 2, 1])).toBeTruthy();
+  });
+
+  it("case 2", () => {
+    expect(canBeEqual([1, 2, 3, 4], [2, 4, 1, 3])).toBeTruthy();
+  });
+
+  it("case 3", () => {
+    expect(canBeEqual([7], [7])).toBeTruthy();
+  });
+
+  it("case 4", () => {
+    expect(canBeEqual([1, 2, 3, 4], [4, 3, 2, 1, 5])).toBeFalsy();
+  });
+
+  it("case 5", () => {
+    expect(canBeEqual([3, 7, 9], [3, 7, 11])).toBeFalsy();
+  });
+});
