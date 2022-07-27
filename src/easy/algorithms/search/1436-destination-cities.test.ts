caabc5976079c56e64998ab2ee350e7f84e35fa0
Hououin Kyouma <inwazzin@gmail.com>
2022-07-27 23:21:38 +0200

    1436 destination cities

---
+++ src/easy/algorithms/search/1436-destination-cities.test.ts
@@ -0,0 +1,28 @@
+export {};
+
+const createCounter = (s: string[][]) => {
+  const counter: Record<string, number> = {};
+  for (const [from, to] of s) {
+    counter[from] = (counter[from] || 0) + 1;
+    counter[to] = (counter[to] || 0) + 1;
+  }
+
+  return counter;
+};
+const second = (arr: string[]) => arr[1];
+
+const destCity = (paths: string[][]): string => {
+  const counter = createCounter(paths);
+
+  return second(paths.find(([_, to]) => counter[to] == 1)!);
+};
+
+describe("destination city", () => {
+  it("case 1", () => {
+    expect(destCity([["London", "New York"], ["New York", "Lima"], ["Lima", "Sao Paulo"]])).toEqual("Sao Paulo");
+  });
+
+  it("case 2", () => {
+    expect(destCity([["B", "C"], ["D", "B"], ["C", "A"]])).toEqual("A");
+  });
+});
