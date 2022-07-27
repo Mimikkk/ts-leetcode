6524f77c1a194452205ff16d89013679f91d945b
Hououin Kyouma <inwazzin@gmail.com>
2022-07-27 23:22:31 +0200

    1455 check if a word occurs as a prefix of any word in a sentence

---
+++ src/easy/strings/sentences/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.test.ts
@@ -0,0 +1,19 @@
+export {};
+
+const isPrefixOfWord = (sentence: string, searchWord: string): number =>
+  sentence.split(" ")
+          .findIndex((word) => word.startsWith(searchWord)) + 1 || -1;
+
+describe("is prefix of word", () => {
+  it("case 1", () => {
+    expect(isPrefixOfWord("i love eating burger", "burg")).toBe(4);
+  });
+
+  it("case 2", () => {
+    expect(isPrefixOfWord("this problem is an easy problem", "pro")).toBe(2);
+  });
+
+  it("case 3", () => {
+    expect(isPrefixOfWord("i am tired", "you")).toBe(-1);
+  });
+});
