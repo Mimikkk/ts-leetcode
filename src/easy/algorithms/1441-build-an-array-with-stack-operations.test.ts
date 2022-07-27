c3e11008efa16beaef119aa5b005ab2592d37be4
Hououin Kyouma <inwazzin@gmail.com>
2022-07-27 23:21:55 +0200

    1441 build an array with stack operations

---
+++ src/easy/algorithms/1441-build-an-array-with-stack-operations.test.ts
@@ -0,0 +1,37 @@
+export {};
+
+const enum Operation {
+  Push = "Push",
+  Pop = "Pop",
+}
+
+const buildArray = (target: number[], n: number): string[] => {
+  const result: Operation[] = [];
+
+  let current = 0;
+  for (let i = 1; i < n + 1; ++i) {
+    if (target[current] === i) {
+      result.push(Operation.Push);
+      if (++current === target.length) break;
+    } else {
+      result.push(Operation.Push);
+      result.push(Operation.Pop);
+    }
+  }
+
+  return result;
+};
+
+describe("build array with stack operations", () => {
+  it("case 1", () => {
+    expect(buildArray([1, 3], 3)).toEqual(["Push", "Push", "Pop", "Push"]);
+  });
+
+  it("case 2", () => {
+    expect(buildArray([1, 2, 3], 3)).toEqual(["Push", "Push", "Push"]);
+  });
+
+  it("case 3", () => {
+    expect(buildArray([1, 2], 4)).toEqual(["Push", "Push"]);
+  });
+});
