7882894ff7e792d7b6d0135a64b5f823faa72454
Hououin Kyouma <inwazzin@gmail.com>
2022-07-27 23:22:54 +0200

    1464 max product of two elements in an array

---
+++ src/easy/arrays/maths/1464-max-product-of-two-elements-in-an-array.test.ts
@@ -0,0 +1,22 @@
+export {};
+
+const desc = (a: number, b: number) => b - a;
+const maxProduct = (nums: number[]): number => {
+  const [a, b] = nums.sort(desc);
+
+  return (a - 1) * (b - 1);
+};
+
+describe("max product of two elements in an array", () => {
+  it("case 1", () => {
+    expect(maxProduct([3, 4, 5, 2])).toBe(12);
+  });
+
+  it("case 2", () => {
+    expect(maxProduct([1, 5, 4, 5])).toBe(16);
+  });
+
+  it("case 2", () => {
+    expect(maxProduct([3, 7])).toBe(12);
+  });
+});
