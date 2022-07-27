bfbc25469ce24a2a676aac5bcbd0ed795d8084da
Hououin Kyouma <inwazzin@gmail.com>
2022-07-28 00:20:01 +0200

    1539 kth missing positive number

---
+++ src/easy/arrays/algorithms/1539-kth-missing-positive-number.test.ts
@@ -0,0 +1,25 @@
+export {};
+
+function findKthPositive(arr: number[], k: number): number {
+  let missing = 0;
+
+  let next = 0;
+  for (let i = 1; i < arr.at(-1)! + 1; i++) {
+    if (i !== arr[next]) {
+      ++missing;
+      if (missing === k) return i;
+    } else ++next;
+  }
+
+  return arr.at(-1)! + (k - missing);
+}
+
+describe("kth missing positive", () => {
+  it("case 1", () => {
+    expect(findKthPositive([2, 3, 4, 7, 11], 5)).toBe(9);
+  });
+
+  it("case 2", () => {
+    expect(findKthPositive([1, 2, 3, 4], 2)).toBe(6);
+  });
+});
