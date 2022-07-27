9c7439d6d7a5f1a53c0081c6e33bda9f4940fa59
Hououin Kyouma <inwazzin@gmail.com>
2022-07-27 23:22:01 +0200

    1446 consecutive chars

---
+++ src/easy/strings/single/1446-consecutive-chars.test.ts
@@ -0,0 +1,31 @@
+export {};
+
+const maxPower = (s: string): number => {
+  let max = 0;
+  let current = 1;
+
+  for (let i = 0; i < s.length; ++i) {
+    if (s[i] === s[i + 1]) {
+      ++current;
+    } else {
+      max = Math.max(max, current);
+      current = 1;
+    }
+  }
+
+  return Math.max(max, current);
+};
+
+describe("max power", () => {
+  it("case 1", () => {
+    expect(maxPower("leetcode")).toBe(2);
+  });
+
+  it("case 2", () => {
+    expect(maxPower("abbcccddddeeeeedcba")).toBe(5);
+  });
+
+  it("case 3", () => {
+    expect(maxPower("cc")).toBe(2);
+  });
+});
