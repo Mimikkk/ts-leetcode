d7b3ee8a4aa5be6e368aebace4c2f2ee80a8d526
Hououin Kyouma <inwazzin@gmail.com>
2022-07-28 00:19:25 +0200

    1507 refromat date

---
+++ src/easy/dates/1507-reformat-date.test.ts
@@ -0,0 +1,29 @@
+export {};
+
+const monthsNumber: Record<string, string> = {
+  Jan:"01",
+  Feb:"02",
+  Mar:"03",
+  Apr:"04",
+  May:"05",
+  Jun:"06",
+  Jul:"07",
+  Aug:"08",
+  Sep:"09",
+  Oct:"10",
+  Nov:"11",
+  Dec:"12",
+};
+const reformatDate = (date: string): string => {
+  const [day, month, year] = date.split(" ");
+  const dayNum = day.length === 3 ? `0${day.slice(0, 1)}` : day.slice(0, 2);
+  const monthNum = month.slice(0, 3);
+
+  return `${year}-${monthsNumber[monthNum]}-${dayNum}`;
+};
+
+describe("reformat date", () => {
+  it("case 1", () => {
+    expect(reformatDate("20th Oct 2052")).toBe("2052-10-20");
+  });
+});
