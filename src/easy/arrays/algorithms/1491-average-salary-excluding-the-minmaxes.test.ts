83c29aa35bcd2791bc330eec5cefb0d303cf0943
Hououin Kyouma <inwazzin@gmail.com>
2022-07-27 23:23:26 +0200

    1491 average salary excluding the minmaxse

---
+++ src/easy/arrays/algorithms/1491-average-salary-excluding-the-minmaxes.test.ts
@@ -0,0 +1,24 @@
+export {};
+
+const average = (salary: number[]): number => {
+  let [min, max] = [Number.MAX_SAFE_INTEGER, Number.MIN_SAFE_INTEGER];
+
+  let sum = 0;
+  for (let i = 0; i < salary.length; ++i) {
+    min = Math.min(min, salary[i]);
+    max = Math.max(max, salary[i]);
+    sum += salary[i];
+  }
+
+  return (sum - max - min) / (salary.length - 2);
+};
+
+describe("avg without max and min", () => {
+  it("case 1", () => {
+    expect(average([4000, 3000, 1000, 2000])).toBe(2500);
+  });
+
+  it("case 2", () => {
+    expect(average([1000, 2000, 3000])).toBe(2000);
+  });
+});
