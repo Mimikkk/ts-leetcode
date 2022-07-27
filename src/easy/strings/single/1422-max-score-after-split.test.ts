8f99ea6c586074766db7dec45bdcf96b4af11f0c
Hououin Kyouma <inwazzin@gmail.com>
2022-07-27 23:21:31 +0200

    1422 max score after split

---
+++ src/easy/strings/single/1422-max-score-after-split.test.ts
@@ -0,0 +1,31 @@
+const maxScore = (s: string): number => {
+  let max = 0;
+  let [leftOnes, rightZeros] = [s.split("1").length - 1, 0];
+
+  for (let i = 0; i < s.length-1; i++) {
+    if (s[i] === "0") rightZeros++;
+    else leftOnes--;
+
+    max = Math.max(max, leftOnes + rightZeros);
+  }
+
+  return max;
+};
+
+describe("max score after split", () => {
+  it("case 1", () => {
+    expect(maxScore("011101")).toEqual(5);
+  });
+
+  it("case 2", () => {
+    expect(maxScore("00111")).toEqual(5);
+  });
+
+  it("case 3", () => {
+    expect(maxScore("1111")).toEqual(3);
+  });
+
+  it("case 4", () => {
+    expect(maxScore("00")).toEqual(1);
+  });
+});
