fb5ff7336d7b686d696e6dfaf2d9303e3a965cd1
Hououin Kyouma <inwazzin@gmail.com>
2022-07-30 01:27:06 +0200

    1768 merge strings alternating

---
+++ src/easy/strings/words/1768-merge-strings-alternately.test.ts
@@ -0,0 +1,20 @@
+export {};
+
+const mergeAlternately = (word1: string, word2: string): string => {
+  const result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < word1.length || j < word2.length) {
+    if (i < word1.length) result.push(word1[i++]);
+    if (j < word2.length) result.push(word2[j++]);
+  }
+
+  return result.join("");
+};
+
+describe("interweave strings", () => {
+  test("should return correct result", () => {
+    expect(mergeAlternately("abc", "pqr")).toEqual("apbqcr");
+  });
+});
