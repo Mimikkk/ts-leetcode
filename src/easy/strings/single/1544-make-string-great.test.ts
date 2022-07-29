6acfd65e285386c7bbe330345c3df92873642f38
Hououin Kyouma <inwazzin@gmail.com>
2022-07-30 00:36:10 +0200

    1544 make string great

---
+++ src/easy/strings/single/1544-make-string-great.test.ts
@@ -0,0 +1,30 @@
+export {};
+
+const isSame = (a: string, b: string) => a.toLowerCase() === b.toLowerCase();
+
+const makeGood = (s: string): string => {
+  const stack = [""];
+
+  for (let c of s) {
+    let top = stack.at(-1)!;
+
+    if (isSame(top, c) && top !== c) stack.pop();
+
+    else stack.push(c);
+  }
+  return stack.join("");
+};
+
+describe("make string good", () => {
+  it("case 1", () => {
+    expect(makeGood("leEeetcode")).toEqual("leetcode");
+  });
+
+  it("case 2", () => {
+    expect(makeGood("abBAcC")).toEqual("");
+  });
+
+  it("case 3", () => {
+    expect(makeGood("mC")).toEqual("mC");
+  });
+});
