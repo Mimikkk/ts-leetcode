d245c4e2912f51c5377fe951761a64dc1be0db75
Hououin Kyouma <inwazzin@gmail.com>
2022-07-30 01:26:52 +0200

    1758 min changes to make alternating binary string

---
+++ src/easy/strings/single/1758-minimum-changes-to-make-alternating-binary-string.test.ts
@@ -0,0 +1,20 @@
+export {};
+
+
+const minOperations = (s: string) => {
+  let chars = ["1", "0"];
+
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (chars[i % 2] === s[i]) ++count;
+  }
+
+  return Math.min(count, s.length - count);
+};
+
+
+describe("minimum to alternate", () => {
+  it("case 1", () => {
+    expect(minOperations("0100")).toEqual(1);
+  });
+});
