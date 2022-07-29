7a3cdd2ef3ffcdfdab6bd74f3139273074ac116d
Hououin Kyouma <inwazzin@gmail.com>
2022-07-30 01:26:27 +0200

    1752 check if array is sorted and rotated

---
+++ src/easy/arrays/algorithms/1752-check-if-array-is-sorted-and-rotated.test.ts
@@ -0,0 +1,24 @@
+export {};
+
+const isSorted = (nums: number[]) => {
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i + 1]) return false;
+  }
+  return true;
+}
+const check = (nums: number[]): boolean => {
+  let count = 0;
+  while (count !== nums.length) {
+    nums.push(nums.shift()!);
+    if (isSorted(nums)) return true;
+    ++count;
+  }
+  return false;
+};
+
+describe("is sorted and rotated", () => {
+  it("case 1", () => {
+    expect(check([1, 2, 3, 4, 5, 6, 7])).toEqual(true);
+    expect(check([7, 1, 2, 3, 4, 5, 6])).toEqual(true);
+  });
+});
