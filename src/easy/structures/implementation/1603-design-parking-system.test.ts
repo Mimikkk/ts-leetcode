9ae35e449ec4a631d6e8a790f89bf6bddc97f189
Hououin Kyouma <inwazzin@gmail.com>
2022-07-31 01:04:13 +0200

    1603-design-parking-system.test.ts

---
+++ src/easy/structures/implementation/1603-design-parking-system.test.ts
@@ -0,0 +1,27 @@
+export {};
+
+class ParkingSystem {
+  constructor(
+    private big: number,
+    private medium: number,
+    private small: number,
+  ) {}
+
+  addCar(type: 1 | 2 | 3) {
+    switch (type) {
+      case 3:
+        return !!this.small && (this.small--, true);
+      case 2:
+        return !!this.medium && (this.medium--, true);
+      case 1:
+        return !!this.big && (this.big--, true);
+    }
+  }
+}
+
+describe("parking system", () => {
+  it("should return true", () => {
+    expect(new ParkingSystem(1, 1, 0).addCar(1)).toBe(true);
+    expect(new ParkingSystem(1, 1, 0).addCar(3)).toBe(false);
+  });
+});
