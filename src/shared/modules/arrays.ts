2072fe541920514e38021e45cb9d07b3c6f77872
Hououin Kyouma <inwazzin@gmail.com>
2022-07-30 19:59:24 +0200

    update array lib

--- src/shared/modules/arrays.ts
+++ src/shared/modules/arrays.ts
@@ -1,18 +1,27 @@
 export {};
 
 module A {
-  export const add = (a: number, b: number) => a + b;
-  export const sum = (arr: number[]) => arr.reduce(add, 0);
-  export const len = (arr: number[]): number => arr.length;
-  export const mean = (arr: number[]) => sum(arr) / len(arr);
+  export const first = <T, Y = undefined>(arr: T[], or?: Y): T | Y =>
+    (arr[0] === undefined ? or : arr[0])!;
 
-  export const sorted = (
-    arr: number[],
-    fn: (a: number, b: number) => number = S.asc,
-  ) => [...arr].sort(fn);
+  export const last = <T, Y = undefined>(arr: T[], or?: Y): T | Y =>
+    (arr.at(-1) === undefined ? or : arr.at(-1))!;
 
-  export module S {
+  export const sorted = <T>(arr: T[], fn: (a: T, b: T) => number) =>
+    [...arr].sort(fn);
+
+  export module N {
+    export const add = (a: number, b: number) => a + b;
+    export const sum = (arr: number[]) => arr.reduce(add, 0);
+    export const len = (arr: number[]): number => arr.length;
+    export const mean = (arr: number[]) => sum(arr) / len(arr);
+
     export const asc = (a: number, b: number) => a - b;
     export const desc = (a: number, b: number) => b - a;
+  }
+
+  export module S {
+    export const asc = (a: string, b: string) => a.localeCompare(b);
+    export const desc = (a: string, b: string) => b.localeCompare(a);
   }
 }
