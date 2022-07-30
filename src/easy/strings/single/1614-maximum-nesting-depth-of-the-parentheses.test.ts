95989a3514a751f3cb3173cda267531c6e0211f4
Hououin Kyouma <inwazzin@gmail.com>
2022-07-30 18:02:46 +0200

    1614-maximum-nesting-depth-of-the-parentheses.test.ts

---
+++ src/easy/strings/single/1614-maximum-nesting-depth-of-the-parentheses.test.ts
@@ -0,0 +1,24 @@
+export {};
+
+const makeVPS = (s: string): string => s.replace(/[^()]/gi, "");
+
+const maxDepth = (s: string): number => {
+  let depth = 0;
+
+  let max = 0;
+  [...makeVPS(s)].forEach((c) => {
+    if (c === "(") {
+      if (++depth > max) max = depth;
+    } else --depth;
+  });
+
+  return max;
+};
+
+describe("max nexted depth of the parentheses", () => {
+  it("case 1", () => {
+    expect(maxDepth("(()())")).toBe(2);
+    expect(maxDepth("(1+(2*3)+((8)/4))+1")).toBe(3);
+    expect(maxDepth("(1)+((2))+(((3)))")).toBe(3);
+  });
+});
