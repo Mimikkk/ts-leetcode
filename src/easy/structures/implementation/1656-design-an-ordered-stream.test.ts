8d62b8f653fde802c7810c1f023061db09e42a4e
Hououin Kyouma <inwazzin@gmail.com>
2022-07-31 01:04:17 +0200

    1656-design-an-ordered-stream.test.ts

---
+++ src/easy/structures/implementation/1656-design-an-ordered-stream.test.ts
@@ -0,0 +1,30 @@
+export {};
+
+class OrderedStream {
+  private pointer: number;
+  private readonly stream: string[];
+
+  constructor(n: number) {
+    this.pointer = 0;
+    this.stream = new Array(n);
+  }
+
+  insert(id: number, value: string) {
+    let { pointer, stream } = this;
+    stream[id - 1] = value;
+
+
+    let chunk = [];
+    while (stream[pointer]) chunk.push(stream[pointer++]);
+
+    this.pointer = pointer;
+
+    return chunk;
+  }
+}
+
+describe("ordered stream", () => {
+  it("should return the correct chunk", () => {
+    expect(new OrderedStream(5).insert(1, "a")).toEqual(["a"]);
+  });
+});
