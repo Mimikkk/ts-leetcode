f28a257a30639eea88b933585b97e60eecb934be
Hououin Kyouma <inwazzin@gmail.com>
2022-07-31 01:27:11 +0200

    2000-reverse-prefix-of-word.test.ts

---
+++ src/easy/strings/single/2000-reverse-prefix-of-word.test.ts
@@ -0,0 +1,25 @@
+export {};
+
+module S {
+  export const reverse = (word: string) => word.split("").reverse().join("");
+}
+
+const reversePrefix = (word: string, ch: string): string => {
+  let i = 0;
+  while (word[i++] !== ch && i <= word.length);
+  if (i > word.length) return word;
+
+  return S.reverse(word.substring(0, i)) + word.substring(i);
+};
+
+describe("reversePrefix", () => {
+  it("should return the correct value", () => {
+    expect(reversePrefix("abcdefd", "d")).toBe("dcbaefd");
+    expect(reversePrefix("abcd", "d")).toBe("dcba");
+    expect(reversePrefix("abcdefd", "g")).toBe("abcdefd");
+    expect(reversePrefix("abcd", "z")).toBe("abcd");
+    expect(reversePrefix("rzwuktxcjfpamlonbgyieqdvhs", "s")).toBe(
+      "shvdqeiygbnolmapfjcxtkuwzr",
+    );
+  });
+});
