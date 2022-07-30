be444d45041c3730f8e6eff535c6de37d57164e3
Hououin Kyouma <inwazzin@gmail.com>
2022-07-30 02:42:10 +0200

    1710-maximum-units-on-a-truck.test.ts

---
+++ src/easy/algorithms/1710-maximum-units-on-a-truck.test.ts
@@ -0,0 +1,49 @@
+export {};
+
+type BoxType = [number, number];
+module BoxType {
+  export const desc = ([, u1]: BoxType, [, u2]: BoxType) => u2 - u1;
+}
+
+const maximumUnits = (boxTypes: BoxType[], truckSize: number): number => {
+  boxTypes.sort(BoxType.desc);
+  let [loaded, units] = [0, 0];
+
+  types: for (let [count, value] of boxTypes) {
+    while (count--) {
+      units += value;
+      if (++loaded >= truckSize) break types;
+    }
+  }
+
+  return units;
+};
+
+describe("maximum units on a truck", () => {
+  it("case 1", () => {
+    expect(
+      maximumUnits(
+        [
+          [1, 3],
+          [2, 2],
+          [3, 1],
+        ],
+        4,
+      ),
+    ).toBe(8);
+  });
+
+  it("case 2", () => {
+    expect(
+      maximumUnits(
+        [
+          [5, 10],
+          [2, 5],
+          [4, 7],
+          [3, 9],
+        ],
+        10,
+      ),
+    ).toBe(91);
+  });
+});
