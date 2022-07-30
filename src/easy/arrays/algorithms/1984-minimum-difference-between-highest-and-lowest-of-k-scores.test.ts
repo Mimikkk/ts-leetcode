378512933247d4c2a49e87c2f45026909ba37c64
Hououin Kyouma <inwazzin@gmail.com>
2022-07-31 01:17:46 +0200

    1984-minimum-difference-between-highest-and-lowest-of-k-scores.test.ts

---
+++ src/easy/arrays/algorithms/1984-minimum-difference-between-highest-and-lowest-of-k-scores.test.ts
@@ -0,0 +1,19 @@
+import { A } from "shared/modules";
+
+const minimumDifference = (nums: number[], k: number): number => {
+  nums.sort(A.N.asc);
+  let min = Infinity;
+
+  for (let i = k - 1; i < nums.length; ++i) {
+    min = Math.min(min, nums[i] - nums[i - k + 1]);
+  }
+
+  return min;
+};
+
+describe("min difference", () => {
+  it("should return the minimum difference", () => {
+    expect(minimumDifference([90], 1)).toBe(0);
+    expect(minimumDifference([9, 4, 1, 7], 2)).toBe(2);
+  });
+});
