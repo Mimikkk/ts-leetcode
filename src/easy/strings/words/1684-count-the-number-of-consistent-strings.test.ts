c559f06111187a5e37d791d3320bc543543c7868
Hououin Kyouma <inwazzin@gmail.com>
2022-07-30 02:41:47 +0200

    1684-count-the-number-of-consistent-strings.test.ts

---
+++ src/easy/strings/words/1684-count-the-number-of-consistent-strings.test.ts
@@ -0,0 +1,12 @@
+export {};
+
+const countConsistentStrings = (allowed: string, words: string[]): number =>
+  words
+    .map((word) => word.split(""))
+    .filter((word) => !word.filter((c) => !allowed.includes(c)).length).length;
+
+describe("count consistent strings", () => {
+  it("case 1", () => {
+    expect(countConsistentStrings("abc", ["a", "b", "c"])).toBe(3);
+  });
+});
