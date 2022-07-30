3502d168945277287a4c66edcc31eb4686ae1027
Hououin Kyouma <inwazzin@gmail.com>
2022-07-30 02:42:06 +0200

    1704-determine-if-string-halves-are-alike.test.ts

---
+++ src/easy/strings/words/1704-determine-if-string-halves-are-alike.test.ts
@@ -0,0 +1,23 @@
+export {};
+
+const vowels = "aeiouAEIOU";
+const countVowels = (s: string): number => {
+  let count = 0;
+  for (const letter of s) if (vowels.includes(letter)) ++count;
+  return count;
+};
+const half = (s: string): [string, string] => {
+  const half = s.length / 2;
+  return [s.slice(0, half), s.slice(half)];
+};
+
+const halvesAreAlike = (s: string) => {
+  const [c1, c2] = half(s).map(countVowels);
+  return c1 === c2;
+};
+
+describe("are halvesAreAlike", () => {
+  it("case 1", () => {
+    expect(halvesAreAlike("abce")).toBe(true);
+  });
+});
