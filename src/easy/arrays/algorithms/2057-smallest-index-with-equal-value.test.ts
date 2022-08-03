988a13eab003816837ce0d74289850cf2c4ee19d
Hououin Kyouma <inwazzin@gmail.com>
2022-08-03 02:15:16 +0200

    2057-smallest-index-with-equal-value.test.ts

---
+++ src/easy/arrays/algorithms/2057-smallest-index-with-equal-value.test.ts
@@ -0,0 +1,10 @@
+export {};
+
+const smallestEqual = (nums: number[]) =>
+  nums.findIndex((n, i) => i % 10 === n);
+
+describe("smallest equal", () => {
+  it("should return the smallest index", () => {
+    expect(smallestEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toEqual(-1);
+  });
+});
