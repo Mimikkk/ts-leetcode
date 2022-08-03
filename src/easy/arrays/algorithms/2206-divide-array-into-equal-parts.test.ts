184e396002a2d2e7ab4965ba636084f6df1e4097
Hououin Kyouma <inwazzin@gmail.com>
2022-08-03 02:16:04 +0200

    2206-divide-array-into-equal-parts.test.ts

---
+++ src/easy/arrays/algorithms/2206-divide-array-into-equal-parts.test.ts
@@ -0,0 +1,10 @@
+import { R } from "shared/modules";
+
+const divideArray = (nums: number[]) =>
+  R.values(R.counter(nums)).every((value) => value % 2 === 0);
+
+describe("divide array into equal parts", () => {
+  it("should return the number of parts", () => {
+    expect(divideArray([3, 2, 3, 2, 2, 2])).toEqual(true);
+  });
+});
