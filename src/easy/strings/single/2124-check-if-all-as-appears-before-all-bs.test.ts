b4910a33bff8f416b536b51870b88063678fc603
Hououin Kyouma <inwazzin@gmail.com>
2022-08-03 02:15:42 +0200

    2124-check-if-all-as-appears-before-all-bs.test.ts

---
+++ src/easy/strings/single/2124-check-if-all-as-appears-before-all-bs.test.ts
@@ -0,0 +1,23 @@
+export {};
+
+const checkString = (s: string) => {
+  let aOccured = false;
+  let bOccured = false;
+
+  return ![...s].some((c) => {
+    if (c === "a") {
+      aOccured = true;
+      if (bOccured) return true;
+    } else if (c === "b") {
+      bOccured = true;
+    }
+  });
+};
+
+describe("check string", () => {
+  it("test", () => {
+    expect(checkString("aaabbb")).toEqual(true);
+    expect(checkString("abab")).toEqual(false);
+    expect(checkString("bbb")).toEqual(true);
+  });
+});
