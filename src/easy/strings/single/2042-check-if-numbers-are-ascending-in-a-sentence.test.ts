c46a4aa83f785bb41c6e3d5b63945da1e5e585cb
Hououin Kyouma <inwazzin@gmail.com>
2022-08-03 02:15:10 +0200

    2042-check-if-numbers-are-ascending-in-a-sentence.test.ts

---
+++ src/easy/strings/single/2042-check-if-numbers-are-ascending-in-a-sentence.test.ts
@@ -0,0 +1,15 @@
+import { A } from "../../../shared/modules";
+
+const areNumbersAscending = (s: string) => {
+  let numbers = s.match(/(\d+)/g)?.map(Number) || [];
+
+  return !A.windows(numbers, 2).find(([a, b]) => a >= b);
+};
+
+describe("are numbers asc", () => {
+  it("should return true", () => {
+    expect(areNumbersAscending("1 2 3")).toBe(true);
+    expect(areNumbersAscending("1 2 ss 23 13")).toBe(false);
+    expect(areNumbersAscending("1 1")).toBe(false);
+  });
+});
