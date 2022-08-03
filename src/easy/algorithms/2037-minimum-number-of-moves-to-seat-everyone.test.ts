98dcf02903996b04f52a820677a32c5b2976993d
Hououin Kyouma <inwazzin@gmail.com>
2022-08-03 02:15:07 +0200

    2037-minimum-number-of-moves-to-seat-everyone.test.ts

---
+++ src/easy/algorithms/2037-minimum-number-of-moves-to-seat-everyone.test.ts
@@ -0,0 +1,18 @@
+import { A } from "shared/modules";
+
+export {};
+
+const minMovesToSeat = (seats: number[], students: number[]): number => {
+  seats.sort(A.N.desc);
+  students.sort(A.N.desc);
+
+  return A.N.sum(
+    A.zip(seats, students).map(([seat, student]) => Math.abs(student - seat)),
+  );
+};
+
+describe("min moves to seat", () => {
+  it("should return the min moves", () => {
+    expect(minMovesToSeat([3, 1, 5], [2, 7, 4])).toBe(4);
+  });
+});
