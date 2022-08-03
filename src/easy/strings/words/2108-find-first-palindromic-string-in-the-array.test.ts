d4de5dea1f8797c5498451eb63ddcf131c36457b
Hououin Kyouma <inwazzin@gmail.com>
2022-08-03 02:15:31 +0200

    2108-find-first-palindromic-string-in-the-array.test.ts

---
+++ src/easy/strings/words/2108-find-first-palindromic-string-in-the-array.test.ts
@@ -0,0 +1,13 @@
+export {};
+
+
+const isPalindrome = (word: string) => word.split("").reverse().join("") === word;
+const firstPalindrome = (words: string[]) => words.find(isPalindrome) || "";
+
+describe("first palindrome", () => {
+  it("should return the first palindrome", () => {
+    expect(firstPalindrome(["a", "b", "c", "d", "e"])).toEqual("a");
+    expect(firstPalindrome(["a"])).toEqual("a");
+    expect(firstPalindrome([])).toEqual("");
+  });
+});
