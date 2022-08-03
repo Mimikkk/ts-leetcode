728b42a2f2354b8680969b295686215e0ab50b95
Hououin Kyouma <inwazzin@gmail.com>
2022-08-03 21:00:21 +0200

    1974-minimum-time-to-type-word-using-special-typewriter.test.ts

---
+++ src/easy/strings/algorithms/1974-minimum-time-to-type-word-using-special-typewriter.test.ts
@@ -0,0 +1,34 @@
+import { A } from "shared/modules";
+
+const asCode = (char: string) => char.charCodeAt(0);
+
+const findDistance = ([a, b]: [number, number]) => {
+  const difference = Math.abs(a - b);
+
+  return Math.min(difference, 26 - difference) + 1;
+};
+
+const minTimeToType = (word: string) =>
+  A.N.sum(A.windows(["a", ...word].map(asCode), 2).map(findDistance));
+
+describe("min time", () => {
+  it("case 1", () => {
+    expect(minTimeToType("hello")).toBe(25);
+  });
+
+  it("case 2", () => {
+    expect(minTimeToType("world")).toBe(34);
+  });
+
+  it("case 3", () => {
+    expect(minTimeToType("helloworld")).toBe(63);
+  });
+
+  it("case 4", () => {
+    expect(minTimeToType("helloworldhello")).toBe(85);
+  });
+
+  it("case 5", () => {
+    expect(minTimeToType("helloworldhellohelloworld")).toBe(148);
+  });
+});
