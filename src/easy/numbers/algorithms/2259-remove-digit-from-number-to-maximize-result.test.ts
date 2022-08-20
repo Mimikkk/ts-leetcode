b11c55a4671b417f7cec4184e687635a44a22670
Hououin Kyouma <inwazzin@gmail.com>
2022-08-20 17:01:10 +0200

    2259-remove-digit-from-number-to-maximize-result.test.ts

---
+++ src/easy/numbers/algorithms/2259-remove-digit-from-number-to-maximize-result.test.ts
@@ -0,0 +1,27 @@
+export {};
+
+const removeDigit = (number: string, digit: string) => {
+  let indexToRemove: number = 0;
+
+  for (let i = 0; i < number.length; ++i) {
+    if (number[i] === digit) {
+      indexToRemove = i;
+
+      if (
+        i < number.length - 1 &&
+        number.charCodeAt(i) < number.charCodeAt(i + 1)
+      )
+        break;
+    }
+  }
+
+  return (
+    number.substring(0, indexToRemove) + number.substring(indexToRemove + 1)
+  );
+};
+
+describe("remove digit", () => {
+  it("case 1", () => {
+    expect(removeDigit("1432219", "3")).toBe("142219");
+  });
+});
