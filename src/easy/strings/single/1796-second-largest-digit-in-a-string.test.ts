4963406cb5f6a9cb9480173c409fbed427366cc0
Hououin Kyouma <inwazzin@gmail.com>
2022-08-20 16:59:46 +0200

    1796-second-largest-digit-in-a-string.test.ts

---
+++ src/easy/strings/single/1796-second-largest-digit-in-a-string.test.ts
@@ -0,0 +1,18 @@
+import { A } from "shared/modules";
+
+const secondHighest = (s: string) =>
+  A.second(
+    A.unique(
+      s
+        .replace(/[^0-9]/g, "")
+        .split("")
+        .map(Number),
+    ).sort(A.N.desc),
+    -1,
+  );
+
+describe("secon", () => {
+  it("case 1", () => {
+    expect(secondHighest("dfa12321afd")).toBe(2);
+  });
+});
