fbfc2209fba9aa5b9a6331d40425a5daf0c1b075
Hououin Kyouma <inwazzin@gmail.com>
2022-08-20 16:59:35 +0200

    1790-check-if-one-string-swap-can-make-strings-equal.test.ts

---
+++ src/easy/strings/single/1790-check-if-one-string-swap-can-make-strings-equal.test.ts
@@ -0,0 +1,21 @@
+export {};
+
+const areAlmostEqual = (s1: string, s2: string) => {
+  if (s1 === s2) return true;
+
+  const a1 = s1.split("").sort();
+  const a2 = s2.split("").sort();
+
+  if (!a1.every((c, i) => c === a2[i])) return false;
+
+  const difference = s1.split("").filter((c, i) => c !== s2[i]).length;
+
+  return difference <= 2;
+};
+
+describe("are almost equal", () => {
+  it("case 1", () => {
+    expect(areAlmostEqual("a", "a")).toBe(true);
+    expect(areAlmostEqual("ba", "ab")).toBe(true);
+  });
+});
