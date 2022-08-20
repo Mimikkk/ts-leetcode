891724eb5dd723aeba0c40a0c72edcdbdee0a39a
Hououin Kyouma <inwazzin@gmail.com>
2022-08-20 17:00:36 +0200

    1812-determine-color-of-a-chessboard-square.test.ts

---
+++ src/easy/strings/single/1812-determine-color-of-a-chessboard-square.test.ts
@@ -0,0 +1,20 @@
+export {};
+
+const code = (s: string) => s.charCodeAt(0);
+const squareIsWhite = (coordinates: string) => {
+  const [x, y] = coordinates.split("");
+
+  return !!((code(x) + code(y)) % 2);
+};
+
+describe("square is white", () => {
+  it("case 1", () => {
+    expect(squareIsWhite("a1")).toBe(false);
+  });
+  it("case 2", () => {
+    expect(squareIsWhite("h3")).toBe(true);
+  });
+  it("case 3", () => {
+    expect(squareIsWhite("c7")).toBe(false);
+  });
+});
