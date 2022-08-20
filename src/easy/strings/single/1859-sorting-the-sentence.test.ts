aa08edb797b565619e68f419de5ccb078007b77f
Hououin Kyouma <inwazzin@gmail.com>
2022-08-20 17:01:01 +0200

    1859-sorting-the-sentence.test.ts

---
+++ src/easy/strings/single/1859-sorting-the-sentence.test.ts
@@ -0,0 +1,17 @@
+export {};
+
+const sortSentence = (s: string) =>
+  [...s.matchAll(/(([a-z]+?)(\d+))/gi)]
+    .map(([, , word, order]) => [word, +order] as [string, number])
+    .sort(([, a], [, b]) => a - b)
+    .map(([v]) => v)
+    .join(" ");
+
+describe("sort sentences", () => {
+  it("case 1", () => {
+    expect(sortSentence("is2 sentence4 This1 a3")).toBe("This is a sentence");
+  });
+  it("case 2", () => {
+    expect(sortSentence("Myself2 Me1 I4 and3")).toBe("Me Myself and I");
+  });
+});
