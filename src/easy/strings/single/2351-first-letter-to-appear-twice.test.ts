576282647d1e56b5be80c548622a570e7f3df57c
Hououin Kyouma <inwazzin@gmail.com>
2022-08-20 17:01:15 +0200

    2351-first-letter-to-appear-twice.test.ts

---
+++ src/easy/strings/single/2351-first-letter-to-appear-twice.test.ts
@@ -0,0 +1,27 @@
+export {};
+
+const repeatedCharacter = (s: string): string => {
+  const seen = new Set<string>();
+
+  for (const c of s) {
+    if (seen.has(c)) return c;
+    seen.add(c);
+  }
+
+  return "";
+};
+
+describe("first repeat", () => {
+  it("case 1", () => {
+    expect(repeatedCharacter("abacabad")).toBe("a");
+  });
+  it("case 2", () => {
+    expect(repeatedCharacter("abacabaabacaba")).toBe("a");
+  });
+  it("case 3", () => {
+    expect(repeatedCharacter("abcdefghijklmnopqrstuvwxyziflskecznslkjfabe")).toBe("i");
+  });
+  it("case 4", () => {
+    expect(repeatedCharacter("bcccccccb")).toBe("c");
+  });
+});
