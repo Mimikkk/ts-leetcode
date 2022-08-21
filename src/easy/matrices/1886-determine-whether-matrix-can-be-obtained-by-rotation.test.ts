91861968458fd0c394a57126adbbf6e4ed54a87b
Hououin Kyouma <inwazzin@gmail.com>
2022-08-22 00:35:03 +0200

    1886-determine-whether-matrix-can-be-obtained-by-rotation.test.ts

---
+++ src/easy/matrices/1886-determine-whether-matrix-can-be-obtained-by-rotation.test.ts
@@ -0,0 +1,72 @@
+import { M } from "../../shared/modules";
+
+const findRotation = (mat: number[][], target: number[][]) => {
+  for (let i = 0; i < 4; ++i) {
+    if (M.equal(mat, target)) return true;
+    mat = M.rotateBy90Degrees(mat);
+  }
+  return false;
+};
+
+describe("find rotation", () => {
+  it("case 1", () => {
+    expect(
+      findRotation(
+        [
+          [1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9],
+        ],
+        [
+          [1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9],
+        ],
+      ),
+    ).toBe(true);
+  });
+  it("case 2", () => {
+    expect(
+      findRotation(
+        [
+          [0, 1],
+          [1, 0],
+        ],
+        [
+          [1, 0],
+          [0, 1],
+        ],
+      ),
+    ).toBe(true);
+  });
+  it("case 3", () => {
+    expect(
+      findRotation(
+        [
+          [0, 1],
+          [1, 1],
+        ],
+        [
+          [1, 0],
+          [0, 1],
+        ],
+      ),
+    ).toBe(false);
+  });
+  it("case 4", () => {
+    expect(
+      findRotation(
+        [
+          [0, 0, 0],
+          [0, 1, 0],
+          [1, 1, 1],
+        ],
+        [
+          [1, 1, 1],
+          [0, 1, 0],
+          [0, 0, 0],
+        ],
+      ),
+    ).toBe(true);
+  });
+});
