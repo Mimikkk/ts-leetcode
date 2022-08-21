507305474395da8a664549b5875f5f63585b6678
Hououin Kyouma <inwazzin@gmail.com>
2022-08-22 00:35:39 +0200

    2264-largest-3-same-digit-number-in-string.test.ts

---
+++ src/easy/strings/single/2264-largest-3-same-digit-number-in-string.test.ts
@@ -0,0 +1,26 @@
+export {};
+
+const largestGoodInteger = (num: string) => {
+  let max = undefined;
+
+  for (let i = 0; i < num.length - 2; ++i) {
+    if (
+      num[i] === num[i + 1] &&
+      num[i] === num[i + 2] &&
+      (max === undefined || +num.substring(i, i + 3) > +max)
+    )
+      max = num.substring(i, i + 3);
+  }
+
+  return max ?? "";
+};
+
+describe("largest 3 same digit num", () => {
+  it("case 1", () => {
+    expect(largestGoodInteger("123")).toBe("");
+    expect(largestGoodInteger("000111222")).toBe("222");
+    expect(largestGoodInteger("0000")).toBe("000");
+    expect(largestGoodInteger("2300019")).toBe("000");
+    expect(largestGoodInteger("222")).toBe("222");
+  });
+});
