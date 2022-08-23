0e14878a3de838aa38a468447f8382075bb95825
Hououin Kyouma <inwazzin@gmail.com>
2022-08-24 01:22:51 +0200

    2379-minimum-recolors-to-get-k-consecutive-black-blocks.test.ts

---
+++ src/easy/algorithms/2379-minimum-recolors-to-get-k-consecutive-black-blocks.test.ts
@@ -0,0 +1,29 @@
+import { A } from "../../shared/modules";
+import range = A.N.range;
+export {};
+
+const minimumRecolors = (blocks: string, k: number) => {
+  let whites = range(0, k - 1).filter((i) => blocks[i] == "W").length;
+
+  let min = whites;
+  for (let j = 0; j < blocks.length - k; ++j) {
+    if (blocks[j] == "W") --whites;
+    if (blocks[j + k] == "W") ++whites;
+
+    min = Math.min(min, whites);
+  }
+
+  return min;
+};
+
+describe("min recolors", () => {
+  it("case 1", () => {
+    expect(minimumRecolors("WW", 2)).toEqual(2);
+  });
+  it("case 2", () => {
+    expect(minimumRecolors("WBBWWBBWBW", 7)).toEqual(3);
+  });
+  it("case 3", () => {
+    expect(minimumRecolors("WBWBBBW", 2)).toEqual(0);
+  });
+});
