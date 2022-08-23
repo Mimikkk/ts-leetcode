406b2f7d9866a956efa49345192cfec9da5add43
Hououin Kyouma <inwazzin@gmail.com>
2022-08-24 01:22:42 +0200

    2367-number-of-arithmetic-triplets.test.ts

---
+++ src/easy/maths/2367-number-of-arithmetic-triplets.test.ts
@@ -0,0 +1,16 @@
+export {};
+
+const arithmeticTriplets = (nums: number[], diff: number) => {
+  let count = 0;
+  for (let i = 0; i < nums.length - 1; i++) {
+    let [triplets, e2] = [0, i];
+
+    for (let e1 = e2 + 1; e1 < nums.length; e1++) {
+      if (nums[e1] - nums[e2] != diff) continue;
+
+      e2 = e1;
+      if (++triplets == 2) ++count;
+    }
+  }
+  return count;
+};
