c0740aeed7c9e701584f7fcb8cb56f5acb86f2b8
Hououin Kyouma <inwazzin@gmail.com>
2022-08-24 01:22:21 +0200

    2335-minimum-amount-of-time-to-fill-cups.test.ts

---
+++ src/easy/arrays/algorithms/2335-minimum-amount-of-time-to-fill-cups.test.ts
@@ -0,0 +1,16 @@
+export {};
+
+const fillCups = (amount: number[]): number =>
+  Math.max(
+    Math.max(...amount),
+    Math.floor((amount.reduce((a, b) => a + b) + 1) / 2),
+  );
+
+describe("fill cups", () => {
+  it("case 1", () => {
+    expect(fillCups([1, 4, 2])).toEqual(4);
+  });
+  it("case 2", () => {
+    expect(fillCups([1, 4, 2])).toEqual(4);
+  });
+});
