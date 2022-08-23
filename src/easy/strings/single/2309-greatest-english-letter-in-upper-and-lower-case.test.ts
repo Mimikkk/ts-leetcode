b606087139335c76712cf241b071854848ad017c
Hououin Kyouma <inwazzin@gmail.com>
2022-08-23 02:32:09 +0200

    2309-greatest-english-letter-in-upper-and-lower-case.test.ts

---
+++ src/easy/strings/single/2309-greatest-english-letter-in-upper-and-lower-case.test.ts
@@ -0,0 +1,17 @@
+export {};
+
+function greatestLetter(s: string): string {
+  let letters = [...new Set(s)].sort((a, b) => a.localeCompare(b));
+
+  for (let i = letters.length - 1; i >= 0; --i) {
+    if (letters[i + 1] === letters[i].toUpperCase()) return letters[i + 1];
+  }
+
+  return "";
+}
+
+describe("greatest letter", () => {
+  it("case 1", () => {
+    expect(greatestLetter("aAbB")).toEqual("B");
+  });
+});
