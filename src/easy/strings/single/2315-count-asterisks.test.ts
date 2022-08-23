6c8401d336b4091b9bd4c838fc65a05798b5e593
Hououin Kyouma <inwazzin@gmail.com>
2022-08-23 02:32:12 +0200

    2315-count-asterisks.test.ts

---
+++ src/easy/strings/single/2315-count-asterisks.test.ts
@@ -0,0 +1,10 @@
+export {};
+
+const countAsterisks = (s: string) =>
+  s.replace(/[^|*]/g, '').replace(/\|.*?\|/g, '').length;
+
+describe("count asterisks", () => {
+  it("case 1", () => {
+    expect(countAsterisks("|*|*||")).toEqual(1);
+  });
+});
