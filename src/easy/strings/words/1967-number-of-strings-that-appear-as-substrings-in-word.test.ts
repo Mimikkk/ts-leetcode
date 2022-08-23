7279676fdddfa67b1ea41f9396fc1ba339703606
Hououin Kyouma <inwazzin@gmail.com>
2022-08-23 02:31:46 +0200

    1967-number-of-strings-that-appear-as-substrings-in-word.test.ts

---
+++ src/easy/strings/words/1967-number-of-strings-that-appear-as-substrings-in-word.test.ts
@@ -0,0 +1,16 @@
+export {};
+
+const numOfStrings = (patterns: string[], word: string) =>
+  patterns.filter((pattern) => word.includes(pattern)).length;
+
+describe("num of strings", () => {
+  it("case 1", () => {
+    expect(numOfStrings(["a", "abc", "bc", "d"], "abc")).toEqual(3);
+  });
+  it("case 2", () => {
+    expect(numOfStrings(["a", "b", "c"], "aaaaabbbbb")).toEqual(2);
+  });
+  it("case 3", () => {
+    expect(numOfStrings(["a", "a", "a"], "ab")).toEqual(3);
+  });
+});
