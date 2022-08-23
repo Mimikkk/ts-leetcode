a72b606762073f1b4fdfb38a7902aeff2796d6ef
Hououin Kyouma <inwazzin@gmail.com>
2022-08-24 01:22:18 +0200

    2325-decode-the-message.test.ts

---
+++ src/easy/strings/single/2325-decode-the-message.test.ts
@@ -0,0 +1,26 @@
+export {};
+
+const decodeMessage = (key: string, message: string): string => {
+  const unique = [...new Set(key.split(" ").join(""))];
+  const translator: Record<string, string> = {};
+
+  for (let i = 0; i < 26; ++i) {
+    translator[unique[i]] = String.fromCharCode(i + 97);
+  }
+
+  return [...message].map((c) => translator[c] || c).join("");
+};
+
+describe("decode message", () => {
+  it("case 1", () => {
+    expect(decodeMessage("cba", "abc")).toEqual("cba");
+  });
+  it("case 2", () => {
+    expect(
+      decodeMessage(
+        "the quick brown fox jumps over the lazy dog",
+        "vkbs bs t suepuv",
+      ),
+    ).toEqual("this is a secret");
+  });
+});
