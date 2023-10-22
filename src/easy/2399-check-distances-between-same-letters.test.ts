18878a592a824c8fdb5fce9d65061ecd0ba93243
Hououin Kyouma <inwazzin@gmail.com>
2023-10-22 23:47:44 +0200

    2399, 2409

---
+++ src/easy/2399-check-distances-between-same-letters.test.ts
@@ -0,0 +1,51 @@
+import { expect } from "vitest";
+export {};
+
+const checkDistances = (s: string, distance: number[]): boolean => {
+  const distanceBetweenOccurrencesByChar = Object.fromEntries(
+    distance.map((d, i) => [String.fromCharCode("a".charCodeAt(0) + i), d]),
+  );
+
+  const occurred = new Map<string, number>();
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    const lastOccurrence = occurred.get(char);
+    if (lastOccurrence !== undefined) {
+      const distanceBetweenOccurrences = i - lastOccurrence - 1;
+
+      if (distanceBetweenOccurrencesByChar[char] !== distanceBetweenOccurrences)
+        return false;
+    }
+
+    occurred.set(char, i);
+  }
+
+  return true;
+};
+
+describe("2399", () => {
+  it("case 1", () => {
+    expect(
+      checkDistances(
+        "abaccb",
+        [
+          1, 3, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0,
+        ],
+      ),
+    ).toBe(true);
+  });
+
+  it("case 2", () => {
+    expect(
+      checkDistances(
+        "aa",
+        [
+          1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+          0, 0, 0,
+        ],
+      ),
+    ).toBe(false);
+  });
+});
---
+++ src/easy/2409-count-days-spent-together.test.ts
@@ -0,0 +1,29 @@
+import { expect } from "vitest";
+export {};
+
+const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const add = (a: number, b: number) => a + b;
+const sum = (arr: number[]) => arr.reduce(add, 0);
+const transform = (date: string) => date.split("-").map((x) => +x) as [number, number];
+const dateToDayInYear = ([month, day]: [number, number]) => sum(daysInMonth.slice(0, month - 1)) + day;
+const strToDayInYear = (date: string) => dateToDayInYear(transform(date));
+
+const overlapInDays = (startADay: number, endADay: number, startBDay: number, endBDay: number): number =>
+  Math.max(Math.min(endADay, endBDay) - Math.max(startADay, startBDay) + 1, 0);
+
+const countDaysTogether = (arriveA: string, leaveA: string, arriveB: string, leaveB: string): number =>
+  overlapInDays(strToDayInYear(arriveA), strToDayInYear(leaveA), strToDayInYear(arriveB), strToDayInYear(leaveB));
+
+describe("2409", () => {
+  it("case 1", () => {
+    expect(countDaysTogether("08-15", "08-18", "08-16", "08-19")).toBe(3);
+  });
+
+  it("case 2", () => {
+    expect(countDaysTogether("10-01", "10-31", "11-01", "12-31")).toBe(0);
+  });
+
+  it("case 3", () => {
+    expect(countDaysTogether("10-20", "12-22", "06-21", "07-05")).toBe(0);
+  });
+});
