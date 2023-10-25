e494f300aeaee92a2cccd3bdc12c0eec0ead2a70
Hououin Kyouma <inwazzin@gmail.com>
2023-10-25 12:41:32 +0200

    2455, 2460

---
+++ src/easy/2455-average-value-of-even-numbers-that-are-divisible-by-three.test.ts
@@ -0,0 +1,36 @@
+import { exercise } from "@shared/utilities/exercise";
+export {};
+
+const averageValue = (nums: number[]): number => {
+  let values = 0;
+  let count = 0;
+
+  for (const num of nums) {
+    if (num % 6 === 0) {
+      values += num;
+      count += 1;
+    }
+  }
+
+  return count ? Math.floor(values / count) : 0;
+};
+
+exercise(2455, averageValue, [
+  {
+    input: [[1, 3, 6, 10, 12, 15]],
+    output: 9,
+  },
+  {
+    input: [[1, 2, 4, 7, 10]],
+    output: 0,
+  },
+  {
+    input: [
+      [
+        94, 65, 82, 40, 79, 74, 92, 84, 37, 19, 16, 85, 20, 79, 25, 89, 55, 67, 84, 3, 79, 38, 16, 44, 2, 54, 58, 94,
+        69, 71, 14, 24, 13, 21,
+      ],
+    ],
+    output: 61,
+  },
+]);
---
+++ src/easy/2460-apply-operations-to-an-array.test.ts
@@ -0,0 +1,23 @@
+import { exercise } from "@shared/utilities/exercise";
+export {};
+
+const applyOperations = (nums: number[]): number[] => {
+  const result: number[] = [];
+
+  return result;
+};
+
+exercise(2455, applyOperations, [
+  {
+    input: [[1, 2, 2, 1, 1, 0]],
+    output: [1, 4, 2, 0, 0, 0],
+  },
+  {
+    input: [[0, 1]],
+    output: [1, 0],
+  },
+  {
+    input: [[1, 2, 3, 4, 5]],
+    output: [1, 2, 3, 4, 5],
+  },
+]);
