f02d79778b47f12620d517383d6f1c05e21f979b
Hououin Kyouma <inwazzin@gmail.com>
2023-10-29 22:04:47 +0100

    2460

--- src/easy/2460-apply-operations-to-an-array.test.ts
+++ src/easy/2460-apply-operations-to-an-array.test.ts
@@ -2,12 +2,33 @@ import { exercise } from "@shared/utilities/exercise";
 export {};
 
 const applyOperations = (nums: number[]): number[] => {
-  const result: number[] = [];
+  for (let i = 1; i < nums.length; ++i) {
+    if (nums[i - 1] === nums[i]) {
+      nums[i - 1] *= 2;
+      nums[i] = 0;
+    }
+  }
+
+  const result = Array.from<number>({ length: nums.length }).fill(0);
+
+  let i = 0;
+  let k = 0;
+  while (i + k < result.length) {
+    if (nums[i + k] === 0) ++k;
+    else {
+      result[i] = nums[i + k];
+      ++i;
+    }
+  }
 
   return result;
 };
 
-exercise(2455, applyOperations, [
+exercise(2460, applyOperations, [
+  {
+    input: [[1, 0, 2, 0, 0, 1]],
+    output: [1, 2, 1, 0, 0, 0],
+  },
   {
     input: [[1, 2, 2, 1, 1, 0]],
     output: [1, 4, 2, 0, 0, 0],
--- src/shared/utilities/exercise.ts
+++ src/shared/utilities/exercise.ts
@@ -3,9 +3,9 @@ import { describe, expect, it } from "vitest";
 export const exercise = <T, Y = any, Fn extends (...args: T[]) => Y = (...args: T[]) => Y>(
   number: number,
   fn: Fn,
-  cases: { input: T[]; output: Y }[],
+  cases: ({ input: T[]; output: Y } | [T[], Y])[],
 ) =>
   describe(`${number}`, () =>
-    it.each(cases.map((c, i) => ({ ...c, index: i + 1 })))("case $index: $input -> $output", ({ input, output }) =>
-      expect(fn(...input)).toEqual(output),
-    ));
+    it.each(
+      cases.map((c) => (Array.isArray(c) ? { input: c[0], output: c[1] } : c)).map((c, i) => ({ ...c, index: i + 1 })),
+    )("case $index: $input -> $output", ({ input, output }) => expect(fn(...input)).toEqual(output)));
