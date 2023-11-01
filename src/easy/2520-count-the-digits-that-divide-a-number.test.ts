61f231c64ef60948c8b8b51d04848ca94d251896
Hououin Kyouma <inwazzin@gmail.com>
2023-11-01 19:49:48 +0100

    2520

---
+++ src/easy/2520-count-the-digits-that-divide-a-number.test.ts
@@ -0,0 +1,21 @@
+import { exercise } from "@shared/utilities/exercise";
+
+const divmod = (n: number, mod: number) => [~~(n / mod), n % mod];
+const countDigits = (num: number): number => {
+  let count = 0;
+
+  let digit;
+  let div = num;
+  while (div > 0) {
+    [div, digit] = divmod(div, 10);
+    if (num % digit === 0) ++count;
+  }
+
+  return count;
+};
+
+exercise(countDigits, [
+  [[7], 1],
+  [[121], 2],
+  [[1248], 4],
+]);
