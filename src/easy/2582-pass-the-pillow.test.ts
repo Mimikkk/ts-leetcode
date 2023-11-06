38655718ff6c500895a512bedc6c7343426c3ec5
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2023-11-06 15:18:28 +0100

    2582

--- src/easy/2582-pass-the-pillow.test.ts
+++ src/easy/2582-pass-the-pillow.test.ts
@@ -1,26 +1,18 @@
 import { exercise } from "@shared/utilities/exercise";
 
-const passThePillow = (n: number, time: number): number => {
-  // 4 | 3 2 1 | 2 3 4 | 3 2 1 | 2 3 4 ...
-  const offset = n - ((n + ((n - time) % n)) % n);
-  const dance2 = ~~(time / n);
-  const dance3 = ~~(time / n) % 2;
-
-  console.log(n, time, offset, dance2, dance3, n - offset, dance3 === 1 ? offset - n : n - offset);
-
-  return 0;
-};
+const passThePillow = (n: number, time: number): number =>
+  (time / (n - 1)) & 1 ? n - (time % (n - 1)) : 1 + (time % (n - 1));
 
 exercise(passThePillow, [
-  [[4, 0], 4],
-  [[4, 1], 3],
-  [[4, 2], 2],
-  [[4, 3], 1],
-  [[4, 4], 2],
-  [[4, 5], 3],
-  [[4, 6], 4],
-  [[4, 7], 3],
-  [[4, 8], 2],
-  [[4, 9], 1],
-  [[4, 10], 2],
+  [[4, 0], 1],
+  [[4, 1], 2],
+  [[4, 2], 3],
+  [[4, 3], 4],
+  [[4, 4], 3],
+  [[4, 5], 2],
+  [[4, 6], 1],
+  [[4, 7], 2],
+  [[4, 8], 3],
+  [[4, 9], 4],
+  [[4, 10], 3],
 ]);
