333eadd17a52a8a6744483f2c0cc4cf97ee2731a
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2023-11-13 14:11:50 +0100

    2586

--- src/easy/2586-count-the-number-of-vowel-strings-in-range.test.ts
+++ src/easy/2586-count-the-number-of-vowel-strings-in-range.test.ts
@@ -1,9 +1,13 @@
 import { exercise } from "@shared/utilities/exercise";
 
+const vowels = new Set(["a", "e", "i", "o", "u"]);
 const vowelStrings = (words: string[], left: number, right: number): number => {
-  console.log(words.splice(left, right));
+  words = words.slice(left, right + 1);
+  let count = 0;
 
-  return 0;
+  for (let word of words) if (vowels.has(word[0]) && vowels.has(word[word.length - 1])) ++count;
+
+  return count;
 };
 
 exercise(vowelStrings, [
