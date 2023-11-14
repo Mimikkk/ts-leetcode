7829d6b8315de240fdb1bd7a25d5683e22b61171
Hououin Kyouma <inwazzin@gmail.com>
2023-11-14 22:12:35 +0100

    2667, 2697, 2724

---
+++ src/easy/2697-lexicographically-smallest-palindrome.test.ts
@@ -0,0 +1,30 @@
+import { exercise } from "@shared/utilities/exercise";
+
+const lexicallySmaller = (a: string, b: string): string => (a < b ? a : b);
+const makeSmallestPalindrome = (s: string): string => {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      const smaller = lexicallySmaller(s[left], s[right]);
+
+      if (s[right] === smaller) {
+        s = s.slice(0, left) + smaller + s.slice(left + 1);
+      } else {
+        s = s.slice(0, right) + smaller + s.slice(right + 1);
+      }
+    }
+
+    ++left;
+    --right;
+  }
+
+  return s;
+};
+
+exercise(makeSmallestPalindrome, [
+  [["egcfe"], "efcfe"],
+  [["abcd"], "abba"],
+  [["seven"], "neven"],
+]);
---
+++ src/easy/2724-sort-by.test.ts
@@ -0,0 +1,13 @@
+import { exercise } from "@shared/utilities/exercise";
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn<T extends JSONValue> = (value: T) => number;
+
+const sortBy = <T extends JSONValue>(arr: T[], fn: Fn<T>): T[] => arr.sort((a, b) => fn(a) - fn(b));
+
+exercise(sortBy, [
+  [
+    [[5, 4, 1, 2, 3], (x) => x as number],
+    [1, 2, 3, 4, 5],
+  ],
+]);
