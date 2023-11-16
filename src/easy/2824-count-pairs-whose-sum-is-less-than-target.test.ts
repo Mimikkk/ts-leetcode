96f6c15fa3f968d3da0b97f50a3ca25b3538002a
Hououin Kyouma <inwazzin@gmail.com>
2023-11-16 17:49:20 +0100

    2666, 2715, 2824

---
+++ src/easy/2824-count-pairs-whose-sum-is-less-than-target.test.ts
@@ -0,0 +1,13 @@
+import { exercise } from "@shared/utilities/exercise";
+
+const countPairs = (nums: number[], target: number): number => {
+  let n = nums.length;
+  let count = 0;
+  for (let i = 0; i < n - 1; ++i) for (let j = i + 1; j < n; ++j) if (nums[i] + nums[j] < target) ++count;
+  return count;
+};
+
+exercise(countPairs, [
+  [[[-1, 1, 2, 3, 1], 2], 3],
+  [[[-6, 2, 5, -2, -7, -1, 3], -2], 10],
+]);
