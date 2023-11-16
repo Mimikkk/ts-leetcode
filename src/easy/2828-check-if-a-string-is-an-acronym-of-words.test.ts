fd0bf446df2ba2de805eb84b063ae16a378f83ee
Hououin Kyouma <inwazzin@gmail.com>
2023-11-16 21:45:44 +0100

    2828, 2913

---
+++ src/easy/2828-check-if-a-string-is-an-acronym-of-words.test.ts
@@ -0,0 +1,13 @@
+import { exercise } from "@shared/utilities/exercise";
+
+const isAcronym = (words: string[], s: string): boolean => {
+  if (words.length !== s.length) return false;
+
+  for (let i = 0; i < s.length; ++i) if (words[i][0] !== s[i]) return false;
+  return true;
+};
+
+exercise(isAcronym, [
+  [[["cd", "ac", "dc", "ca", "zz"], "z"], false],
+  [[["a", "a", "a"], "aaa"], true],
+]);
