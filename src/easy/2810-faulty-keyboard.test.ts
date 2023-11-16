121d837f5f3721230b9d4dfa16380a31f4d4f302
Hououin Kyouma <inwazzin@gmail.com>
2023-11-16 22:14:07 +0100

    2677,2810,2932

---
+++ src/easy/2810-faulty-keyboard.test.ts
@@ -0,0 +1,17 @@
+import { exercise } from "@shared/utilities/exercise";
+
+const finalString = (s: string): string => {
+  let result = [];
+
+  for (let c of s) {
+    if (c === "i") result.reverse();
+    else result.push(c);
+  }
+
+  return result.join("");
+};
+
+exercise(finalString, [
+  [["string"], "rtsng"],
+  [["poiinter"], "ponter"],
+]);
