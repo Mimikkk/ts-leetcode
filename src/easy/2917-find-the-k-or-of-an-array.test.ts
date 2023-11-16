3f66b1d9d8f292a0177a759ee46874037121a865
Hououin Kyouma <inwazzin@gmail.com>
2023-11-16 22:45:27 +0100

    2716, 2917

---
+++ src/easy/2917-find-the-k-or-of-an-array.test.ts
@@ -0,0 +1,32 @@
+import { exercise } from "@shared/utilities/exercise";
+
+const findKOr = (nums: number[], k: number): number => {
+  let max = 0;
+  for (let i = 0; i < nums.length; ++i) if (nums[i] > max) max = nums[i];
+  const bitCount = Math.max(0, Math.ceil(Math.log2(max)));
+  const bitCounts = Array(bitCount).fill(0);
+
+  for (let i = 0; i < bitCount; ++i) {
+    for (let j = 0; j < nums.length; ++j) {
+      if (nums[j] === 0) continue;
+
+      if (nums[j] & 1) ++bitCounts[i];
+      nums[j] >>= 1;
+    }
+  }
+
+  let kor = 0;
+  let value = 1;
+  for (let i = 0; i < bitCount; ++i) {
+    if (bitCounts[i] >= k) kor += value;
+    value <<= 1;
+  }
+
+  return kor;
+};
+
+exercise(findKOr, [
+  [[[7, 12, 9, 8, 9, 15], 4], 9],
+  [[[2, 12, 1, 11, 4, 5], 6], 0],
+  [[[10, 8, 5, 9, 11, 6, 8], 1], 15],
+]);
