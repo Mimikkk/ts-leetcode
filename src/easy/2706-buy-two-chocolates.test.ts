097a55f37de5b8369b28994183ed26ff11d483d8
Hououin Kyouma <inwazzin@gmail.com>
2023-11-18 12:47:21 +0100

    2706, 2833

---
+++ src/easy/2706-buy-two-chocolates.test.ts
@@ -0,0 +1,13 @@
+import { exercise } from "@shared/utilities/exercise";
+
+const buyChoco = (prices: number[], money: number): number => {
+  const [first, second] = prices.sort((a, b) => a - b);
+  const price = first + second;
+
+  return price > money ? money : money - price;
+};
+
+exercise(buyChoco, [
+  [[[1, 2, 2], 3], 0],
+  [[[3, 2, 3], 3], 3],
+]);
---
+++ src/easy/2833-furthest-point-from-origin.test.ts
@@ -0,0 +1,32 @@
+import { exercise } from "@shared/utilities/exercise";
+
+const furthestDistanceFromOrigin = (moves: string): number => {
+  let countLeft = 0;
+  let countRight = 0;
+  let countUndefined = 0;
+
+  for (let i = 0; i < moves.length; ++i) {
+    switch (moves[i]) {
+      case "L": {
+        ++countLeft;
+        break;
+      }
+      case "R": {
+        ++countRight;
+        break;
+      }
+      default: {
+        ++countUndefined;
+      }
+    }
+  }
+
+  return Math.abs(countLeft - countRight) + countUndefined;
+};
+
+exercise(furthestDistanceFromOrigin, [
+  [["RRRLLL"], 0],
+  [["L_RL__R"], 3],
+  [["_R__LL_"], 5],
+  [["_______"], 7],
+]);
