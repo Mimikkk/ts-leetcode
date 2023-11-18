087795e7fc25b540d2cae8a67088865600d8e6d2
Hououin Kyouma <inwazzin@gmail.com>
2023-11-18 16:56:22 +0100

    2729

---
+++ src/easy/2729-check-if-the-number-is-fascinating.test.ts
@@ -0,0 +1,32 @@
+import { exercise } from "@shared/utilities/exercise";
+
+const readDigits = (n: number): number[] => {
+  const digits = [];
+
+  while (n) {
+    digits.unshift(n % 10);
+    n = ~~(n / 10);
+  }
+
+  return digits;
+};
+
+const isFascinating = (n: number): boolean => {
+  const digits = readDigits(n)
+    .concat(readDigits(n * 2))
+    .concat(readDigits(n * 3));
+
+  const counts = new Set();
+  for (const digit of digits) {
+    if (digit === 0 || counts.has(digit)) return false;
+    counts.add(digit);
+  }
+
+  return true;
+};
+
+exercise(isFascinating, [
+  [[192], true],
+  [[100], false],
+  [[783], false],
+]);
