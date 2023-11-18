e4211ccb8bdff5615a7c8ad244651584df9f1a2b
Hououin Kyouma <inwazzin@gmail.com>
2023-11-18 12:38:41 +0100

    2639, 2696, 2917 fix, 2932

--- src/easy/2917-find-the-k-or-of-an-array.test.ts
+++ src/easy/2917-find-the-k-or-of-an-array.test.ts
@@ -3,7 +3,7 @@ import { exercise } from "@shared/utilities/exercise";
 const findKOr = (nums: number[], k: number): number => {
   let max = 0;
   for (let i = 0; i < nums.length; ++i) if (nums[i] > max) max = nums[i];
-  const bitCount = Math.max(0, Math.ceil(Math.log2(max)));
+  const bitCount = Math.max(0, Math.floor(Math.log2(max) + 1));
   const bitCounts = Array(bitCount).fill(0);
 
   for (let i = 0; i < bitCount; ++i) {
@@ -29,4 +29,5 @@ exercise(findKOr, [
   [[[7, 12, 9, 8, 9, 15], 4], 9],
   [[[2, 12, 1, 11, 4, 5], 6], 0],
   [[[10, 8, 5, 9, 11, 6, 8], 1], 15],
+  [[[1], 1], 1],
 ]);
---
+++ src/easy/2932-find-champion-i.test.ts
@@ -0,0 +1,60 @@
+import { exercise } from "@shared/utilities/exercise";
+
+const maxBy = <T>(list: T[], fn: (item: T) => number): T | undefined => {
+  let max: T | undefined;
+  let maxValue = -Infinity;
+
+  for (const item of list) {
+    const value = fn(item);
+    if (value > maxValue) {
+      max = item;
+      maxValue = value;
+    }
+  }
+
+  return max;
+};
+const findChampion = (grid: number[][]): number => {
+  let maxRowIndex = 0;
+  let maxCount: number = 0;
+
+  const rowCount = grid.length;
+  for (let i = 0; i < rowCount; i++) {
+    const row = grid[i];
+
+    let count = 0;
+    const rowLength = grid[i].length;
+    for (let j = 0; j < rowLength; ++j) {
+      if (row[j] === 1) ++count;
+    }
+
+    if (count > maxCount) {
+      maxRowIndex = i;
+      maxCount = count;
+    }
+  }
+
+  return maxRowIndex;
+};
+
+exercise(findChampion, [
+  [
+    [
+      [
+        [0, 1],
+        [0, 0],
+      ],
+    ],
+    0,
+  ],
+  [
+    [
+      [
+        [0, 0, 1],
+        [1, 0, 1],
+        [0, 0, 0],
+      ],
+    ],
+    1,
+  ],
+]);
