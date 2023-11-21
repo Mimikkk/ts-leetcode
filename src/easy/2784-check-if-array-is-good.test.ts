77ec153f60c5cfd6100a88803a57832be7e175c3
Hououin Kyouma <inwazzin@gmail.com>
2023-11-21 17:07:52 +0100

    2784, 2806

---
+++ src/easy/2784-check-if-array-is-good.test.ts
@@ -0,0 +1,25 @@
+import { exercise } from "@shared/utilities/exercise";
+
+const isGood = (nums: number[]): boolean => {
+  const n = new Set(nums).size;
+  const values = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; ++i) {
+    if (nums[i] === n) {
+      if (values.get(nums[i]) === 2) return false;
+    } else if (values.has(nums[i])) return false;
+
+    values.set(nums[i], (values.get(nums[i]) ?? 0) + 1);
+  }
+
+  for (const n of values.keys()) if (n < 1 || n > n) return false;
+  return values.get(n) === 2;
+};
+
+exercise(isGood, [
+  [[[2, 1, 3]], false],
+  [[[1, 3, 3, 2]], true],
+  [[[1, 1]], true],
+  [[[3, 4, 4, 1, 2, 1]], false],
+  [[[14, 2, 2]], false],
+]);
--- src/easy/2806-account-balance-after-rounded-purchase.test.ts
+++ src/easy/2806-account-balance-after-rounded-purchase.test.ts
@@ -1,16 +1,8 @@
 import { exercise } from "@shared/utilities/exercise";
 
-const { max, log10 } = Math;
-const accountBalanceAfterPurchase = (unitPrice: number): number => {
-  const balance = 100;
-
-  console.log(~~(balance / unitPrice));
-
-  return balance;
-};
+const accountBalanceAfterPurchase = (unitsPurchased: number): number => 100 - 10 * Math.round(unitsPurchased / 10);
 
 exercise(accountBalanceAfterPurchase, [
   [[9], 90],
   [[15], 80],
-  [[100], 80],
 ]);
