69c051979414a5c6a25b64f449a99e1eff42bd0c
Hououin Kyouma <inwazzin@gmail.com>
2023-12-02 11:53:18 +0100

    2

---
+++ src/adventofcode2023/2-cube-conundrum-easy.test.ts
@@ -0,0 +1,35 @@
+import { exercise } from "@shared/utilities/exercise";
+import TestCase from "./2-cube-conundrum.case.txt?raw";
+import UserCase from "./2-cube-conundrum.user.txt?raw";
+
+namespace Bag {
+  export type Color = "red" | "green" | "blue";
+
+  const counts: Record<Color, number> = { red: 12, green: 13, blue: 14 };
+  export const isInvalid = (entries: [number, Color][]) => entries.some(([count, color]) => count > counts[color]);
+}
+
+const conundrum = (input: string): number => {
+  const lines = input.split("\r\n").filter((Line) => Line);
+
+  const bags = lines.map((line) =>
+    line.match(/(\d+) (\w+)/g)!.map((x) => {
+      const [count, color] = x.split(" ");
+      return [+count, color];
+    }),
+  ) as [number, Bag.Color][][];
+
+  let invalidIdSum = 0;
+
+  for (let i = 0; i < bags.length; i++) {
+    if (Bag.isInvalid(bags[i])) continue;
+    invalidIdSum += i + 1;
+  }
+
+  return invalidIdSum;
+};
+
+exercise(conundrum, [
+  [[TestCase], 8],
+  [[UserCase], 2476],
+]);
