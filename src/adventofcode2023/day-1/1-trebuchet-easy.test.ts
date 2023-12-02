b6116673294055e7d80c9e222bbdb91e037058d3
Hououin Kyouma <inwazzin@gmail.com>
2023-12-02 12:23:13 +0100

    add func version

--- src/adventofcode2023/day-1/1-trebuchet-easy.test.ts
+++ src/adventofcode2023/day-1/1-trebuchet-easy.test.ts
@@ -2,7 +2,7 @@ import { exercise } from "@shared/utilities/exercise";
 import UserCase from "./1-trebuchet.user.txt?raw";
 import TestCase from "./1-trebuchet-easy.case.txt?raw";
 
-namespace Digit {
+namespace IterDigit {
   const is = (c: string): boolean => c >= "0" && c <= "9";
 
   export const first = (s: string): string => {
@@ -17,19 +17,37 @@ namespace Digit {
   };
 }
 
-const trebuchet = (input: string): number => {
+const iterTrebuchet = (input: string): number => {
   const lines = input.split("\r\n").filter((Line) => Line);
 
   let result = 0;
-  const { first, last } = Digit;
   for (const line of lines) {
-    result += +`${first(line)}${last(line)}`;
+    result += 10 * +IterDigit.first(line) + +IterDigit.last(line);
   }
 
   return result;
 };
 
-exercise(trebuchet, [
+namespace FuncDigit {
+  const is = (c: string): boolean => c >= "0" && c <= "9";
+
+  export const first = (s: string[]): string => s.find(is)!;
+  export const last = (s: string[]): string => s.reverse().find(is)!;
+}
+const funcTrebuchet = (input: string): number =>
+  input
+    .split("\r\n")
+    .filter((Line) => Line)
+    .map((line) => [...line])
+    .map((line) => 10 * +FuncDigit.first(line) + +FuncDigit.last(line))
+    .reduce((a, b) => a + b, 0);
+
+exercise(iterTrebuchet, [
+  [[TestCase], 142],
+  [[UserCase], 55538],
+]);
+
+exercise(funcTrebuchet, [
   [[TestCase], 142],
   [[UserCase], 55538],
 ]);
