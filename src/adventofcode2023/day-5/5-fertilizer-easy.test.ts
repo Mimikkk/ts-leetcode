975e44ec79da9b6f9d5aa52727203dca64714cd4
Hououin Kyouma <inwazzin@gmail.com>
2023-12-05 10:08:26 +0100

    5 init

---
+++ src/adventofcode2023/day-5/5-fertilizer-easy.test.ts
@@ -0,0 +1,11 @@
+import { exercise } from "@shared/utilities/exercise.js";
+import TestCase from "./5-fertilizer.case.txt?raw";
+
+const fertilizer = (input: string): number => {
+  return 0;
+};
+
+exercise(fertilizer, [
+  [[TestCase], 13],
+  // [[UserCase], 18519],
+]);
