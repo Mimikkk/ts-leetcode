877bd07315380991071137effb7d2d3a7dc53501
Hououin Kyouma <inwazzin@gmail.com>
2023-12-05 13:23:51 +0100

    day5

--- src/adventofcode2023/day-5/5-fertilizer-easy.test.ts
+++ src/adventofcode2023/day-5/5-fertilizer-easy.test.ts
@@ -1,11 +1,73 @@
 import { exercise } from "@shared/utilities/exercise.js";
 import TestCase from "./5-fertilizer.case.txt?raw";
+import UserCase from "./5-fertilizer.user.txt?raw";
 
-const fertilizer = (input: string): number => {
-  return 0;
+type SeedMap = [number, number, number];
+
+export const parseFertilizer = (input: string): [number[], SeedMap[][]] => {
+  const lines = input.split(/\r?\n/);
+
+  const seeds = lines[0]
+    .split(" ")
+    .map((s) => +s)
+    .slice(1);
+
+  const maps: SeedMap[][] = Array(7);
+  for (let i = 0; i < 7; ++i) maps[i] = [];
+  for (let i = 3, j = 0, it = lines.length; i < it; i += 2, ++j) {
+    while (lines[i] !== "" && i < it) maps[j].push(lines[i++].split(" ").map((s) => +s) as SeedMap);
+  }
+
+  return [seeds, maps] as const;
 };
 
-exercise(fertilizer, [
-  [[TestCase], 13],
-  // [[UserCase], 18519],
+export const iterFertilizer = (input: string): number => {
+  const [seeds, maps] = parseFertilizer(input);
+
+  for (let i = 0, it = maps.length; i < it; ++i) {
+    let map = maps[i];
+
+    for (let j = 0, jt = seeds.length; j < jt; ++j) {
+      let seed = seeds[j];
+
+      let value: number | undefined = undefined;
+      for (let k = 0, kt = map.length; k < kt; ++k) {
+        const [destination, source, range] = map[k];
+
+        if (seed < source || seed >= source + range) continue;
+        value = destination + (seed - source);
+      }
+      if (value === undefined) value = seed;
+      seeds[j] = value;
+    }
+  }
+
+  let min = seeds[0];
+  for (let i = 1, it = seeds.length; i < it; ++i) if (seeds[i] < min) min = seeds[i];
+
+  return min;
+};
+
+const canMap = (seed: number, [, source, range]: SeedMap): boolean => seed >= source && seed < source + range;
+const mapRange = (seed: number, [destination, source]: SeedMap): number => destination + (seed - source);
+
+export const funcFertilizer = (input: string): number => {
+  let [seeds, maps] = parseFertilizer(input);
+
+  return Math.min(
+    ...maps.reduce(
+      (seeds, map) => seeds.map((seed) => mapRange(seed, map.find((range) => canMap(seed, range)) ?? [seed, seed, 0])),
+      seeds,
+    ),
+  );
+};
+
+exercise(iterFertilizer, [
+  [[TestCase], 35],
+  [[UserCase], 199602917],
+]);
+
+exercise(funcFertilizer, [
+  [[TestCase], 35],
+  [[UserCase], 199602917],
 ]);
--- src/adventofcode2023/day-5/5-fertilizer-hard.test.ts
+++ src/adventofcode2023/day-5/5-fertilizer-hard.test.ts
@@ -1,11 +1,40 @@
 import { exercise } from "@shared/utilities/exercise.js";
 import TestCase from "./5-fertilizer.case.txt?raw";
+import UserCase from "./5-fertilizer.user.txt?raw";
+import { parseFertilizer } from "./5-fertilizer-easy.test.js";
 
-const fertilizer = (input: string): number => {
-  return 0;
+export const iterFertilizer = (input: string): number => {
+  let [seeds, maps] = parseFertilizer(input);
+
+  let ranges: [number, number][] = Array(seeds.length / 2);
+  for (let i = 0; i < seeds.length; i += 2) ranges[i / 2] = [seeds[i], seeds[i + 1]];
+
+  for (const map of maps) {
+    const next: [number, number][] = [];
+
+    inner: for (const [start, end] of ranges) {
+      for (const [destination, source, range] of map) {
+        if (!(start < source + range && source < start + end)) continue;
+        const max = Math.max(start, source);
+        const min = Math.min(start + end, source + range) - max;
+
+        next.push([max - source + destination, min]);
+        if (max > start || max + min < start + end)
+          ranges.push([start, max - start], [max + min, start + end - max - min]);
+
+        continue inner;
+      }
+
+      next.push([start, end]);
+    }
+
+    ranges = next;
+  }
+
+  return Math.min(...ranges.map(([s]) => s));
 };
 
-exercise(fertilizer, [
-  [[TestCase], 13],
-  // [[UserCase], 18519],
+exercise(iterFertilizer, [
+  [[TestCase], 46],
+  [[UserCase], 2254686],
 ]);
