9bdb7b8bf6075d9bcf45437a0d892cac97210ad0
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2023-12-07 17:58:58 +0100

    cleanup

--- src/adventofcode2023/day-7/7-camel-cards-easy.test.ts
+++ src/adventofcode2023/day-7/7-camel-cards-easy.test.ts
@@ -3,52 +3,58 @@ import TestCase from "./7-camel-cards.case.txt?raw";
 import UserCase from "./7-camel-cards.user.txt?raw";
 
 const cards = "AKQJT98765432";
-type Hand = Record<string, number>;
 
-const readHand = (hand: string): Hand => {
-  let counter: Hand = {};
-  for (let card of hand) counter[card] = (counter[card] || 0) + 1;
-  return counter;
-};
+namespace Hand {
+  type Hand = { cards: string; values: Record<string, number> };
 
-const rankHand = (hand: Hand): number => {
-  let [first, second] = Object.values(hand).sort((a, b) => b - a);
+  const rank = (hand: Hand): number => {
+    let [first, second] = Object.values(hand.values).sort((a, b) => b - a);
 
-  const rank = () => {
-    if (first === 5) return 6;
-    if (first === 4) return 5;
-    if (first === 3) return second === 2 ? 4 : 3;
-    if (first === 2) return second === 2 ? 2 : 1;
-    return 0;
+    switch (first) {
+      case 5:
+        return 6;
+      case 4:
+        return 5;
+      case 3:
+        return second === 2 ? 4 : 3;
+      case 2:
+        return second === 2 ? 2 : 1;
+      default:
+        return 0;
+    }
   };
 
-  return rank();
-};
+  export const read = (cards: string): Hand => {
+    let hand: Hand = { cards, values: {} };
+    for (let card of cards) hand.values[card] = (hand.values[card] || 0) + 1;
+    return hand;
+  };
 
-const camelCards = (input: string): number => {
-  const hands = input
+  export const compare = (a: Hand, b: Hand): number => {
+    let aRank = rank(a);
+    let bRank = rank(b);
+    if (aRank !== bRank) return aRank - bRank;
+
+    for (let i = 0; i < 5; ++i) {
+      const aIndex = cards.indexOf(a.cards[i]);
+      const bIndex = cards.indexOf(b.cards[i]);
+      if (aIndex === bIndex) continue;
+      return bIndex - aIndex;
+    }
+
+    return 0;
+  };
+}
+
+const camelCards = (input: string): number =>
+  input
     .split(/\r?\n/)
     .filter((line) => line)
     .map((line) => line.split(/ +/))
-    .map(([cards, pot]) => [cards, +pot] as const)
-    .sort(([a], [b]) => {
-      let aRank = rankHand(readHand(a));
-      let bRank = rankHand(readHand(b));
-      if (aRank !== bRank) return aRank - bRank;
-
-      for (let i = 0; i < 5; ++i) {
-        const aIndex = cards.indexOf(a[i]);
-        const bIndex = cards.indexOf(b[i]);
-        if (aIndex === bIndex) continue;
-        return bIndex - aIndex;
-      }
-
-      return 0;
-    })
-    .map(([, pot], index) => [pot, index + 1] as const);
-
-  return hands.reduce((a, [value, rank]) => a + value * rank, 0);
-};
+    .map(([cards, pot]) => [Hand.read(cards), +pot] as const)
+    .sort(([a], [b]) => Hand.compare(a, b))
+    .map(([, pot], index) => [pot, index + 1] as const)
+    .reduce((a, [value, rank]) => a + value * rank, 0);
 
 exercise(camelCards, [
   [[TestCase], 6440],
