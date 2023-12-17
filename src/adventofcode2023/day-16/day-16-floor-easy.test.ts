1b0a1b2a2d97b2cd526799090b3c7c8aa5edca8e
Hououin Kyouma <inwazzin@gmail.com>
2023-12-17 13:03:18 +0100

    day-17 init

--- src/adventofcode2023/day-16/day-16-floor-easy.test.ts
+++ src/adventofcode2023/day-16/day-16-floor-easy.test.ts
@@ -3,7 +3,7 @@ import TestCase from "./day-16-floor.case.txt?raw";
 import UserCase from "./day-16-floor.user.txt?raw";
 import { Floor } from "./day-16-floor.utils.js";
 
-const floor = (input: string): number => Floor.explore(Floor.parse(input), [0, 0], "right");
+const floor = (input: string): number => Floor.explore(Floor.parse(input), [[0, 0], "right"]);
 
 exercise(floor, [
   [[TestCase], 46],
---
+++ src/adventofcode2023/day-17/day-17-crucible-hard.test.ts
@@ -0,0 +1,10 @@
+import { exercise } from "@shared/utilities/exercise.js";
+import TestCase from "./day-17-crucible.case.txt?raw";
+import UserCase from "./day-17-crucible.user.txt?raw";
+
+const crucible = (input: string): number => 0;
+
+exercise(crucible, [
+  [[TestCase], 51],
+  [[UserCase], 8026],
+]);
