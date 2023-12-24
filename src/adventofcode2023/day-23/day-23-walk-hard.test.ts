97b23e7f0c4ca68cf1729e26ed7469b1cb3935fb
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2023-12-25 00:34:22 +0100

    day23,day24,day25 init

---
+++ src/adventofcode2023/day-23/day-23-walk-hard.test.ts
@@ -0,0 +1,10 @@
+import { exercise } from "@shared/utilities/exercise.js";
+import TestCase from "./day-23-walk.case.txt?raw";
+import UserCase from "./day-23-walk.user.txt?raw";
+
+const walk = (input: string): number => 0;
+
+exercise(walk, [
+  [[TestCase], 5],
+  [[UserCase], 3617],
+]);
