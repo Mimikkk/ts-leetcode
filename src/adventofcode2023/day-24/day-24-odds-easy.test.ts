e14f0dc505091202f400f711350b82ce665ce4bd
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2023-12-26 04:20:18 +0100

    day-24

--- src/adventofcode2023/day-24/day-24-odds-easy.test.ts
+++ src/adventofcode2023/day-24/day-24-odds-easy.test.ts
@@ -2,9 +2,75 @@ import { exercise } from "@shared/utilities/exercise.js";
 import TestCase from "./day-24-odds.case.txt?raw";
 import UserCase from "./day-24-odds.user.txt?raw";
 
-const odds = (input: string): number => 0;
+const odds = (input: string): number => {
+  interface Hailstone {
+    px: number;
+    py: number;
+    pz: number;
+    vx: number;
+    vy: number;
+    vz: number;
+  }
+
+  const findIntersection = (
+    p1x: number,
+    v1x: number,
+    p1y: number,
+    v1y: number,
+    p2x: number,
+    v2x: number,
+    p2y: number,
+    v2y: number,
+  ) => {
+    const [x1, x2, y1, y2] = [p1x, p1x + 100000000000000 * v1x, p1y, p1y + 100000000000000 * v1y];
+    const [x3, x4, y3, y4] = [p2x, p2x + 100000000000000 * v2x, p2y, p2y + 100000000000000 * v2y];
+
+    const denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+    if (denominator === 0) return false;
+
+    const x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / denominator;
+    const y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denominator;
+
+    return { x, y };
+  };
+
+  const time = (s: number, v: number, p: number) => (p - s) / v;
+
+  const hailstones: Hailstone[] = [];
+  const min = 200000000000000;
+  const max = 400000000000000;
+
+  for (const line of input.split(/\r?\n/).filter((x) => x)) {
+    const [positions, velocity] = line.split(" @ ");
+    const [px, py, pz] = positions.split(", ").map((n) => Number(n));
+    const [vx, vy, vz] = velocity.split(", ").map((n) => Number(n));
+
+    hailstones.push({ px, py, pz, vx, vy, vz });
+  }
+
+  let count = 0;
+  for (let i = 0, it = hailstones.length; i < it; ++i) {
+    for (let j = i + 1; j < it; ++j) {
+      const h1 = hailstones[i];
+      const h2 = hailstones[j];
+
+      const intersection = findIntersection(h1.px, h1.vx, h1.py, h1.vy, h2.px, h2.vx, h2.py, h2.vy);
+      if (!intersection) continue;
+
+      const { x, y } = intersection;
+      if (x < min || x > max || y < min || y > max) continue;
+
+      const timeH1 = time(h1.px, h1.vx, x);
+      const timeH2 = time(h2.px, h2.vx, x);
+      if (timeH1 < 0 || timeH2 < 0) continue;
+
+      count++;
+    }
+  }
+  return count;
+};
 
 exercise(odds, [
-  [[TestCase], 5],
-  [[UserCase], 3617],
+  [[TestCase], 0],
+  [[UserCase], 27732],
 ]);
--- src/adventofcode2023/day-24/day-24-odds-hard.test.ts
+++ src/adventofcode2023/day-24/day-24-odds-hard.test.ts
@@ -2,9 +2,92 @@ import { exercise } from "@shared/utilities/exercise.js";
 import TestCase from "./day-24-odds.case.txt?raw";
 import UserCase from "./day-24-odds.user.txt?raw";
 
-const odds = (input: string): number => 0;
+const odds = (input: string): number => {
+  interface Hailstone {
+    px: number;
+    py: number;
+    pz: number;
+    vx: number;
+    vy: number;
+    vz: number;
+  }
+
+  const hailstones: Hailstone[] = [];
+
+  const velocitiesX: Record<number, number[]> = {};
+  const velocitiesY: Record<number, number[]> = {};
+  const velocitiesZ: Record<number, number[]> = {};
+
+  const getRockVelocity = (velocities: Record<number, number[]>): number => {
+    let possibleV: number[] = [];
+    for (let x = -1000; x <= 1000; x++) possibleV.push(x);
+
+    Object.keys(velocities).forEach((velocity) => {
+      const vel = +velocity;
+      if (velocities[vel].length < 2) return;
+
+      let newPossibleV: number[] = [];
+      possibleV.forEach((possible) => {
+        if ((velocities[vel][0] - velocities[vel][1]) % (possible - vel) === 0) {
+          newPossibleV.push(possible);
+        }
+      });
+
+      possibleV = newPossibleV;
+    });
+
+    return possibleV[0];
+  };
+
+  for (const line of input.split(/\r?\n/).filter((x) => x)) {
+    const [positions, velocity] = line.split(" @ ");
+    const [px, py, pz] = positions.split(", ").map((n) => +n);
+    const [vx, vy, vz] = velocity.split(", ").map((n) => +n);
+
+    if (!velocitiesX[vx]) velocitiesX[vx] = [px];
+    else velocitiesX[vx].push(px);
+
+    if (!velocitiesY[vy]) velocitiesY[vy] = [py];
+    else velocitiesY[vy].push(py);
+
+    if (!velocitiesZ[vz]) velocitiesZ[vz] = [pz];
+    else velocitiesZ[vz].push(pz);
+
+    hailstones.push({ px, py, pz, vx, vy, vz });
+  }
+
+  const rvx = getRockVelocity(velocitiesX);
+  const rvy = getRockVelocity(velocitiesY);
+  const rvz = getRockVelocity(velocitiesZ);
+
+  const results: Record<number, number> = {};
+
+  for (let i = 0, it = hailstones.length; i < it; ++i) {
+    for (let j = i + 1; j < it; ++j) {
+      const stoneA = hailstones[i];
+      const stoneB = hailstones[j];
+
+      const ma = (stoneA.vy - rvy) / (stoneA.vx - rvx);
+      const mb = (stoneB.vy - rvy) / (stoneB.vx - rvx);
+      const ca = stoneA.py - ma * stoneA.px;
+      const cb = stoneB.py - mb * stoneB.px;
+
+      const rpx = (cb - ca) / (ma - mb);
+      const rpy = ma * rpx + ca;
+
+      const time = Math.round((rpx - stoneA.px) / (stoneA.vx - rvx));
+      const rpz = stoneA.pz + (stoneA.vz - rvz) * time;
+
+      const result = rpx + rpy + rpz;
+      if (!results[result]) results[result] = 1;
+      else ++results[result];
+    }
+  }
+
+  return +Object.keys(results).sort((a, b) => results[+b] - results[+a])[0];
+};
 
 exercise(odds, [
-  [[TestCase], 5],
-  [[UserCase], 3617],
+  [[TestCase], 3],
+  [[UserCase], 641619849766168],
 ]);
