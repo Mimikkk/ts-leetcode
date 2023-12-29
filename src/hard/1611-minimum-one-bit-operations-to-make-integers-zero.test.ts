cd3785b4b337a4c75e8df253de4557dfcb80b87b
Hououin Kyouma <inwazzin@gmail.com>
2023-12-29 13:46:19 +0100

    1611

---
+++ src/hard/1611-minimum-one-bit-operations-to-make-integers-zero.test.ts
@@ -0,0 +1,15 @@
+import { exercise } from "@shared/utilities/exercise.js";
+
+const minimumOneBitOperations = (n: number): number => {
+  let count = 0;
+  for (let i = 0; i < 32; ++i) {
+    if (n & (1 << i)) count = (1 << (i + 1)) - 1 - count;
+  }
+  return count;
+};
+
+exercise(minimumOneBitOperations, [
+  [[0], 0],
+  [[3], 2],
+  [[6], 4],
+]);
