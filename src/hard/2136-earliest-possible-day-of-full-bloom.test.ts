4793d13b84238b16a8f991ea3454c4951991e0d1
Hououin Kyouma <inwazzin@gmail.com>
2023-12-31 19:26:31 +0100

    2136

---
+++ src/hard/2136-earliest-possible-day-of-full-bloom.test.ts
@@ -0,0 +1,40 @@
+import { exercise } from "@shared/utilities/exercise.js";
+
+const earliestFullBloom = (plantTime: number[], growTime: number[]): number => {
+  const times = Array.from(plantTime, (plant, i) => [plant, growTime[i]]).sort(([, a], [, b]) => b - a);
+
+  let min = 0;
+  let passedDays = 0;
+  for (const [plant, grow] of times) {
+    passedDays += plant;
+    const value = grow + passedDays;
+    if (value > min) min = value;
+  }
+
+  return min;
+};
+
+exercise(earliestFullBloom, [
+  [
+    [
+      [1, 4, 3],
+      [2, 3, 1],
+    ],
+    9,
+  ],
+  [
+    [
+      [1, 2, 3, 2],
+      [2, 1, 2, 1],
+    ],
+    9,
+  ],
+  [[[1], [1]], 2],
+  [
+    [
+      [1232, 312, 312, 312, 412, 4, 124, 124, 124, 12, 421, 4, 124, 1242, 14, 12, 4, 1235, 124],
+      [124, 12, 412, 4, 124, 124, 12, 412, 4, 124, 124, 12, 412, 4, 124, 124, 12, 4, 5235],
+    ],
+    6152,
+  ],
+]);
