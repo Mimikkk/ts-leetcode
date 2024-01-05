85fc2c009cea471c6207722596de38e7547f05e1
Hououin Kyouma <inwazzin@gmail.com>
2024-01-05 17:24:00 +0100

    1356 init

---
+++ src/hard/1356-minimum-number-of-increments-on-subarrays-to-form-a-target-array.test.ts
@@ -0,0 +1,11 @@
+import { exercise } from "@shared/utilities/exercise.js";
+
+const minNumberOperations = (target: number[]): number => {
+  return 0;
+};
+
+exercise(minNumberOperations, [
+  [[[1, 2, 3, 2, 1]], 3],
+  [[[3, 1, 1, 2]], 4],
+  [[[3, 1, 5, 4, 2]], 7],
+]);
