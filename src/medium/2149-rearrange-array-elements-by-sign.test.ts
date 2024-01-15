29d2e66c2aa045b6a06ac7c6c2a220ecc2647969
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2024-01-15 16:15:08 +0100

    2149

---
+++ src/medium/2149-rearrange-array-elements-by-sign.test.ts
@@ -0,0 +1,22 @@
+import { exercise } from "@shared/utilities/exercise.js";
+import { TreeNode } from "@shared/structures/index.js";
+
+const rearrangeArray = (nums: number[]): number[] => {
+  const positives: number[] = [];
+  const negatives: number[] = [];
+  for (const n of nums) (n < 0 ? negatives : positives).push(n);
+
+  const result = Array(nums.length);
+  for (let i = 0; i < nums.length; i += 2) {
+    result[i] = positives[i / 2];
+    result[i + 1] = negatives[i / 2];
+  }
+
+  return result;
+};
+
+exercise(rearrangeArray, [
+  [[[3, 1, -2, -5, 2, -4]], [3, -2, 1, -5, 2, -4]],
+  [[[-1, 1]], [1, -1]],
+  [[[]], []],
+]);
