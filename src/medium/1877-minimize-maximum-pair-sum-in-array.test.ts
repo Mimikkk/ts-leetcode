6cfc1db12ee57e414e3b787e9e0139f58e5b5120
Hououin Kyouma <inwazzin@gmail.com>
2024-01-16 15:25:24 +0100

    807, 1877

---
+++ src/medium/1877-minimize-maximum-pair-sum-in-array.test.ts
@@ -0,0 +1,14 @@
+import { exercise } from "@shared/utilities/exercise.js";
+
+const minPairSum = (nums: number[]): number =>
+  Math.max(
+    ...nums
+      .sort((a, b) => a - b)
+      .slice(0, nums.length / 2)
+      .map((n, i) => n + nums[nums.length - i - 1]),
+  );
+
+exercise(minPairSum, [
+  [[[3, 5, 2, 3]], 7],
+  [[[3, 5, 4, 2, 4, 6]], 8],
+]);
---
+++ src/medium/807-max-increase-to-keep-city-skyline.test.ts
@@ -0,0 +1,42 @@
+import { exercise } from "@shared/utilities/exercise.js";
+
+const max = (n: number[]) => Math.max(...n);
+const maxIncreaseKeepingSkyline = (grid: number[][]): number => {
+  const rowMaxes = grid.map(max);
+  const colMaxes = grid[0].map((_, j) => grid.map((row) => row[j])).map(max);
+
+  let gridSum = 0;
+  const n = grid.length;
+  const m = grid[0].length;
+  for (let i = 0; i < n; ++i) {
+    for (let j = 0; j < m; ++j) {
+      gridSum += Math.min(rowMaxes[i], colMaxes[j]) - grid[i][j];
+    }
+  }
+
+  return gridSum;
+};
+
+exercise(maxIncreaseKeepingSkyline, [
+  [
+    [
+      [
+        [3, 0, 8, 4],
+        [2, 4, 5, 7],
+        [9, 2, 6, 3],
+        [0, 3, 1, 0],
+      ],
+    ],
+    35,
+  ],
+  [
+    [
+      [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+      ],
+    ],
+    0,
+  ],
+]);
