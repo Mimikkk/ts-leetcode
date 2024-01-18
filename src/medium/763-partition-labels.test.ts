0133a7d25887f1bfc2e9373801bdc0f76ac784dd
Hououin Kyouma <inwazzin@gmail.com>
2024-01-18 16:25:51 +0100

    763

---
+++ src/medium/763-partition-labels.test.ts
@@ -0,0 +1,31 @@
+import { exercise } from "@shared/utilities/exercise.js";
+
+const partitionLabels = (s: string): number[] => {
+  const sizes: number[] = [];
+  const occurrences = new Map([...s].map((c, i) => [c, i]));
+
+  let head = 0;
+  let tail = 0;
+  let previous = 0;
+  while (head < s.length) {
+    let lastIndex = occurrences.get(s[head])!;
+
+    while (tail <= lastIndex) {
+      const index = occurrences.get(s[tail])!;
+      if (index > lastIndex) lastIndex = index;
+      ++tail;
+    }
+
+    sizes.push(tail - previous);
+    previous = tail;
+    head = tail;
+  }
+
+  return sizes;
+};
+
+exercise(partitionLabels, [
+  [["ababcbacadefegdehijhklij"], [9, 7, 8]],
+  [["eccbbbbdec"], [10]],
+  [["caedbdedda"], [1, 9]],
+]);
