bb54459b7f7797660dfd5c0d5248f95160e6b446
Hououin Kyouma <inwazzin@gmail.com>
2024-01-19 21:18:26 +0100

    2079

---
+++ src/medium/2079-watering-plants.test.ts
@@ -0,0 +1,23 @@
+import { exercise } from "@shared/utilities/exercise.js";
+
+const wateringPlants = (thirst: number[], capacity: number): number => {
+  let steps = thirst.length;
+  let water = capacity;
+
+  for (let i = 0; i < thirst.length; i++) {
+    if (water < thirst[i]) {
+      steps += 2 * i;
+      water = capacity;
+    }
+
+    water -= thirst[i];
+  }
+
+  return steps;
+};
+
+exercise(wateringPlants, [
+  [[[2, 2, 3, 3], 5], 14],
+  [[[1, 1, 1, 4, 2, 3], 4], 30],
+  [[[7, 7, 7, 7, 7, 7, 7], 8], 49],
+]);
