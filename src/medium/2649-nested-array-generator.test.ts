7b7a9d2f397629e6093664a497b803799a0441d7
Hououin Kyouma <inwazzin@gmail.com>
2024-01-20 14:00:18 +0100

    950, 2649

--- src/medium/2649-nested-array-generator.test.ts
+++ src/medium/2649-nested-array-generator.test.ts
@@ -1,20 +1,14 @@
 import { exercise } from "@shared/utilities/exercise.js";
-import { it, suite } from "vitest";
 
-type Tensor = (Tensor | number)[];
+const countVowelStrings = (n: number): number => {
+  const memory = [1, 2, 3, 4, 5];
 
-function* inorderTraversal(tensor: Tensor): Generator<number, void, unknown> {
-  for (const value of tensor) {
-    if (Array.isArray(value)) {
-      yield* inorderTraversal(value);
-    } else {
-      yield value;
-    }
-  }
-}
+  for (let i = 1; i < n; ++i) for (let j = 1; j < 5; ++j) memory[j] += memory[j - 1];
 
-it("should traverse a nested array", () => {
-  const tensor: Tensor = [1, [2, 3], 4, [5, [6, 7, [8, 9]]]];
-  const values = [...inorderTraversal(tensor)];
-  expect(values).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-});
+  return memory[4];
+};
+
+exercise(countVowelStrings, [
+  [[1], 5],
+  [[2], 15],
+]);
---
+++ src/medium/950-reveal-cards-in-increasing-order.test.ts
@@ -0,0 +1,18 @@
+import { exercise } from "@shared/utilities/exercise.js";
+
+const deckRevealedIncreasing = (deck: number[]): number[] => {
+  const stack = deck.sort((a, b) => b - a);
+  const queue = [stack.shift()!];
+
+  while (stack.length > 0) {
+    queue.unshift(queue.pop()!);
+    queue.unshift(stack.shift()!);
+  }
+
+  return queue;
+};
+
+exercise(deckRevealedIncreasing, [
+  [[[17, 13, 11, 2, 3, 5, 7]], [2, 13, 3, 11, 5, 17, 7]],
+  [[[1, 1000]], [1, 1000]],
+]);
