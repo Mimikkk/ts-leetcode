86aaf783387231f27d80dda62aba73540f9a7e5a
Hououin Kyouma <inwazzin@gmail.com>
2024-01-29 22:28:39 +0100

    2951

---
+++ src/medium/2951-find-the-peaks.test.ts
@@ -0,0 +1,16 @@
+import { exercise } from "@shared/utilities/exercise.js";
+
+const findPeaks = (mountain: number[]): number[] => {
+  const peakIndices = [];
+
+  for (let i = 1; i < mountain.length - 1; ++i) {
+    if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) peakIndices.push(i);
+  }
+
+  return peakIndices;
+};
+
+exercise(findPeaks, [
+  [[[2, 4, 4]], []],
+  [[[1, 4, 3, 8, 5]], [1, 3]],
+]);
