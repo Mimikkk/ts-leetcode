4da20030a93f456f138f148e990d36e269a454e8
Hououin Kyouma <inwazzin@gmail.com>
2024-02-11 22:52:45 +0100

    1043

---
+++ src/medium/1043-partition-array-for-maximum-sum.test.ts
@@ -0,0 +1,25 @@
+import { exercise } from "@shared/utilities/exercise.js";
+
+const maxSumAfterPartitioning = (arr: number[], k: number): number => {
+  const n = arr.length;
+  const sums = Array(n + 1).fill(0);
+
+  for (let index = 1; index <= n; ++index) {
+    let max = 0;
+    for (let offset = 1; offset <= k; ++offset) {
+      const previous = arr[index - offset];
+      if (previous > max) max = previous;
+
+      const sum = sums[index - offset] + max * offset;
+      if (sums[index] < sum) sums[index] = sum;
+    }
+  }
+
+  return sums[n];
+};
+
+exercise(maxSumAfterPartitioning, [
+  [[[1, 15, 7, 9, 2, 5, 10], 3], 84],
+  [[[1, 4, 1, 5, 7, 3, 6, 1, 9, 9, 3], 4], 83],
+  [[[1], 1], 1],
+]);
