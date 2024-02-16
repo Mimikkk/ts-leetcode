99bf7ed9505ac0f0ca4b717933c9b2aebcb03b75
Hououin Kyouma <inwazzin@gmail.com>
2024-02-16 18:33:52 +0100

    2900

---
+++ src/medium/2900-longest-unequal-adjacent-groups-subsequnece.test.ts
@@ -0,0 +1,25 @@
+import { exercise } from "@shared/utilities/exercise.js";
+
+const getLongestSubsequence = (words: string[], groups: (0 | 1)[]): string[] =>
+  words.reduce<string[]>((acc, word, index) => {
+    if (groups[index] !== groups[index - 1]) acc.push(word);
+
+    return acc;
+  }, []);
+
+exercise(getLongestSubsequence, [
+  [
+    [
+      ["e", "a", "b"],
+      [0, 0, 1],
+    ],
+    ["e", "b"],
+  ],
+  [
+    [
+      ["a", "b", "c", "d"],
+      [1, 0, 1, 1],
+    ],
+    ["a", "b", "c"],
+  ],
+]);
