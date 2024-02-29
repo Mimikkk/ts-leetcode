fd42dea04919c11f1a8712d5d9036a1336c310f3
Hououin Kyouma <inwazzin@gmail.com>
2024-02-29 23:50:13 +0100

    bench

--- src/benches/typed-key.bench.ts
+++ src/benches/typed-key.bench.ts
@@ -1,18 +1,12 @@
 import { bench } from "@arktype/attest";
 
-type TypedKey<T, K> = keyof { [P in keyof T as T[P] extends K ? P : never]: P };
+type A_TypedKey<T, K> = keyof { [P in keyof T as T[P] extends K ? P : never]: P };
+type B_TypedKey<T, K> = { [P in keyof T]: T[P] extends K ? P : never }[keyof T];
 
-type makeComplexType<s extends string> = s extends `${infer head}${infer tail}`
-  ? head | tail | makeComplexType<tail>
-  : s;
+bench("Version - A", () => {
+  return {} as A_TypedKey<{ a: string; b: number; c: string }, string>;
+}).types([38, "instantiations"]);
 
-bench("bench type", () => {
-  return {} as makeComplexType<"defenestration">;
-  // This is an inline snapshot that will be populated or compared when you run the file
-}).types([169, "instantiations"]);
-
-bench("bench runtime and type", () => {
-  return {} as makeComplexType<"antidisestablishmentarianism">;
-})
-  .mean([2, "ns"])
-  .types([1, "instantiations"]);
+bench("Version - B", () => {
+  return {} as B_TypedKey<{ a: string; b: number; c: string }, string>;
+}).types([24, "instantiations"]);
