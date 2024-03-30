d6de4bb51ea6e736d316bb6e4071ea9eb6501ffa
Hououin Kyouma <inwazzin@gmail.com>
2024-03-30 15:47:08 +0100

    1978, 2070

--- src/type-challenges/medium/1978-percentage-share.ts
+++ src/type-challenges/medium/1978-percentage-share.ts
@@ -1,38 +1,8 @@
-/*
-  1978 - Percentage Parser
-  -------
-  by SSShuai1999 (@SSShuai1999) #medium #template-literal
+type PercentageSign<S extends string> = S extends `+${string}` ? "+" : S extends `-${string}` ? "-" : "";
+type PercentageUnit<S extends string> = S extends `${string}%` ? "%" : "";
+type PercentageValue<S extends string> = S extends `${PercentageSign<S>}${infer A}${PercentageUnit<S>}` ? A : "";
 
-  ### Question
-
-  Implement PercentageParser<T extends string>.
-  According to the `/^(\+|\-)?(\d*)?(\%)?$/` regularity to match T and get three matches.
-
-  The structure should be: [`plus or minus`, `number`, `unit`]
-  If it is not captured, the default is an empty string.
-
-  For example:
-
-  ```ts
-  type PString1 = ""
-  type PString2 = "+85%"
-  type PString3 = "-85%"
-  type PString4 = "85%"
-  type PString5 = "85"
-
-  type R1 = PercentageParser<PString1> // expected ['', '', '']
-  type R2 = PercentageParser<PString2> // expected ["+", "85", "%"]
-  type R3 = PercentageParser<PString3> // expected ["-", "85", "%"]
-  type R4 = PercentageParser<PString4> // expected ["", "85", "%"]
-  type R5 = PercentageParser<PString5> // expected ["", "85", ""]
-  ```
-
-  > View on GitHub: https://tsch.js.org/1978
-*/
-
-/* _____________ Your Code Here _____________ */
-
-type PercentageParser<A extends string> = any;
+type PercentageParser<S extends string> = [PercentageSign<S>, PercentageValue<S>, PercentageUnit<S>];
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from "@type-challenges/utils";
@@ -64,11 +34,3 @@ type cases = [
   Expect<Equal<PercentageParser<"1">, Case10>>,
   Expect<Equal<PercentageParser<"100">, Case11>>,
 ];
-
-/* _____________ Further Steps _____________ */
-/*
-  > Share your solutions: https://tsch.js.org/1978/answer
-  > View solutions: https://tsch.js.org/1978/solutions
-  > More Challenges: https://tsch.js.org
-*/
-
--- src/type-challenges/medium/2070-drop-char.ts
+++ src/type-challenges/medium/2070-drop-char.ts
@@ -1,24 +1,8 @@
-/*
-  2070 - Drop Char
-  -------
-  by CaptainOfPhB (@CaptainOfPhB) #medium #template-literal #infer
-
-  ### Question
-
-  Drop a specified char from a string.
-
-  For example:
-
-  ```ts
-  type Butterfly = DropChar<' b u t t e r f l y ! ', ' '> // 'butterfly!'
-  ```
-
-  > View on GitHub: https://tsch.js.org/2070
-*/
-
-/* _____________ Your Code Here _____________ */
-
-type DropChar<S, C> = any;
+type DropChar<S extends string, C extends string> = S extends `${infer A}${infer B}`
+  ? Equal<A, C> extends true
+    ? DropChar<B, C>
+    : `${A}${DropChar<B, C>}`
+  : S;
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from "@type-challenges/utils";
@@ -33,10 +17,3 @@ type cases = [
   Expect<Equal<DropChar<" b u t t e r f l y ! ", "b">, "  u t t e r f l y ! ">>,
   Expect<Equal<DropChar<" b u t t e r f l y ! ", "t">, " b u   e r f l y ! ">>,
 ];
-
-/* _____________ Further Steps _____________ */
-/*
-  > Share your solutions: https://tsch.js.org/2070/answer
-  > View solutions: https://tsch.js.org/2070/solutions
-  > More Challenges: https://tsch.js.org
-*/
